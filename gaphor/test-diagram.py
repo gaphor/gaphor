a5cd19071e1784f3d89c1f106f3447445437dd6d
Arjan Molenaar <gaphor@gmail.com>
2002-01-12 09:47:44 +0000

    Now need DiaCanvas 0.5.0
    git-svn-id: file:///Users/arjan/backup/gaphor/trunk/gaphor@28 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/test-diagram.py
+++ gaphor/test-diagram.py
@@ -10,66 +10,10 @@ import diacanvas
 import UML
 
 def mainquit(*args):
+    for k in UML.Element._hash.keys():
+	print "Element", k, ":", UML.Element._hash[k]().__dict__
+    print "Forcing Garbage collection:"
     gtk.main_quit()
-    print "Comment.body:", comment.body
-    print "Actor.name:", actor.name
-    #sys.exit(0)
-
-def display_canvas_view(view):
-	win = gtk.Window()
-	win.connect ('destroy', mainquit)
-	win.set_title ('DiaCanvas Python example')
-	win.set_default_size (300, 300)
-	
-	vbox = gtk.VBox()
-	win.add (vbox)
-	vbox.show()
-	
-	print "table"
-	table = gtk.Table(2,2, gtk.FALSE)
-	table.set_row_spacings (4)
-	table.set_col_spacings (4)
-	vbox.pack_start (table)
-	table.show()
-
-	print "frame"
-	frame = gtk.Frame()
-	frame.set_shadow_type (gtk.SHADOW_IN)
-	table.attach (frame, 0, 1, 0, 1,
-		      gtk.EXPAND | gtk.FILL | gtk.SHRINK,
-		      gtk.EXPAND | gtk.FILL | gtk.SHRINK)
-	frame.show()
-
-	print "view"
-	view = diacanvas.CanvasView (canvas=diagram.canvas)
-	view.set_scroll_region(0, 0, 600, 450)
-	frame.add (view)
-	view.show()
-	del view
-
-	print "sbar"
-	sbar = gtk.VScrollbar (view.get_vadjustment())
-	table.attach (sbar, 1, 2, 0, 1, gtk.FILL,
-		      gtk.EXPAND | gtk.FILL | gtk.SHRINK)
-	sbar.show()
-
-	print "sbar"
-	sbar = gtk.HScrollbar (view.get_hadjustment())
-	table.attach (sbar, 0, 1, 1, 2, gtk.EXPAND | gtk.FILL | gtk.SHRINK,
-		      gtk.FILL)
-	sbar.show()
-
-	print "showing"
-	win.show()
-	print "done"
-
-diagram_list = [ ]
-	
-def display_diagram(diagram):
-    view = diacanvas.CanvasView()
-    diagram_list.append(view)
-    view.set_canvas(diagram.canvas)
-    display_canvas_view (view)
 
 # First some consistency tests
 dia = diagram.Diagram()
@@ -85,33 +29,50 @@ print dia.namespace
 
 assert (dia in pack.ownedElement.list)
 
-#dia = diacanvas.Canvas()
+del dia
+del pack
+UML.Element_hash_gc()
+for k in UML.Element._hash.keys():
+    print "Element", k, ":", UML.Element._hash[k]().__dict__
+
 dia = diagram.Diagram()
 
 #item = canvas.root.add (diagram.Comment)
 #item.move (30, 50)
-item = dia.create_item (diagram.Comment, (10,10))
-comment = item.get_subject()
+#item = dia.create_item (diagram.Comment, (10,10))
 
 #item = canvas.root.add (diagram.Actor)
 #item.move (150, 50)
 item = dia.create_item (diagram.Actor, (150, 50))
 actor = item.get_subject()
-actor.name = "Some text"
-
-item = dia.create_item (diagram.UseCase, (50, 150))
+actor.name = "Actor"
+#item.set(name="Some text")
+del actor
+#item = dia.create_item (diagram.UseCase, (50, 150))
 
 item = dia.canvas.root.add(diacanvas.CanvasLine, add_point=(0, 0))
 item.set (add_point=(100,100))
+del item
 
 #view = diacanvas.CanvasView().set_canvas (dia.canvas)
 #display_diagram (dia)
 #display_canvas_view (diacanvas.CanvasView().set_canvas (dia.canvas))
-dia.new_view()
-#win.connect ('destroy', mainquit)
+win = dia.new_view()
+win.connect ('destroy', mainquit)
+del win
 print "diagram displayed"
 
 gtk.main()
 
-print "Comment.body:", comment.body
-print "Actor.name:", actor.name
+print "Garbage collection after gtk.main() has finished:"
+UML.Element_hash_gc()
+for k in UML.Element._hash.keys():
+    print "Element", k, ":", UML.Element._hash[k]().__dict__
+
+del dia
+print "removed Diagram:"
+UML.Element_hash_gc()
+for k in UML.Element._hash.keys():
+    print "Element", k, ":", UML.Element._hash[k]().__dict__
+
+print "Program ended normally..."
