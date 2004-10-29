1c2639fad3322faeb70e033e4f0a88d95f9a9120
slmm <noreply@example.com>
2004-10-29 12:41:19 +0000

    Made it sort of working
    git-svn-id: file:///Users/arjan/backup/gaphor/trunk/gaphor@442 a8418922-720d-0410-834f-a69b97ada669

--- data/plugins/pngexport/pngexport.py
+++ data/plugins/pngexport/pngexport.py
@@ -9,9 +9,9 @@ class PNGExport(Action):
 
     def execute(self):
         view=diacanvas.get_active_view()
-        window=view.get_root_window()
+        window=view.window
         (x,y,width,height,depth) = window.get_geometry()
         pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,width,height)
-        pixbuf.get_from_drawable(window, view.get_colormap(),0,0,0,0,width,height)
-        pixbuf.save("/tmp/screenshot.png","png")
+        buffer=pixbuf.get_from_drawable(window, view.get_colormap(),0,0,0,0,width,height)
+        buffer.save("/tmp/screenshot.png","png")
         
