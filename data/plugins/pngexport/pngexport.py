968ddb4233d813b0db73deee6b9a522d8895691c
Arjan Molenaar <gaphor@gmail.com>
2004-11-05 15:14:23 +0000

    *** empty log message ***
    git-svn-id: file:///Users/arjan/backup/gaphor/trunk/gaphor@458 a8418922-720d-0410-834f-a69b97ada669

--- data/plugins/pngexport/pngexport.py
+++ data/plugins/pngexport/pngexport.py
@@ -8,10 +8,12 @@ import gtk
 class PNGExport(Action):
 
     def execute(self):
-        view=diacanvas.get_active_view()
-        window=view.window
+        view = self.get_window().get_current_diagram_view()
+        #view=diacanvas.get_active_view()
+        window = view.window
+        # Should use canvas geometry:
         (x,y,width,height,depth) = window.get_geometry()
         pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,width,height)
-        buffer=pixbuf.get_from_drawable(window, view.get_colormap(),0,0,0,0,width,height)
+        buffer = pixbuf.get_from_drawable(window, view.get_colormap(),0,0,0,0,width,height)
         buffer.save("/tmp/screenshot.png","png")
         
