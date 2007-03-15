b7ae80a6e6a32c9eef07aaf6841ddb2a9bde116b
Arjan Molenaar <gaphor@gmail.com>
2007-03-15 13:35:07 +0000

    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@1167 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/diagramline.py
+++ gaphor/diagram/diagramline.py
@@ -20,7 +20,7 @@ class LineItem(gaphas.Line, DiagramItem):
     def __init__(self, id = None):
         gaphas.Line.__init__(self)
         DiagramItem.__init__(self, id)
-        self.fuzzyness = 2
+        self.fuzziness = 2
         self._stereotype_bounds = None
 
     head = property(lambda self: self._handles[0])
