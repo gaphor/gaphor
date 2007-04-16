c25f4cf980524db4ea2ce73ab275f4a4a2ecc75a
Arjan Molenaar <gaphor@gmail.com>
2007-04-16 11:29:31 +0000

    Fixed #27. alignment works.
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@1224 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/data/plugins/alignment/alignment.py
+++ gaphor/data/plugins/alignment/alignment.py
@@ -1,6 +1,10 @@
+
+from gaphor.core import transactional
 from gaphor.plugin import Action
 
+
 class Alignment(Action):
+
     def init(self, window):
 	self._window = window
     
@@ -8,46 +12,49 @@ class Alignment(Action):
         diagram_tab = self.get_window().get_current_diagram_tab()
         self.sensitive = diagram_tab and len(diagram_tab.get_view().selected_items) > 0
 
+    @transactional
     def execute(self):
         view = self._window.get_current_diagram_view()
         items = view.selected_items
         if len(items)>1:
-            view.canvas.undo_manager.begin_transaction()
             self.align(items)
-            view.canvas.undo_manager.commit_transaction()
         
     def moveItemsToTarget(self, items, target_x, target_y):
         for item in items:
             if target_x is not None:
-                affine=list(item.item.affine)
-                x=target_x-affine[4]
+                x = target_x - item.matrix[4]
             else:
-                x=0
+                x = 0
             if target_y is not None:
-                affine=list(item.item.affine)
-                y=target_y-affine[5]
+                y = target_y - item.matrix[5]
             else:
-                y=0
-            item.item.move(x,y)
+                y = 0
+            item.matrix.translate(x,y)
+            item.request_update()
             
-    def getXCords(self, items):
-        return [item.item.affine[4] for item in items]
-
-    def getYCords(self, items):
-        return [item.item.affine[5] for item in items]
-
     def align(self, items):
         pass
 
+
 class AlignVertical(Alignment):
+	
+    def getXCoords(self, items):
+        return [item.matrix[4] for item in items]
+
     def align(self, items):
-        target_x=min(self.getXCords(items))
+        target_x=min(self.getXCoords(items))
         target_y=None
         self.moveItemsToTarget(items, target_x, target_y)
 
+
 class AlignHorizontal(Alignment):
+
+    def getYCoords(self, items):
+        return [item.matrix[5] for item in items]
+
     def align(self, items):
-        target_y=min(self.getYCords(items))
+        target_y=min(self.getYCoords(items))
         target_x=None
         self.moveItemsToTarget(items, target_x, target_y)
 
+# vim:sw=4:et:ai
