869259950d03d6157ecc14d4482a0bdecb9baba9
Arjan Molenaar <gaphor@gmail.com>
2007-04-18 06:08:21 +0000

    load element_factory as service, make on demand service initialization work.
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@1232 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/application.py
+++ gaphor/application.py
@@ -23,19 +23,12 @@ class _Application(object):
     # interface.implements(IApplication)
     
     def __init__(self):
-        self.services = {
-            'element_factory': gaphor.UML.ElementFactory()
-        }
-    
-#    def __getattr__(self, key):
-#        return self.services[key]
+        self._uninitialized_services = {}
 
     def init(self):
         """
         Initialize the application.
         """
-        #import gaphor.adapters
-        #import gaphor.actions
         self.load_services()
 
     def load_services(self):
@@ -45,7 +38,6 @@ class _Application(object):
         Services are registered as utilities in zope.component.
         Service should provide an interface gaphor.interfaces.IService.
         """
-        services = []
         for ep in pkg_resources.iter_entry_points('gaphor.services'):
             #print ep, dir(ep)
             log.debug('found entry point service.%s' % ep.name)
@@ -53,18 +45,41 @@ class _Application(object):
             if not IService.implementedBy(cls):
                 raise 'MisConfigurationException', 'Entry point %s doesn''t provide IService' % ep.name
             srv = cls()
-            services.append((ep.name, srv))
+            self._uninitialized_services[ep.name] = srv
 
-        for name, srv in services:
-            log.debug('initializing service.%s' % name)
+        # HACK: implicitly add UML.ElementFactory for now.
+        from gaphor import UML
+        self._uninitialized_services['element_factory'] = resource(UML.ElementFactory)
+        # /HACK
+
+        while self._uninitialized_services:
+            self.init_service(self._uninitialized_services.iterkeys().next())
+
+    def init_service(self, name):
+        """
+        Initialize a not yet initialized service.
+
+        Raises ComponentLookupError if the service has nor been found
+        """
+        try:
+            srv = self._uninitialized_services.pop(name)
+        except KeyError:
+            raise component.ComponentLookupError(IService, name)
+        else:
+            log.info('initializing service service.%s' % name)
+            # TODO: do init() before provideUtility!
             component.provideUtility(srv, IService, name)
             srv.init(self)
+            return srv
 
     distribution = property(lambda s: pkg_resources.get_distribution('gaphor'),
                             doc='Get the PkgResources distribution for Gaphor')
 
     def get_service(self, name):
-        return component.getUtility(IService, name)
+        try:
+            return component.getUtility(IService, name)
+        except component.ComponentLookupError:
+            return self.init_service(name)
 
     def run(self):
         import gtk
