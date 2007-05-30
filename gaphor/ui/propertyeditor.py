13fb576a4360efb87750b6214cec8b0315bb20e8
Arjan Molenaar <gaphor@gmail.com>
2007-05-30 19:45:58 +0000

    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@1320 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/ui/propertyeditor.py
+++ gaphor/ui/propertyeditor.py
@@ -16,12 +16,15 @@ class PropertyEditor(object):
     def __init__(self):
         super(PropertyEditor, self).__init__()
         self._current_item = None
+        self._last_tab = None
     
     def construct(self):
         self.notebook = gtk.Notebook()
-        self.notebook.set_scrollable(False)
+        self.notebook.set_scrollable(True)
         self.notebook.set_show_border(True)
-
+        self.notebook.set_tab_pos(gtk.POS_LEFT)
+        self.notebook.connect('switch-page', self._on_switch_page)
+        
         # Make sure we recieve 
         component.provideHandler(self._selection_change)
         
@@ -31,17 +34,38 @@ class PropertyEditor(object):
         """
         Load all tabs that can operate on the given item.
         """
+        last_tab = self._last_tab
         for name, adapter in component.getAdapters([item,], IPropertyPage):
             self.notebook.prepend_page(adapter.construct(), gtk.Label(name))
         self.notebook.show_all()
+
+        self._last_tab = last_tab
+
+        # Show the last selected tab again.
+        for page_num in range(0, self.notebook.get_n_pages()):
+            page = self.notebook.get_nth_page(page_num)
+            label_text = self.notebook.get_tab_label_text(page)
+            if label_text == self._last_tab:
+                self.notebook.set_current_page(page_num)
+                break
+
+            
         
     def clear_all_tabs(self):
         """
         Remove all tabs from the notebook.
         """
+        last_tab = self._last_tab
         while self.notebook.get_n_pages():
             self.notebook.remove_page(0)
+        self._last_tab = last_tab
         
+    def _on_switch_page(self, notebook, page, page_num):
+        page = self.notebook.get_nth_page(page_num)
+        label_text = self.notebook.get_tab_label_text(page)
+        self._last_tab = label_text
+        log.debug('New last property page %s: %s' % (page, label_text))
+
     @component.adapter(IDiagramSelectionChange)
     def _selection_change(self, event):
         """
