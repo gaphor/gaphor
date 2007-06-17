f8630ea69e482f9ad01d27385227132c3fab5fa4
wrobell <wrobell@pld-linux.org>
2007-06-17 16:12:39 +0000

    - use text elements for named items
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@1407 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/nameditem.py
+++ gaphor/diagram/nameditem.py
@@ -34,22 +34,29 @@ class NamedItem(ElementItem):
 
         self.width  = self.min_width
         self.height = self.min_height
+        self._name = self.add_text('name', style = self.__style__)
+
         self.name_x = 0
         self.name_y = 0
         self._name_size = (0, 0)
+
 
     def on_subject_notify(self, pspec, notifiers=()):
         #log.debug('Class.on_subject_notify(%s, %s)' % (pspec, notifiers))
         ElementItem.on_subject_notify(self, pspec, ('name',) + notifiers)
 
+
     def on_subject_notify__name(self, subject, pspec=None):
+        self._name.text = subject.name
         self.request_update()
+
 
     def get_name_size(self):
         """
         Return width, height of the text (including padding)
         """
         return self._name_size
+
 
     def update_name_size(self, context):
         """
@@ -61,32 +68,3 @@ class NamedItem(ElementItem):
             width, height = text_extents(cr, text)
             padding = self.style.name_padding
             self._name_size = width + padding[0] + padding[2], height + padding[1] + padding[3]
-
-    def update(self, context):
-        """
-        Calculate position of item's name.
-        """
-        cr = context.cairo
-        text = self.subject.name
-        if text:
-            width, height = text_extents(cr, text)
-
-            self.name_x, self.name_y = get_text_point(text_extents(cr, text),
-                    self.width, self.height,
-                    self.style.name_align, self.style.name_padding,
-                    self.style.name_outside)
-        super(NamedItem, self).update(context)
-
-
-    def draw(self, context):
-        """
-        Draw item's name.
-        """
-        cr = context.cairo
-
-        text = self.subject.name
-        if text:
-            #cr.move_to(self.name_x, self.name_y)
-            #cr.show_text(text)
-            text_align(cr, self.name_x, self.name_y, text, 1, -1)
-        super(NamedItem, self).draw(context)
