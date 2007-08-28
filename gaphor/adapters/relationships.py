9fbc07956ee56af8d02b64d558772842f9c1ee86
wrobell <wrobell@pld-linux.org>
2007-08-28 11:18:02 +0000

    - it is not ready yet, therefore commented out to allow run unit tests   nicely
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2054 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/adapters/relationships.py
+++ gaphor/adapters/relationships.py
@@ -10,36 +10,36 @@ by drag and drop).
 from zope import component
 
 
-class AssociationRelationship(Relationship):
-    """Relationship for associations.
-    """
-    def relationship(self, line, head_subject = None, tail_subject = None):
-        # First check if we do not already contain the right subject:
-        if line.subject:
-            end1 = line.subject.memberEnd[0]
-            end2 = line.subject.memberEnd[1]
-            if (end1.type is head_type and end2.type is tail_type) \
-               or (end2.type is head_type and end1.type is tail_type):
-                return
-                
-        # Find all associations and determine if the properties on the
-        # association ends have a type that points to the class.
-        Association = UML.Association
-        for assoc in resource(UML.ElementFactory).itervalues():
-            if isinstance(assoc, Association):
-                #print 'assoc.memberEnd', assoc.memberEnd
-                end1 = assoc.memberEnd[0]
-                end2 = assoc.memberEnd[1]
-                if (end1.type is head_type and end2.type is tail_type) \
-                   or (end2.type is head_type and end1.type is tail_type):
-                    # check if this entry is not yet in the diagram
-                    # Return if the association is not (yet) on the canvas
-                    for item in assoc.presentation:
-                        if item.canvas is line.canvas:
-                            break
-                    else:
-                        return assoc
-        return None
+###class AssociationRelationship(Relationship):
+###    """Relationship for associations.
+###    """
+###    def relationship(self, line, head_subject = None, tail_subject = None):
+###        # First check if we do not already contain the right subject:
+###        if line.subject:
+###            end1 = line.subject.memberEnd[0]
+###            end2 = line.subject.memberEnd[1]
+###            if (end1.type is head_type and end2.type is tail_type) \
+###               or (end2.type is head_type and end1.type is tail_type):
+###                return
+###                
+###        # Find all associations and determine if the properties on the
+###        # association ends have a type that points to the class.
+###        Association = UML.Association
+###        for assoc in resource(UML.ElementFactory).itervalues():
+###            if isinstance(assoc, Association):
+###                #print 'assoc.memberEnd', assoc.memberEnd
+###                end1 = assoc.memberEnd[0]
+###                end2 = assoc.memberEnd[1]
+###                if (end1.type is head_type and end2.type is tail_type) \
+###                   or (end2.type is head_type and end1.type is tail_type):
+###                    # check if this entry is not yet in the diagram
+###                    # Return if the association is not (yet) on the canvas
+###                    for item in assoc.presentation:
+###                        if item.canvas is line.canvas:
+###                            break
+###                    else:
+###                        return assoc
+###        return None
 
 
 # vim:sw=4:et:ai
