0bf38e825a2dba3b52d8e38226cf88b419f4e365
wrobell <wrobell@pld-linux.org>
2007-08-28 12:05:52 +0000

    - this test is about loading services so do not use basic TestCase, which   loads element factory
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2060 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/tests/test_application.py
+++ gaphor/tests/test_application.py
@@ -2,27 +2,29 @@
 Application service test cases.
 """
 
-from gaphor.tests.testcase import TestCase
+import unittest
+
 from gaphor import UML
 from gaphor.application import Application
 from zope import component
 from gaphor.interfaces import IService
 
 
-class LoadServiceTestCase(TestCase):
-
-    services = ['undo_manager', 'file_manager']
-
+class LoadServiceTestCase(unittest.TestCase):
     def test_service_load(self):
         """
         Load services. At lease the undo_manager should be available after that.
         """
+        Application.init(['undo_manager', 'file_manager'])
+
         self.assertTrue(Application.get_service('undo_manager') is not None)
         self.assertTrue(Application.get_service('file_manager') is not None)
 
         # After that, services are also available as Utilities:
         self.assertTrue(component.queryUtility(IService, 'undo_manager') is not None)
         self.assertTrue(component.queryUtility(IService, 'file_manager') is not None)
+
+        Application.shutdown()
 
 
 # vim:sw=4:et:ai
