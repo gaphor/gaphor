a8984fcc28dbaa0517332a5d136ac36baa1f6835
wrobell <wrobell@pld-linux.org>
2007-08-28 08:49:20 +0000

    - killed test code, which should be in tests testing connection adapters - moved object node and flow tests into their own modules
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2040 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/tests/test_objectnode.py
+++ gaphor/diagram/tests/test_objectnode.py
@@ -1,9 +1,10 @@
 import unittest
 import gaphor.UML as UML
-from gaphor.application import Application
 from gaphor.diagram import items
+from gaphor.application import Application
 
-class FlowTestCase(unittest.TestCase):
+class ObjectNodeTestCase(unittest.TestCase):
+
     def setUp(self):
         Application.init(services=['element_factory'])
         self.element_factory = Application.get_service('element_factory')
@@ -12,7 +13,8 @@ class FlowTestCase(unittest.TestCase):
     def shutDown(self):
         Application.shutdown()
 
-    def test_flow(self):
-        return self.diagram.create(items.FlowItem, subject=self.element_factory.create(UML.ControlFlow))
+    def test_object_node(self):
+        return self.diagram.create(items.ObjectNodeItem, subject=self.element_factory.create(UML.ObjectNode))
 
 # vim:sw=4:et:ai
+
