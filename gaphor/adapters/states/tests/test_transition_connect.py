916df4e57ff77e3e0a410697999db0e294a3df70
wrobell <wrobell@pld-linux.org>
2007-10-19 18:59:23 +0000

    - added transition and state vertices disconnection tests
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2170 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/adapters/states/tests/test_transition_connect.py
+++ gaphor/adapters/states/tests/test_transition_connect.py
@@ -43,4 +43,41 @@ class TransitionConnectorTestCase(TestCase):
         assert t.subject.target == v2.subject.incoming
 
 
+    def test_vertex_connect(self):
+        """Test transition and state vertices disconnection
+        """
+        factory = self.element_factory
+
+        v1 = self.create(items.StateItem)
+        v2 = self.create(items.StateItem)
+
+        t = self.create(items.TransitionItem)
+        assert t.subject is None
+
+        adapter = component.queryMultiAdapter((v1, t), IConnect)
+        assert adapter is not None
+        
+        # connect head of transition to a state
+        adapter.connect(t.head)
+        assert t.subject is None
+
+        adapter = component.queryMultiAdapter((v2, t), IConnect)
+        assert adapter is not None
+
+        # connect tail of transition to a second state
+        adapter.connect(t.tail)
+        assert t.subject is not None
+
+        assert len(factory.lselect(lambda e: e.isKindOf(UML.Transition))) == 1
+        
+        assert t.subject.source == v1.subject.outgoing
+        assert t.subject.target == v2.subject.incoming
+
+        adapter.disconnect(t.tail)
+        assert t.subject is None
+
+        adapter.disconnect(t.head)
+        assert t.subject is None
+
+
 # vim:sw=4:et:ai
