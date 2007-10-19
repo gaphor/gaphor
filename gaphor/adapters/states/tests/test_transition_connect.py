44b32459a1bed4bf803c4d2c880b3f7d54612ad7
wrobell <wrobell@pld-linux.org>
2007-10-19 18:57:25 +0000

    - added transition connection test
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2169 a8418922-720d-0410-834f-a69b97ada669

---
+++ gaphor/adapters/states/tests/test_transition_connect.py
@@ -0,0 +1,46 @@
+"""
+Test transition item connections.
+"""
+
+from gaphor.tests import TestCase
+from zope import component
+from gaphor import UML
+from gaphor.diagram import items
+from gaphor.diagram.interfaces import IConnect
+
+class TransitionConnectorTestCase(TestCase):
+
+    services = ['element_factory', 'adapter_loader']
+
+    def test_vertex_connect(self):
+        """Test transition to state vertex connection
+        """
+        factory = self.element_factory
+
+        v1 = self.create(items.StateItem)
+        v2 = self.create(items.StateItem)
+
+        t = self.create(items.TransitionItem)
+        assert t.subject is None
+
+        adapter = component.queryMultiAdapter((v1, t), IConnect)
+        assert adapter is not None
+        
+        # connect head of transition to a state
+        adapter.connect(t.head)
+        assert t.subject is None
+
+        adapter = component.queryMultiAdapter((v2, t), IConnect)
+        assert adapter is not None
+
+        # connect tail of transition to a second state
+        adapter.connect(t.tail)
+        assert t.subject is not None
+
+        assert len(factory.lselect(lambda e: e.isKindOf(UML.Transition))) == 1
+        
+        assert t.subject.source == v1.subject.outgoing
+        assert t.subject.target == v2.subject.incoming
+
+
+# vim:sw=4:et:ai
