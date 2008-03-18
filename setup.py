71f89c660401395beac4480abaad161a85262fe2
Arjan Molenaar <gaphor@gmail.com>
2008-03-18 08:10:52 +0000

    added gaphorconvert entry point.
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2280 a8418922-720d-0410-834f-a69b97ada669

--- setup.py
+++ setup.py
@@ -162,6 +162,7 @@ It uses the GTK+ environment for user interaction.
     entry_points = {
         'console_scripts': [
             'gaphor = gaphor:main',
+            'gaphorconvert = gaphor.tools.gaphorconvert',
         ],
         'gaphor.services': [
             'adapter_loader = gaphor.services.adapterloader:AdapterLoader',
