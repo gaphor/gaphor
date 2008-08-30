eaf564c13ce1f967a804315b0608e1a5b7e2bc0d
Arjan Molenaar <gaphor@gmail.com>
2008-08-30 19:39:57 +0000

    disable backup-service
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2350 a8418922-720d-0410-834f-a69b97ada669

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ It uses the GTK+ environment for user interaction.
             'undo_manager = gaphor.services.undomanager:UndoManager',
             'element_factory = gaphor.UML.elementfactory:ElementFactory',
             'file_manager = gaphor.services.filemanager:FileManager',
-            'backup_service = gaphor.services.backupservice:BackupService',
+            #'backup_service = gaphor.services.backupservice:BackupService',
             'diagram_export_manager = gaphor.services.diagramexportmanager:DiagramExportManager',
             'action_manager = gaphor.services.actionmanager:ActionManager',
             'gui_manager = gaphor.services.guimanager:GUIManager',
