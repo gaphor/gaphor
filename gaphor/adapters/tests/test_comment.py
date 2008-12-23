c54e2a9cfafd8c2155ef3e1771990962961eaef9
wrobell <wrobell@pld-linux.org>
2008-12-23 10:53:47 +0000

    - removed test_commentline_association test, which duplicates other tests
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/branches/ports@2537 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/adapters/tests/test_comment.py
+++ gaphor/adapters/tests/test_comment.py
@@ -17,7 +17,7 @@ class CommentLineTestCase(TestCase):
         self.connect(line, line.head, comment)
         # connected, but no annotated element yet
         self.assertTrue(line.head.connected_to is not None)
-        self.assertTrue(comment.subject.annotatedElement is None)
+        self.assertFalse(comment.subject.annotatedElement)
 
 
     def test_commentline_same_comment_glue(self):
@@ -75,7 +75,7 @@ class CommentLineTestCase(TestCase):
 
         line.unlink()
 
-        self.assertTrue(comment.subject.annotatedElement is None)
+        self.assertFalse(comment.subject.annotatedElement)
         self.assertTrue(clazz.subject.ownedComment is None)
 
 
@@ -104,85 +104,5 @@ class CommentLineTestCase(TestCase):
 
         gen.unlink()
 
-        self.assertTrue(comment.subject.annotatedElement is None)
+        self.assertFalse(comment.subject.annotatedElement)
         self.assertTrue(gen.subject is None)
-
-
-    def test_commentline_association(self):
-        """
-        Test CommentLineItem with AssociationItem.
-
-        # TODO: check behaviour if:
-          1. comment line is connected to association + comment and after that
-             association is connected to two classes.
-             -> association should be connected to comment.annotatedElement
-          2. association is disconnected while a comment is connected:
-             -> association should be removed from comment.annotatedElement
-        """
-        comment = self.create(items.CommentItem, UML.Comment)
-        line = self.create(items.CommentLineItem)
-        line.head.pos = 100, 100
-        line.tail.pos = 100, 100
-        c1 = self.create(items.ClassItem, UML.Class)
-        c2 = self.create(items.ClassItem, UML.Class)
-        assoc = self.create(items.AssociationItem)
-
-        adapter = component.queryMultiAdapter((c1, assoc), IConnect)
-        handle = assoc.head
-        adapter.connect(handle, c1.ports()[0])
-
-        adapter = component.queryMultiAdapter((c2, assoc), IConnect)
-        handle = assoc.tail
-        adapter.connect(handle, c2.ports()[0]) 
-        assert assoc.head.connected_to is c1
-        assert assoc.tail.connected_to is c2
-        assert assoc.subject
-
-        # Connect the association item to the head of the line:
-
-        adapter = component.queryMultiAdapter((assoc, line), IConnect)
-        assert adapter
-        import gaphor.adapters.connectors
-        assert type(adapter) is gaphor.adapters.connectors.CommentLineLineConnect
-        handle = line.head
-        pos = adapter.glue(handle)
-        assert pos == (10, 50), pos
-        adapter.connect(handle, assoc.ports()[0])
-
-        assert handle.connected_to is assoc
-        assert handle.connection_data is not None
-        assert not comment.subject.annotatedElement
-
-        # Connecting two ends of the line to the same item is not allowed:
-
-        handle = line.tail
-        adapter.connect(handle)
-
-        assert handle.connected_to is None
-        assert not comment.subject.annotatedElement, comment.subject.annotatedElement
-
-        # now connect the comment
-
-        adapter = component.queryMultiAdapter((comment, line), IConnect)
-
-        handle = line.tail
-        adapter.connect(handle)
-
-        assert handle.connected_to is comment
-        assert handle.connection_data is not None
-        assert len(comment.subject.annotatedElement) == 1, comment.subject.annotatedElement
-        assert assoc.subject in comment.subject.annotatedElement, comment.subject.annotatedElement
-
-        # Disconnect comment:
-
-        adapter.disconnect(handle)
-
-        assert handle.connected_to is None, handle.connected_to
-        assert handle.connection_data is None
-        assert len(comment.subject.annotatedElement) == 0, comment.subject.annotatedElement
-        assert not assoc.subject in comment.subject.annotatedElement, comment.subject.annotatedElement
-
-        # Connect again:
-
-        adapter.connect(handle)
-        assert handle.connected_to is not None, handle.connected_to
