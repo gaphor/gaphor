1e1e9cf1af4a2e6811ba7583757207868450801c
wrobell <wrobell@pld-linux.org>
2009-01-20 02:44:15 +0000

    - refresh interface name on interface name change
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/branches/ports@2645 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/connector.py
+++ gaphor/diagram/connector.py
@@ -132,22 +132,28 @@ class ConnectorItem(NamedLine):
         super(ConnectorItem, self).__init__(id)
         self._end = None
         self._interface = self.add_text('end.role.name')
-        #self.add_watch(UML.Connector.end, self.on_connector_end)
 
-
-#    def on_connector_end(self, event):
-#        if isinstance(event, UML.event.AssociationAddEvent):
-#            self.end = event.new_value
-#            self._interface.text = self.end.role.name
 
     def _set_end(self, end):
         self._end = end
         if end and end.role:
             self._interface.text = self.end.role.name
+            self.add_watch(UML.Interface.name, self.on_interface_name) 
         else:
             self._interface.text = ''
+            # fixme: how to remove notification?
 
     end = property(attrgetter('_end'), _set_end, doc='Connector.end reference')
+
+    def on_interface_name(self, event):
+        """
+        Callback used, when interface name changes (interface is referenced
+        by `ConnectorItem.end.role`).
+        """
+        if event and event.element:
+            self._interface.text = event.element.name
+            self.request_update(matrix=False)
+
 
     def draw_tail(self, context):
         cr = context.cairo
