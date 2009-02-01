e0eed1642b262dd80dd34c60164a5857006ea2c2
wrobell <wrobell@pld-linux.org>
2009-02-01 16:09:12 +0000

    - Slot.value is a collection, so use it properly by accessing first item,   which is ok as we store only one value there so far
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2689 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/classes/feature.py
+++ gaphor/diagram/classes/feature.py
@@ -138,7 +138,7 @@ class SlotItem(FeatureItem):
 
     def _render_slot(self):
         slot = self.subject
-        return '%s = "%s"' % (slot.definingFeature.name, slot.value.value)
+        return '%s = "%s"' % (slot.definingFeature.name, slot.value[0].value)
 
     def pre_update(self, context):
         self.update_size(self._render_slot(), context)
