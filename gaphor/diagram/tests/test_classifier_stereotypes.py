bd826898e4595afd53bf55f85bd0da69c2cd245e
wrobell <wrobell@pld-linux.org>
2009-02-02 02:15:41 +0000

    - test loading applied stereotypes attributes from file
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2690 a8418922-720d-0410-834f-a69b97ada669

--- gaphor/diagram/tests/test_classifier_stereotypes.py
+++ gaphor/diagram/tests/test_classifier_stereotypes.py
@@ -25,6 +25,13 @@ class StereotypesAttributesTestCase(TestCase):
         attr = factory.create(UML.Property)
         attr.name = 'st1_attr_1'
         st1.ownedAttribute = attr
+        attr = factory.create(UML.Property)
+        attr.name = 'st1_attr_2'
+        st1.ownedAttribute = attr
+
+        attr = factory.create(UML.Property)
+        attr.name = 'st2_attr_1'
+        st2.ownedAttribute = attr
 
         UML.model.extend_with_stereotype(factory, cls, st1)
         UML.model.extend_with_stereotype(factory, cls, st2)
@@ -100,5 +107,46 @@ class StereotypesAttributesTestCase(TestCase):
         UML.model.remove_stereotype(c.subject, self.st1)
         self.assertEquals(0, len(c._compartments))
 
+    
+    def test_saving_stereotype_attributes(self):
+        """Test stereotype attributes saving
+        """
+        factory = self.element_factory
+        c = self.create(ComponentItem, UML.Component)
+
+        c.show_stereotypes_attrs = True
+
+        UML.model.apply_stereotype(factory, c.subject, self.st1)
+        UML.model.apply_stereotype(factory, c.subject, self.st2)
+
+        attr1, attr2 = self.st1.ownedAttribute
+        obj = c.subject.appliedStereotype[0]
+        slot = UML.model.add_slot(self.element_factory, obj, attr1)
+        slot.value[0].value = 'st1 test1'
+        slot = UML.model.add_slot(self.element_factory, obj, attr2)
+        slot.value[0].value = 'st1 test2'
+
+        data = self.save()
+        self.load(data)
+
+        item = self.diagram.canvas.select(lambda e: isinstance(e, ComponentItem))[0]
+        el = item.subject
+        self.assertEquals(2, len(el.appliedStereotype))
+
+        # check if stereotypes are properly applied
+        names = sorted(obj.classifier[0].name for obj in el.appliedStereotype)
+        self.assertEquals(['st1', 'st2'], names)
+
+        # two attributes were changed for stereotype st1, so 2 slots
+        obj = el.appliedStereotype[0]
+        self.assertEquals(2, len(obj.slot))
+        self.assertEquals('st1_attr_1', obj.slot[0].definingFeature.name)
+        self.assertEquals('st1 test1', obj.slot[0].value[0].value)
+        self.assertEquals('st1_attr_2', obj.slot[1].definingFeature.name)
+        self.assertEquals('st1 test2', obj.slot[1].value[0].value)
+
+        # no stereotype st2 attribute changes, no slots
+        obj = el.appliedStereotype[1]
+        self.assertEquals(0, len(obj.slot))
 
 # vim:sw=4:et
