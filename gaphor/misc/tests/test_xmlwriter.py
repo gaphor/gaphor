bd88f77497f3573fbdf0041c05b1705bf6bd0145
Arjan Molenaar <gaphor@gmail.com>
2009-02-05 19:58:15 +0000

    make xml writer more scm friendly
    git-svn-id: file:///Users/arjan/backup/gaphor/gaphor/trunk@2695 a8418922-720d-0410-834f-a69b97ada669

---
+++ gaphor/misc/tests/test_xmlwriter.py
@@ -0,0 +1,74 @@
+
+import sys
+import unittest
+from gaphor.misc.xmlwriter import XMLWriter
+
+class Writer:
+    def __init__(self):
+        self.s = ''
+
+    def write(self, text):
+        self.s += text
+
+
+class XMLWriterTestCase(unittest.TestCase):
+
+    def test_elements_1(self):
+        w = Writer()
+        xml_w = XMLWriter(w)
+        xml_w.startDocument()
+        xml_w.startElement('foo', {})
+        xml_w.endElement('foo')
+
+        xml = """<?xml version="1.0" encoding="%s"?>\n<foo/>""" % sys.getdefaultencoding()
+        assert w.s == xml, tuple(w.s)
+
+    def test_elements_2(self):
+        w = Writer()
+        xml_w = XMLWriter(w)
+        xml_w.startDocument()
+        xml_w.startElement('foo', {})
+        xml_w.startElement('bar', {})
+        xml_w.endElement('bar')
+        xml_w.endElement('foo')
+
+        xml = """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar/></foo>""" % sys.getdefaultencoding()
+        assert w.s == xml, w.s
+
+    def test_elements_test(self):
+        w = Writer()
+        xml_w = XMLWriter(w)
+        xml_w.startDocument()
+        xml_w.startElement('foo', {})
+        xml_w.startElement('bar', {})
+        xml_w.characters('hello')
+        xml_w.endElement('bar')
+        xml_w.endElement('foo')
+
+        xml = """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar>hello</bar></foo>""" % sys.getdefaultencoding()
+        assert w.s == xml, w.s
+
+    def test_elements_ns_default(self):
+        w = Writer()
+        xml_w = XMLWriter(w)
+        xml_w.startDocument()
+        xml_w.startPrefixMapping(None, 'http://gaphor.devjavu.com/schema')
+        xml_w.startElementNS(('http://gaphor.devjavu.com/schema', 'foo'), 'qn', {})
+        xml_w.endElementNS(('http://gaphor.devjavu.com/schema', 'foo'), 'qn')
+
+        xml = """<?xml version="1.0" encoding="%s"?>\n<foo xmlns="http://gaphor.devjavu.com/schema"/>""" % sys.getdefaultencoding()
+        assert w.s == xml, w.s
+
+    def test_elements_ns_1(self):
+        w = Writer()
+        xml_w = XMLWriter(w)
+        xml_w.startDocument()
+        xml_w.startPrefixMapping('g', 'http://gaphor.devjavu.com/schema')
+        xml_w.startElementNS(('http://gaphor.devjavu.com/schema', 'foo'), 'qn', {})
+        xml_w.endElementNS(('http://gaphor.devjavu.com/schema', 'foo'), 'qn')
+
+        xml = """<?xml version="1.0" encoding="%s"?>\n<g:foo xmlns:g="http://gaphor.devjavu.com/schema"/>""" % sys.getdefaultencoding()
+        assert w.s == xml, w.s
+
+
+# vim:sw=4:et:ai
--- gaphor/misc/xmlwriter.py
+++ gaphor/misc/xmlwriter.py
@@ -3,6 +3,7 @@
 import sys
 from xml.sax.saxutils import XMLGenerator
 
+
 class XMLWriter(XMLGenerator):
     """An XMLGenerator with CDATA handling extensions.
     """
@@ -10,25 +11,97 @@ class XMLWriter(XMLGenerator):
     def __init__(self, out=None):
         XMLGenerator.__init__(self, out, sys.getdefaultencoding())
         self._in_cdata = False
+        self._in_start_tag = False
 
-    def characters(self, content):
-        if self._in_cdata:
-            self._out.write(content.replace(']]>', '] ]>'))
-        else:
-            XMLGenerator.characters(self, content)
+    def _write(self, text, raw=False):
+        """
+        This write method is a bit smart about what it writes.
+        ``raw`` can be set to True to circumvent the smart behaviour.
+        """
+        # We have an end tag:
+        if not raw and self._in_start_tag and text.startswith('</'):
+            XMLGenerator._write(self, '/>')
+            self._in_start_tag = False
+            return
+           
+        # A normal start tag: remember we didn't write it
+        if not raw and text == '>':
+            self._in_start_tag = True
+            return
+
+        # close last start tag:
+        if self._in_start_tag:
+            XMLGenerator._write(self, '>')
+
+        # starting a new tag: add newlines
+        if not raw and text.startswith('<') and not text.startswith('</') and not text.startswith('<?') and not text.startswith('<%'):
+            XMLGenerator._write(self, '\n')
+            
+        self._in_start_tag = False
+        XMLGenerator._write(self, text)
 
     # Lexical handler methods
 
+    def startDocument(self):
+        self._write('<?xml version="1.0" encoding="%s"?>' %
+                        self._encoding)
+
+#    def startElement(self, name, attrs):
+#        self._write('\n')
+#        self._write('<' + name)
+#        for (name, value) in attrs.items():
+#            self._write(' %s=%s' % (name, quoteattr(value)))
+#        self._in_start_tag = True
+
+#    def endElement(self, name):
+#        if self._in_start_tag:
+#            self._in_start_tag = False
+#            self._write('/>')
+#        else:
+#            XMLGenerator.endElement(self, name)
+#        self._write('\n')
+ 
+#    def startElementNS(self, name, qname, attrs):
+#        self._write('\n')
+#        self._write('<' + self._qname(name))
+#
+#        for prefix, uri in self._undeclared_ns_maps:
+#            if prefix:
+#                self._out.write(' xmlns:%s="%s"' % (prefix, uri))
+#            else:
+#                self._out.write(' xmlns="%s"' % uri)
+#        self._undeclared_ns_maps = []
+#
+#        for (name, value) in attrs.items():
+#            self._write(' %s=%s' % (self._qname(name), quoteattr(value)))
+#        self._in_start_tag = True
+
+#    def endElementNS(self, name, qname):
+#        """
+#        Like XMLGenerator.endElementNS(), but adds newline at end of tag.
+#        """
+#        if self._in_start_tag:
+#            self._in_start_tag = False
+#            self._write('/>')
+#        else:
+#            XMLGenerator.endElementNS(self, name, qname)
+
+    def characters(self, content):
+        if self._in_cdata:
+            self._write(content.replace(']]>', '] ]>'), raw=True)
+        else:
+            XMLGenerator.characters(self, content)
+
     def comment(self, comment):
-        self._out.write('<!-- ')
-        self._out.write(comment.replace('-->', '- ->'))
-        self._out.write(' -->')
+        self._write('<!-- ')
+        self._write(comment.replace('-->', '- ->'), raw=True)
+        self._write(' -->')
 
     def startCDATA(self):
-        self._out.write('<![CDATA[')
+        self._write('<![CDATA[')
         self._in_cdata = True
 
     def endCDATA(self):
-        self._out.write(']]>')
+        self._write(']]>')
         self._in_cdata = False
 
