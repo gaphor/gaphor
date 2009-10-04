88c6e02863b6d7cf7b9edbaaff134d8a10689887
wrobell <wrobell@pld-linux.org>
2009-10-04 07:58:03 +0100

    - code cleanup

--- gaphor/adapters/grouping.py
+++ gaphor/adapters/grouping.py
@@ -3,63 +3,37 @@ from zope import interface, component
 from gaphor.diagram import items
 from gaphor.diagram.interfaces import IGroup
 
-
-###class AdapterMetaclass(type):
-###    """
-###    Adapter metaclass.
-###    """
-###    def __new__(self, name, bases, data):
-###        """
-###        Perform class interface adaptation.
-###        """
-###
-###        import sys
-###        frame = sys._getframe(1)
-###        locals = frame.f_locals
-###
-###        cls = type.__new__(self, name, bases, data)
-###        if '__adapts__' in data:
-###            a, b = data['__adapts__']
-###            cls.adapts(a, b)
-###            component.provideAdapter(cls)
-###
-###        return cls
-###
-###
-###    def adapts(self, a, b):
-###        """
-###        Stolen from zope as component.adapts prevents adapting on metaclass
-###        level.
-###        """
-###        import sys
-###        frame = sys._getframe(1)
-###        locals = frame.f_locals
-###        locals['__component_adapts__'] = component._adapts_descr(a, b)
-
-
 class AbstractGroup(object):
     """
-    Base class for grouping of UML objects.
+    Base class for grouping UML objects.
 
-    Attributes:
-     - parent: parent item
-     - item:   item to be part of parent item
+    :Attributes:
+     parent
+        Parent item, which groups other items.
+     item
+        Item to be groupped.
     """
-
-###    __metaclass__ = AdapterMetaclass
-
     interface.implements(IGroup)
 
     def __init__(self, parent, item):
         self.parent = parent
         self.item = item
 
+
+    def pre_can_contain(self):
+        raise NotImplemented, 'This is abstract method'
+
+
     def can_contain(self):
-        raise NotImplemented, 'This is abstract class'
+        raise NotImplemented, 'This is abstract method'
 
 
     def group(self):
-        raise NotImplemented, 'This is abstract class'
+        raise NotImplemented, 'This is abstract method'
+
+
+    def ungroup(self):
+        raise NotImplemented, 'This is abstract method'
 
 
 
@@ -69,19 +43,19 @@ class InteractionLifelineGroup(AbstractGroup):
     Add lifeline to interaction.
     """
 
-###    __adapts__ = items.InteractionItem, items.LifelineItem
     def pre_can_contain(self):
         return isinstance(self.parent, items.InteractionItem) \
                 and issubclass(self.item, items.LifelineItem)
+
 
     def can_contain(self):
         return isinstance(self.parent, items.InteractionItem) \
                 and isinstance(self.item, items.LifelineItem)
 
+
     def group(self):
         self.parent.subject.lifeline = self.item.subject
         self.parent.canvas.reparent(self.item, self.parent)
-        #self.parent.add_item(self.item)
 
 
     def ungroup(self):
@@ -92,6 +66,7 @@ component.provideAdapter(factory=InteractionLifelineGroup,
         adapts=(items.InteractionItem, DiagramItemMeta))
 component.provideAdapter(factory=InteractionLifelineGroup,
         adapts=(items.InteractionItem, items.LifelineItem))
+
 
 
 class NodeGroup(AbstractGroup):
@@ -114,13 +89,8 @@ class NodeGroup(AbstractGroup):
     def ungroup(self):
         del self.parent.subject.nestedNode[self.item.subject]
 
+
 component.provideAdapter(factory=NodeGroup,
         adapts=(items.NodeItem, DiagramItemMeta))
 component.provideAdapter(factory=NodeGroup,
         adapts=(items.NodeItem, items.NodeItem))
-
-
-class ComponentClassGroup(AbstractGroup):
-    """
-    Add class to component.
-    """
