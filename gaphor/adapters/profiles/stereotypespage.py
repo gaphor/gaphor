541d28cd7c9e1c51cc0a5d36983526e7a5ced962
Arjan Molenaar <gaphor@gmail.com>
2009-12-26 00:10:21 +0100

    Move stereotype selection to tree view.

--- gaphor/adapters/profiles/stereotypespage.py
+++ gaphor/adapters/profiles/stereotypespage.py
@@ -8,8 +8,7 @@ from gaphor.ui.interfaces import IPropertyPage
 from gaphor.diagram import items
 from zope import interface, component
 from gaphor import UML
-from gaphor.adapters.propertypages import create_tree_view
-
+from gaphor.adapters.propertypages import on_text_cell_edited, on_bool_cell_edited
 
 class StereotypeAttributes(gtk.TreeStore):
     """
@@ -19,7 +18,7 @@ class StereotypeAttributes(gtk.TreeStore):
     element_factory = inject('element_factory')
 
     def __init__(self, item):
-        gtk.TreeStore.__init__(self, str, str, object, object, object)
+        gtk.TreeStore.__init__(self, str, str, bool, object, object, object)
         self.item = item
         self.refresh()
 
@@ -35,12 +34,13 @@ class StereotypeAttributes(gtk.TreeStore):
                 slots[slot.definingFeature] = slot
 
         for st, obj in zip(applied, instances):
-            parent = self.append(None, (st.name, '', st, None, None))
+            is_applied = False
+            parent = self.append(None, (st.name, '', is_applied, st, None, None))
             for attr in st.ownedAttribute:
                 if not attr.association:
                     slot = slots.get(attr)
                     value = slot.value.value if slot is not None else ''
-                    data = (attr.name, value, attr, obj, slot)
+                    data = (attr.name, value, False, attr, obj, slot)
                     self.append(parent, data)
 
 
@@ -50,12 +50,13 @@ class StereotypeAttributes(gtk.TreeStore):
 
         Slot is created if instance Create valuChange value of instance spe
         """
+        print 'editing col', col
         if col != 1: # only editing of 2nd column is allowed
             return
 
         path = self.get_path(iter)
         row = self[path]
-        name, old_value, attr, obj, slot = row
+        name, old_value, is_applied, attr, obj, slot = row
         if isinstance(attr, UML.Stereotype):
             return # don't edit stereotype rows
 
@@ -78,8 +79,50 @@ class StereotypeAttributes(gtk.TreeStore):
             value = ''
 
         row[1] = value
-        row[4] = slot
+        row[5] = slot
         log.debug('slots %s' % obj.slot)
+
+
+def create_stereotype_tree_view(model):
+    """
+    Create a tree view for a editable tree model.
+
+    :Parameters:
+     model
+        Model, for which tree view is created.
+    """
+    tree_view = gtk.TreeView(model)
+    tree_view.set_rules_hint(True)
+    
+    # Stereotype/Attributes
+    col = gtk.TreeViewColumn('%s / %s' % (_('Stereotype'), _('Attribute')))
+    col.set_expand(True)
+    renderer = gtk.CellRendererToggle()
+    renderer.set_property('activatable', True)
+    renderer.connect('toggled', on_bool_cell_edited, model, 0)
+    col.pack_start(renderer, 0)
+
+    renderer = gtk.CellRendererText()
+    renderer.set_property('editable', False)
+    renderer.set_property('is-expanded', True)
+    col.pack_start(renderer, 0)
+    col.add_attribute(renderer, 'text', 0)
+    tree_view.append_column(col)
+
+    # TODO: use col.set_cell_data_func(renderer, func, None) to toggle visibility
+    # Value
+    renderer = gtk.CellRendererText()
+    renderer.set_property('editable', True)
+    renderer.set_property('is-expanded', True)
+    renderer.connect('edited', on_text_cell_edited, model, 1)
+    col = gtk.TreeViewColumn(_('Value'), renderer, text=1)
+    col.set_expand(True)
+    tree_view.append_column(col)
+
+    #tree_view.connect('key_press_event', remove_on_keypress)
+    #tree_view.connect('key_press_event', swap_on_keypress)
+
+    return tree_view
 
 
 
@@ -127,8 +170,7 @@ class StereotypePage(object):
 
         # stereotype attributes
         self.model = StereotypeAttributes(self.context)
-        t = '%s / %s' % (_('Stereotype'), _('Attribute'))
-        tree_view = create_tree_view(self.model, (t, _('Value')), ro_cols=(0,))
+        tree_view = create_stereotype_tree_view(self.model)
         page.pack_start(tree_view)
 
         page.show_all()
