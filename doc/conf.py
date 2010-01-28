c5cc011c08dd9dfce0f1bf2f228b8df55c58a0f6
Arjan Molenaar <gaphor@gmail.com>
2010-01-28 08:21:40 +0100

    Updated quite a bit of documentation. Everything generates (abeit wiki markup).

---
+++ doc/conf.py
@@ -0,0 +1,8 @@
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo']
+project = 'Gaphor'
+version = '0.15'
+release = version
+source_suffix = '.txt'
+master_doc = 'contents'
+#html_theme = 'traditional'
+#unused_docs = 'constraints.txt index.txt tools.txt comparison.txt'
