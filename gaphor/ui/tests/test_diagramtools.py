c6a2b98eb5ef5ea8f7b4a76abd73df62ad1a0370
Arjan Molenaar <gaphor@gmail.com>
2010-03-01 17:43:15 +0100

    Added diagramtools test case.

---
+++ gaphor/ui/tests/test_diagramtools.py
@@ -0,0 +1,56 @@
+
+import gtk
+import logging
+from gaphor.tests import TestCase
+from gaphor import UML
+from gaphor.diagram import items
+from gaphas.canvas import Context
+
+Event = Context
+
+logging.basicConfig(level=logging.DEBUG)
+
+class DiagramItemConnectorTestCase(TestCase):
+    services = TestCase.services + [ 'gui_manager', 'action_manager', 'properties' ]
+
+    def setUp(self):
+        super(DiagramItemConnectorTestCase, self).setUp()
+        gm = self.get_service('gui_manager')
+        gm.main_window.show_diagram(self.diagram)
+        self.main_window = gm.main_window
+
+    def test_item_reconnect(self):
+        # Setting the stage:
+	ci1 = self.create(items.ClassItem, UML.Class)
+        ci2 = self.create(items.ClassItem, UML.Class)
+        a = self.create(items.AssociationItem)
+
+        self.connect(a, a.head, ci1)
+        self.connect(a, a.tail, ci2)
+
+        self.assertTrue(a.subject)
+        self.assertTrue(a.head_end.subject)
+        self.assertTrue(a.tail_end.subject)
+
+        the_association = a.subject
+
+        # The act: perform button press event and button release
+        view = self.main_window.get_current_diagram_view()
+        
+        self.assertSame(self.diagram.canvas, view.canvas)
+
+        p = view.get_matrix_i2v(a).transform_point(*a.head.pos)
+
+        event = Event(x=p[0], y=p[1], type=gtk.gdk.BUTTON_PRESS, state=0)
+
+        view.do_event(event)
+
+        self.assertSame(the_association, a.subject)
+
+        event = Event(x=p[0], y=p[1], type=gtk.gdk.BUTTON_RELEASE, state=0)
+
+        view.do_event(event)
+
+        self.assertSame(the_association, a.subject)
+
+# vim:sw=4:et:ai
