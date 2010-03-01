8be512e6da5b9947ac75fceb1a8db8af1c7b7659
Arjan Molenaar <gaphor@gmail.com>
2010-03-01 18:30:10 +0100

    Added extra alignment functions provided by Marc-André Moreau.

--- gaphor/plugins/alignment/__init__.py
+++ gaphor/plugins/alignment/__init__.py
@@ -19,9 +19,14 @@ class Alignment(object):
         <menubar action="mainwindow">
           <menu action="diagram">
             <placeholder name="ternary">
-              <separator />
-              <menuitem action="align-horizontal" />
-              <menuitem action="align-vertical" />
+		<separator />
+		<menuitem action="align-left" />
+		<menuitem action="align-center" />
+		<menuitem action="align-right" />
+		<separator />
+		<menuitem action="align-top" />
+		<menuitem action="align-middle" />
+		<menuitem action="align-bottom" />
             </placeholder>
           </menu>
         </menubar>
@@ -42,48 +47,98 @@ class Alignment(object):
     def update(self, event=None):
         self._last_update = event
         sensitive = event and len(event.selected_items) > 1
-        self.action_group.get_action('align-horizontal').set_sensitive(sensitive)
-        self.action_group.get_action('align-vertical').set_sensitive(sensitive)
+	self.action_group.get_action('align-left').set_sensitive(sensitive)
+	self.action_group.get_action('align-center').set_sensitive(sensitive)
+	self.action_group.get_action('align-right').set_sensitive(sensitive)
+	self.action_group.get_action('align-top').set_sensitive(sensitive)
+	self.action_group.get_action('align-middle').set_sensitive(sensitive)
+	self.action_group.get_action('align-bottom').set_sensitive(sensitive)
 
     def get_items(self):
         return (self._last_update and self._last_update.selected_items) or []
-        
-    def moveItemsToTarget(self, items, target_x, target_y):
-        for item in items:
-            if target_x is not None:
-                x = target_x - item.matrix[4]
-            else:
-                x = 0
-            if target_y is not None:
-                y = target_y - item.matrix[5]
-            else:
-                y = 0
-            item.matrix.translate(x,y)
-            item.request_update()
             
-    def getXCoords(self, items):
+    def getXCoordsLeft(self, items):
         return [item.matrix[4] for item in items]
+	
+    def getXCoordsRight(self, items):
+        return [item.matrix[4] + item.width for item in items]
 
-    @action(name='align-vertical', label='Align vertical',
-            tooltip="Align diagram elements vertically")
-    @transactional
-    def align_vertical(self):
-        items = self.get_items()
-        target_x=min(self.getXCoords(items))
-        target_y=None
-        self.moveItemsToTarget(items, target_x, target_y)
-
-    def getYCoords(self, items):
+    def getYCoordsTop(self, items):
         return [item.matrix[5] for item in items]
+	
+    def getYCoordsBottom(self, items):
+        return [item.matrix[5] + item.height for item in items]
 
-    @action(name='align-horizontal', label='Align horizontal',
-            tooltip="Align diagram elements horizontally")
+    @action(name='align-left', label='Left',
+            tooltip="Vertically align diagram elements on the left")
     @transactional
-    def align_horizontal(self):
+    def align_left(self):
         items = self.get_items()
-        target_y = min(self.getYCoords(items))
-        target_x = None
-        self.moveItemsToTarget(items, target_x, target_y)
+        target_x=min(self.getXCoordsLeft(items))
+        for item in items:
+            x = target_x - item.matrix[4]
+            item.matrix.translate(x,0)
+            item.request_update()
+	
+    @action(name='align-center', label='Center',
+            tooltip="Vertically align diagram elements on their centers")
+    @transactional
+    def align_center(self):
+        items = self.get_items()
+        min_x=min(self.getXCoordsLeft(items))
+	max_x=max(self.getXCoordsRight(items))
+	center_x = max_x - min_x
+        for item in items:
+            x = center_x - (item.width / 2) - item.matrix[4]
+            item.matrix.translate(x,0)
+            item.request_update()
+	
+    @action(name='align-right', label='Right',
+            tooltip="Vertically align diagram elements on the right")
+    @transactional
+    def align_right(self):
+        items = self.get_items()
+        target_x=max(self.getXCoordsRight(items))
+        for item in items:
+            x = target_x - item.width - item.matrix[4]
+            item.matrix.translate(x,0)
+            item.request_update()
+
+    @action(name='align-top', label='Top',
+            tooltip="Horizontally align diagram elements on their tops")
+    @transactional
+    def align_top(self):
+        items = self.get_items()
+        target_y = min(self.getYCoordsTop(items))
+        for item in items:
+            y = target_y - item.matrix[5]
+            item.matrix.translate(0,y)
+            item.request_update()
+	    
+    @action(name='align-middle', label='Middle',
+            tooltip="Horizontally align diagram elements on their middles")
+    @transactional
+    def align_middle(self):
+        items = self.get_items()
+        min_y = min(self.getYCoordsTop(items))
+	max_y = max(self.getYCoordsBottom(items))
+	middle_y = max_y - min_y
+        for item in items:
+	    y = middle_y - (item.height / 2) - item.matrix[5]
+            item.matrix.translate(0,y)
+            item.request_update()
+	    
+    @action(name='align-bottom', label='Bottom',
+            tooltip="Horizontally align diagram elements on their bottoms")
+    @transactional
+    def align_bottom(self):
+        items = self.get_items()
+        target_y = min(self.getYCoordsBottom(items))
+        for item in items:
+            y = target_y - item.height - item.matrix[5]
+            item.matrix.translate(0,y)
+            item.request_update()
+	
 
 
 # vim:sw=4:et:ai
