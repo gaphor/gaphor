4b69eb526e6bbdd9fae619d8f083a3a32243f5a7
adamboduch <adam.boduch@gmail.com>
2010-11-29 00:57:46 -0500

    Added some doc strings to the properties service.

--- gaphor/services/properties.py
+++ gaphor/services/properties.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
-"""
-"""
+"""The properties module allows Gaphor properties to be saved to the local
+file system.  These are things like preferences."""
 
 import sys
 import os
@@ -12,6 +11,8 @@ from gaphas.decorators import async
 from gaphor.misc import get_user_data_dir
 
 class IPropertyChangeEvent(interface.Interface):
+    
+    """A property changed event has a name, an old value, and a new value."""
 
     name = interface.Attribute("The property name")
     old_value = interface.Attribute("The property value before the change")
@@ -19,6 +20,10 @@ class IPropertyChangeEvent(interface.Interface):
 
 
 class PropertyChangeEvent(object):
+    
+    """This event is triggered any time a property is changed.  This event
+    holds the property name, the current value, and the new value."""
+    
     interface.implements(IPropertyChangeEvent)
 
     def __init__(self, name, old_value, new_value):
@@ -26,37 +31,51 @@ class PropertyChangeEvent(object):
         self.old_value = old_value
         self.new_value = new_value
 
-
 _no_default = object()
 
-
 class Properties(object):
-    """
-    The Properties class holds a collection of application wide properties.
+    """The Properties class holds a collection of application wide properties.
 
-    Properties are persisted
-    """
+    Properties are persisted to the local file system."""
     interface.implements(IService)
 
     def __init__(self, backend=None):
+        """Constructor.  Initialize the Gaphor application object, the
+        dictionary for storing properties in memory, and the storage backend.
+        This defaults to FileBackend"""
+        
         self._app = None
         self._resources = {}
         self._backend = backend or FileBackend()
 
     def init(self, app):
+        """Initialize the properties service.  This will load any stored 
+        properties from the file system."""
+        
         self._app = app
         self._backend.load(self._resources)
     
     def shutdown(self):
+        """Shutdown the properties service.  This will ensure that all 
+        properties are saved."""
+        
         self._backend.save(self._resources)
 
     def __call__(self, key, default=_no_default):
+        """Retrieve the specified property.  If the property doesn't exist,
+        the default parameter is returned.  This defaults to _no_default."""
+        
         return self.get(key, default)
 
     def save(self):
+        """Save all properties by calling save() on the properties storage
+        backend."""
+        
         self._backend.save(self._resources)
 
     def _items(self):
+        """Return an iterator for all stored properties."""
+        
         return self._resources.iteritems()
 
     def dump(self, stream=sys.stdout):
@@ -67,12 +86,11 @@ class Properties(object):
         pprint.pprint(self._resources.items(), stream)
 
     def get(self, key, default=_no_default):
-        """
-        Locate a property.
+        """Locate a property.
 
         Resource should be the class of the resource to look for or a string. In
-        case of a string the resource will be looked up in the GConf configuration.
-        """
+        case of a string the resource will be looked up in the GConf
+        configuration."""
         
         try:
             return self._resources[key]
@@ -84,41 +102,46 @@ class Properties(object):
             return default
 
     def set(self, key, value):
-        """
-        Set a property to a specific value.
+        """Set a property to a specific value.
 
         No smart things are done with classes and class names (like the
-        resource() method does).
-        """
+        resource() method does)."""
+        
         resources = self._resources
         old_value = resources.get(key)
+        
         if value != old_value:
             resources[key] = value
             self._app.handle(PropertyChangeEvent(key, old_value, value))
             self._backend.update(resources, key, value)
 
-
 class FileBackend(object):
-    """
-    Resource backend that stores data to a resource file
-    ($HOME/.gaphor/resource).
-    """
+    """Resource backend that stores data to a resource file
+    ($HOME/.gaphor/resource)."""
+    
     RESOURCE_FILE='resources'
    
     def __init__(self, datadir=get_user_data_dir()):
+        """Constructor.  Initialize the directory used for storing 
+        properties."""
+        
         self.datadir = datadir
 
     def get_filename(self, create=False):
+        """Return the current file used to store Gaphor properties.  If the
+        created parameter is set to True, the file is created if it doesn't 
+        exist.  This defaults to False."""
+        
         datadir = self.datadir
+        
         if create and not os.path.exists(datadir):
             os.mkdir(datadir)
+        
         return os.path.join(datadir, self.RESOURCE_FILE)
 
     def load(self, resource):
-        """
-        Load resources from a file. Resources are saved like you do with
-        a dict().
-        """
+        """Load resources from a file. Resources are saved like you do with
+        a dict()."""
         
         filename = self.get_filename()
         
@@ -131,8 +154,7 @@ class FileBackend(object):
                 resource[key] = value
 
     def save(self, resource):
-        """
-        Save persist resources from the resources dictionary.
+        """Save persist resources from the resources dictionary.
         @resource is the Resource instance
         @persistent is a list of persistent resource names.
         """
@@ -144,7 +166,6 @@ class FileBackend(object):
 
     @async(single=True, timeout=500)
     def update(self, resource, key, value):
+        """Update the properties file with any changes in the background."""
+        
         self.save(resource)
-
-
-# vim:sw=4:et:ai
