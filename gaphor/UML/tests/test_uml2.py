edd7d3966e0618c8278b6dbe33a7f074b56c1e23
adamboduch <adam.boduch@gmail.com>
2011-03-02 16:38:15 -0500

    Improved the Dependency unit test.

--- gaphor/UML/tests/test_uml2.py
+++ gaphor/UML/tests/test_uml2.py
@@ -123,6 +123,8 @@ class ClassesTestCase(unittest.TestCase):
         
     def test_dependency(self):
         
+        """Testing Dependency elements in the meta-model"""
+        
         try:
             
             element = self.factory.create(UML.Dependency)
@@ -130,6 +132,15 @@ class ClassesTestCase(unittest.TestCase):
         except AttributeError:
             
             self.fail('Dependency elements are not part of the meta-model')
+            
+        client = self.factory.create(UML.Package)
+        supplier = self.factory.create(UML.Package)
+        
+        element.client = client
+        element.supplier = supplier
+        
+        self.assertTrue(client in element.source, 'DirectedRelationship.source does not contain client - %s' % element.client)
+        self.assertTrue(supplier in element.target, 'DirectedRelationship.target does not contain supplier - %s' % element.supplier)
         
     def test_element_import(self):
         
