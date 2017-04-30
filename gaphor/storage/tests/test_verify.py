0698ddb360b9eda719f10f16b68c6b8b0b785b10
danyeaw <danyeaw@gmail.com>
2017-04-29 20:38:59 -0400

    Import updates to change UML to uml2

--- gaphor/storage/tests/test_verify.py
+++ gaphor/storage/tests/test_verify.py
@@ -2,22 +2,22 @@
 from __future__ import absolute_import
 from gaphor.tests.testcase import TestCase
 from gaphor.storage.verify import orphan_references
-from gaphor import UML
+from gaphor.UML import uml2
 
 
 class VerifyTestCase(TestCase):
 
     def test_verifier(self):
         factory = self.element_factory
-        c = factory.create(UML.Class)
-        p = factory.create(UML.Property)
+        c = factory.create(uml2.Class)
+        p = factory.create(uml2.Property)
         c.ownedAttribute = p
 
         assert not orphan_references(factory)
 
         # Now create a separate item, not part of the factory:
 
-        m = UML.Comment(id="acd123")
+        m = uml2.Comment(id="acd123")
         m.annotatedElement = c
         assert m in c.ownedComment
 
