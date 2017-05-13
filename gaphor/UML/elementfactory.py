210a1cf47319edafa80e41b097a1f8ce33b44902
danyeaw <danyeaw@gmail.com>
2017-05-12 22:07:22 -0400

    Fixed license preamble header.

--- gaphor/UML/elementfactory.py
+++ gaphor/UML/elementfactory.py
@@ -19,20 +19,6 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # Gaphor.  If not, see <http://www.gnu.org/licenses/>.
-
-# Gaphor is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# Gaphor is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Gaphor.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 Factory for and registration of model elements.
 """
