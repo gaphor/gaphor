385d249b7a29dc8b7c97bef6bd6e773519b20b38
Dan Yeaw <dan@yeaw.me>
2018-01-13 18:33:49 -0500

    Correct wrong short license on each source file
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/adapters/profiles/tests/test_metaclasseditor.py
+++ gaphor/adapters/profiles/tests/test_metaclasseditor.py
@@ -6,16 +6,17 @@
 # This file is part of Gaphor.
 #
 # Gaphor is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 2 of the License, or (at your option) any later
-# version.
+# terms of the GNU Library General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
 #
 # Gaphor is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License 
+# more details.
 #
-# You should have received a copy of the GNU General Public License along with
-# Gaphor.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Library General Public 
+# along with Gaphor.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import
 from gaphor.tests import TestCase
 from gaphor.adapters.profiles.metaclasseditor import MetaclassNameEditor
