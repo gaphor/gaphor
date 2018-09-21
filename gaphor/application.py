62d767515f1acefa456b94e81e47c0d6316c61a4
Dan Yeaw <dan@yeaw.me>
2018-09-21 14:08:46 -0400

    Futurize: Create iterator of dictionary keys
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/application.py
+++ gaphor/application.py
@@ -82,7 +82,7 @@ class _Application(object):
         for name in self.essential_services:
             self.init_service(name)
         while self._uninitialized_services:
-            self.init_service(self._uninitialized_services.iterkeys().next())
+            self.init_service(iter(self._uninitialized_services.keys()).next())
 
     def init_service(self, name):
         """
