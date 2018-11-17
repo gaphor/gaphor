a2736b4c4c4d6d004a7d055e7e9f0436a7be5b3d
Dan Yeaw <dan@yeaw.me>
2018-11-17 15:37:52 -0500

    Fix * imports for building UML with Python3
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/UML/__init__.py
+++ gaphor/UML/__init__.py
@@ -1,10 +1,6 @@
-from gaphor.UML.collection import collection
-
-from gaphor.UML.uml2 import *
-from gaphor.UML.elementfactory import ElementFactory
 from gaphor.UML import modelfactory as model
+from gaphor.UML.collection import collection
+from gaphor.UML.elementfactory import ElementFactory
+from gaphor.UML.uml2 import *
 from gaphor.UML.umlfmt import format
 from gaphor.UML.umllex import parse
-
-__all__ = ['collection', 'context', 'diagram', 'element', 'elementfactory', 'event', 'interface', 'modelfactory',
-           'properties', 'uml2', 'umlfmt', 'umllex']
