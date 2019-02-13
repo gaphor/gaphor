717d19868b90e06598dd51ee0d6e77984daf8c65
Dan Yeaw <dan@yeaw.me>
2019-02-12 21:30:59 -0500

    Fix AttributeError when hitting F2 with no elements selected
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/ui/mainwindow.py
+++ gaphor/ui/mainwindow.py
@@ -554,13 +554,14 @@ class Namespace(object):
     def tree_view_rename_selected(self):
         view = self._namespace
         element = view.get_selected_element()
-        path = view.get_model().path_from_element(element)
-        column = view.get_column(0)
-        cell = column.get_cells()[1]
-        cell.set_property("editable", 1)
-        cell.set_property("text", element.name)
-        view.set_cursor(path, column, True)
-        cell.set_property("editable", 0)
+        if element is not None:
+            path = view.get_model().path_from_element(element)
+            column = view.get_column(0)
+            cell = column.get_cells()[1]
+            cell.set_property("editable", 1)
+            cell.set_property("text", element.name)
+            view.set_cursor(path, column, True)
+            cell.set_property("editable", 0)
 
     @action(
         name="tree-view-create-diagram",
