f1a27bfa7e02c4441233d71b97006658e939cfed
Arjan Molenaar <gaphor@gmail.com>
2019-03-03 21:56:41 +0100

    Restore test_gen_uml to its former glory

--- tests/test_gen_uml.py
+++ tests/test_gen_uml.py
@@ -41,14 +41,16 @@ class Element(object): pass
 class SubClass(Element): pass
 class C(object): pass
 class D(C): pass
-C.attr = attribute('attr', 8cb780ba-3f11-11de-9595-00224128e79d, default=8cb7fd1a-3f11-11de-9595-00224128e79d, lower=8cb7c11a-3f11-11de-9595-00224128e79d, upper=8cb7df60-3f11-11de-9595-00224128e79d)
-C.name1 = association('name1', SubClass, lower=602cb072-3bcb-11de-ac7f-00224128e79d, opposite='name2')
-SubClass.name2 = association('name2', C, lower=602d56c6-3bcb-11de-ac7f-00224128e79d, opposite='name1')
-C.base = association('base', SubClass, lower=e053585e-3bcc-11de-aa0c-00224128e79d, opposite='abstract')
-D.subbase = association('subbase', SubClass, lower=f8d56502-3bcc-11de-aa0c-00224128e79d, opposite='concrete')
-SubClass.concrete = association('concrete', D, lower=f8d5c998-3bcc-11de-aa0c-00224128e79d, upper=1665b18a-3bcd-11de-aa0c-00224128e79d, opposite='subbase')
-D.name3 = association('name3', SubClass, lower=1af287dc-3bcd-11de-aa0c-00224128e79d, opposite='name4')
-SubClass.abstract = derivedunion('abstract', C, e053abd8-3bcc-11de-aa0c-00224128e79d, f48f64a2-3bcc-11de-aa0c-00224128e79d, SubClass.concrete)
+C.attr = attribute('attr', str)
+C.name1 = association('name1', SubClass, opposite='name2')
+SubClass.name2 = association('name2', C, opposite='name1')
+C.base = association('base', SubClass, opposite='abstract')
+D.subbase = association('subbase', SubClass, opposite='concrete')
+SubClass.concrete = association('concrete', D, opposite='subbase')
+D.name3 = association('name3', SubClass, opposite='name4')
+# 'SubClass.value' is a simple attribute
+SubClass.value = attribute('value', str)
+SubClass.abstract = derivedunion('abstract', C, 0, '*', SubClass.concrete)
 SubClass.name4 = redefine(SubClass, 'name4', D, name2)
 """
 
