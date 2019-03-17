56b3171c79f88eb5ba33e7e85f06da0b1556f8de
Dan Yeaw <dan@yeaw.me>
2019-03-17 12:20:07 -0400

    Fix gcc command not found as a command by installing base-devel
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- .azure/msys2-install.sh
+++ .azure/msys2-install.sh
@@ -7,6 +7,7 @@ export MSYS2_FC_CACHE_SKIP=1
 pacman --noconfirm -Suy
 
 pacman --noconfirm -S --needed \
+    base-devel \
     git \
     mingw-w64-$MSYS2_ARCH-gtk3 \
     mingw-w64-$MSYS2_ARCH-python3 \
