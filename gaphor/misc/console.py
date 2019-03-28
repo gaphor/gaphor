005f21f946b3aea6da5ca99ec754c78d76fc1ac0
Arjan Molenaar <gaphor@gmail.com>
2019-03-28 16:18:47 +0100

    Clean up gi.require_version statements
    This should only be defined at top-level.

--- gaphor/misc/console.py
+++ gaphor/misc/console.py
@@ -13,10 +13,10 @@ from rlcompleter import Completer
 
 if __name__ == '__main__':
     import gi
-    gi.require_version('Gdk', '3.0')
+    gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gdk
 from gi.repository import Gtk
+from gi.repository import Gdk
 from gi.repository import Pango
 
 
--- gaphor/plugins/checkmetamodel/checkmodelgui.py
+++ gaphor/plugins/checkmetamodel/checkmodelgui.py
@@ -6,7 +6,6 @@ import logging
 
 import gi
 
-gi.require_version("Gtk", "3.0")
 from gi.repository import GObject
 from gi.repository import Gtk
 from zope.interface import implementer
