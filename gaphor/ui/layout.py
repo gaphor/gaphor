e08025eef79ab5d494f5d797334986e815d65338
Arjan Molenaar <gaphor@gmail.com>
2019-03-30 18:32:55 +0100

    Remove LGPL copyright notice from layout.py
    Layout,py was copied and adapted from etk.docking. My fellow coder @dieterv is
    fine with this change.

--- gaphor/ui/layout.py
+++ gaphor/ui/layout.py
@@ -1,22 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Copyright © 2010 etkdocking Contributors
-#
-# This file is part of etkdocking.
-#
-# etkdocking is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# etkdocking is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with etkdocking. If not, see <http://www.gnu.org/licenses/>.
-
+"""
+Layout code from a simple XML description.
+"""
 
 from simplegeneric import generic
 from xml.etree.ElementTree import fromstring
