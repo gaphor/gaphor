bc00ee63d1417680a722a625adb473052bd13502
Arjan Molenaar <gaphor@gmail.com>
2019-04-12 10:02:52 +0200

    Only delete items in namespace that qualify

--- gaphor/ui/namespace.py
+++ gaphor/ui/namespace.py
@@ -410,10 +410,12 @@ class Namespace(object):
 
     @component.adapter(ElementDeleteEvent)
     def _on_element_delete(self, event):
-        iter = self.iter_for_element(event.element)
-        # iter should be here, unless we try to delete an element who's parent element is already deleted, so let's be lenient.
-        if iter:
-            self.model.remove(iter)
+        element = event.element
+        if type(element) in self.filter:
+            iter = self.iter_for_element(element)
+            # iter should be here, unless we try to delete an element who's parent element is already deleted, so let's be lenient.
+            if iter:
+                self.model.remove(iter)
 
     @component.adapter(DerivedSetEvent)
     def _on_association_set(self, event):
