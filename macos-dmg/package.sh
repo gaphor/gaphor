19f4339be5f076b1ed084c0f46c928c986dfb00e
Arjan Molenaar <gaphor@gmail.com>
2019-04-21 22:29:50 +0200

    Fix me a MacOS bundle packager

--- macos-dmg/package.sh
+++ macos-dmg/package.sh
@@ -4,6 +4,8 @@
 #
 # Thanks: http://stackoverflow.com/questions/1596945/building-osx-app-bundle
 
+set -euo pipefail
+
 # Also fix $INSTALLDIR/MacOS/gaphor in case this number changes
 APP=Gaphor.app
 INSTALLDIR=$APP/Contents
@@ -15,25 +17,71 @@ function log() {
   echo $* >&2
 }
 
+libffi_path="$(brew ls libffi | grep pkgconfig | xargs dirname)"
+echo "Adding libffi pkg-config path ${libffi_path} to \$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="${libffi_path}:${PKG_CONFIG_PATH:-}"
+
+rm -rf Gaphor.app Gaphor-*.dmg Gaphor-*-macos.zip
+
+mkdir -p ${INSTALLDIR}/MacOS
+mkdir -p ${INSTALLDIR}/Resources
+
+cp Info.plist ${INSTALLDIR}
+cp PkgInfo ${INSTALLDIR}
+cp gaphor ${INSTALLDIR}/MacOS
+cp gaphor.icns ${INSTALLDIR}/Resources
+
 log "Building venv for app bundle..."
 
 python3 -m venv --prompt Gaphor.app --copies ${INSTALLDIR}
 
 source ${INSTALLDIR}/bin/activate
 
-log "Installing Gaphor in ${INSTALLDIR}..."
+function rel_path {
+  echo $1 | sed 's#/usr/local/Cellar/[^/]*/[^/]*/##'
+}
 
-pip install ../dist/gaphor-*.tar.gz
+{
+  brew deps gtk+3
+  echo "python"
+  brew deps python
+} | sort | uniq -u |\
+while read dep
+do
+  log "Scanning Homebrew files for $dep"
+  brew list -v $dep
+done |\
+grep -v '^find ' |\
+while read f
+do
+  echo "$(rel_path $f) $f"
+done |\
+grep '^lib/\|share/gir-1.0\|share/locale' |\
+while read rf f
+do
+  log "Adding ${INSTALLDIR}/${rf}"
+  mkdir -p "${INSTALLDIR}/$(dirname $rf)"
+  test -L "$f" || cp $f "${INSTALLDIR}/${rf}"
+done
 
-# Check with Homebrew which version to pick
-cp /usr/local/lib/libgtk-3.0.dylib ${INSTALLDIR}/lib
+# grep -v '^[^/]* ' |\
+# grep -v '^share/doc/' |\
+# grep -v '^share/gtk-doc/' |\
+# grep -v '^share/gettext/' |\
+# grep -v '^share/emacs/' |\
+# grep -v '^share/man/' |\
+# grep -v '^include/' |\
+
+# # Check with Homebrew which version to pick
+# cp /usr/local/lib/libgtk-3.0.dylib ${INSTALLDIR}/lib
+# cp /usr/local/lib/libgobject-2.0.0.dylib ${INSTALLDIR}/lib
 
 
 # Modules, config, etc.
-for dir in etc/fonts etc/gtk-3.0 lib/gtk-3.0 lib/gdk-pixbuf-2.0 lib/girepository-1.0 share/gir-1.0 share/themes/Default/gtk-3.0; do
-  mkdir -p ${INSTALLDIR}/$dir
-  cp -r ${LOCALDIR}/$dir/* ${INSTALLDIR}/$dir
-done
+# for dir in etc/fonts etc/gtk-3.0 lib/gtk-3.0 lib/gdk-pixbuf-2.0 lib/girepository-1.0 share/gir-1.0 share/themes/Default/gtk-3.0; do
+#   mkdir -p ${INSTALLDIR}/$dir
+#   cp -r ${LOCALDIR}/$dir/* ${INSTALLDIR}/$dir
+# done
 
 
 # Somehow files are writen with mode 444
@@ -58,26 +106,35 @@ function fix_paths {
   done
 }
 
-binlibs=`find $INSTALLDIR -type f -name '*.so'`
-
-for lib in $binlibs; do
-  log Resolving $lib
-  resolve_deps $lib
+{
+  find $INSTALLDIR -type f -name '*.so'
+  find $INSTALLDIR -type f -name '*.dylib'
+} |\
+while read lib
+do
+  # log Resolving $lib
+  # resolve_deps $lib
   fix_paths $lib
-done | sort -u | while read lib; do
-  log Copying $lib
-  cp $lib $LIBDIR
-  chmod u+w $LIBDIR/`basename $lib`
-  fix_paths $LIBDIR/`basename $lib`
+# done | sort -u | while read lib; do
+#   log Copying $lib
+#   cp $lib $LIBDIR
+#   chmod u+w $LIBDIR/`basename $lib`
+#   fix_paths $LIBDIR/`basename $lib`
 done
 
-function fix_config {
-  local file=$1
-  local replace=$2
+# function fix_config {
+#   local file=$1
+#   local replace=$2
 
-  mv $file $file.orig
-  sed "$replace" $file.orig > $file
-}
+#   mv $file $file.orig
+#   sed "$replace" $file.orig > $file
+# }
+
+GDK_PIXBUF_MODULEDIR=${INSTALLDIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders gdk-pixbuf-query-loaders --update-cache
+
+log "Installing Gaphor in ${INSTALLDIR}..."
+
+pip install ../dist/gaphor-*.tar.gz
 
 # Fix config files
 
