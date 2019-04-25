d1c2b41414a22e54d82d2d3c00dd697adb271af5
Arjan Molenaar <gaphor@gmail.com>
2019-04-25 21:53:25 +0200

    Gaphor installed, but with dyld trouble

--- macos-dmg/package.sh
+++ macos-dmg/package.sh
@@ -8,10 +8,13 @@ set -euo pipefail
 
 # Also fix $INSTALLDIR/MacOS/gaphor in case this number changes
 APP=Gaphor.app
+VERSION="$(ls ../dist/gaphor-*.tar.gz | tail -1 | sed 's#^.*gaphor-\(.*\).tar.gz#\1#')"
 INSTALLDIR=$APP/Contents
 LIBDIR=$INSTALLDIR/lib
 
 LOCALDIR=/usr/local
+
+PYVER="$(python3 -c 'import sys; print("{}.{}".format(*sys.version_info))')"
 
 function log() {
   echo $* >&2
@@ -23,13 +26,14 @@ export PKG_CONFIG_PATH="${libffi_path}:${PKG_CONFIG_PATH:-}"
 
 rm -rf Gaphor.app Gaphor-*.dmg Gaphor-*-macos.zip
 
-mkdir -p ${INSTALLDIR}/MacOS
-mkdir -p ${INSTALLDIR}/Resources
+mkdir -p "${INSTALLDIR}/MacOS"
+mkdir -p "${INSTALLDIR}/Resources"
 
-cp Info.plist ${INSTALLDIR}
-cp PkgInfo ${INSTALLDIR}
-cp gaphor ${INSTALLDIR}/MacOS
-cp gaphor.icns ${INSTALLDIR}/Resources
+cp PkgInfo "${INSTALLDIR}"
+cp gaphor.icns "${INSTALLDIR}/Resources"
+cat Info.plist | sed 's#VERSION#'${VERSION}'#g' > "${INSTALLDIR}/Info.plist"
+cat gaphor | sed 's#3.7#'${PYVER}'#' > "${INSTALLDIR}/MacOS/gaphor"
+chmod +x "${INSTALLDIR}/MacOS/gaphor"
 
 function rel_path {
   echo $1 | sed 's#/usr/local/Cellar/[^/]*/[^/]*/##'
@@ -46,7 +50,8 @@ while read f
 do
   echo "$(rel_path $f) $f"
 done |\
-grep '^bin\|^lib/\|^share/gir-1.0/\|^share/locale/\|^Frameworks/' |\
+grep '^bin/gdk-pixbuf-query-loaders\|^lib/\|^share/gir-1.0/\|^share/locale/\|^Frameworks/' |\
+grep -v '^lib/.*\.a' |\
 while read rf f
 do
   # log "Adding ${INSTALLDIR}/${rf}"
@@ -56,6 +61,12 @@ done
 
 # Somehow files are writen with mode 444
 find $INSTALLDIR -type f -exec chmod u+w {} \;
+
+log "Installing Gaphor in ${INSTALLDIR}..."
+
+pip3 install --prefix "${INSTALLDIR}" --force-reinstall ../dist/gaphor-${VERSION}.tar.gz
+
+( cd "${INSTALLDIR}/bin" && ln -s "../Frameworks/Python.framework/Versions/${PYVER}/bin/python${PYVER}" .; )
 
 function resolve_deps {
   local lib=$1
@@ -76,11 +87,11 @@ function fix_paths {
     # @executable_path is /path/to/Gaphor.app/MacOS
     if [[ "$dep" =~ ^.*/Frameworks/.* ]]
     then
-      log ">> Framework: @executable_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#')"
-      install_name_tool -change $dep @executable_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#') $lib
+      log ">> Framework: @loader_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#')"
+      install_name_tool -change $dep @loader_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#') $lib
     else
-      log ">> Lib: @executable_path/../lib/$(basename $dep)"
-      install_name_tool -change $dep @executable_path/../lib/$(basename $dep) $lib
+      log ">> Lib: @loader_path/../lib/$(basename $dep)"
+      install_name_tool -change $dep @loader_path/../lib/$(basename $dep) $lib
     fi
   done
 }
@@ -98,16 +109,7 @@ do
   fix_paths $lib
 done
 
-# cp /usr/local/bin/gdk-pixbuf-query-loaders ${INSTALLDIR}/bin
-# fix_paths ${INSTALLDIR}/bin/gdk-pixbuf-query-loaders
-log "Installing Gaphor in ${INSTALLDIR}..."
-
-PYVER="$(python3 -c 'import sys; print("{}.{}".format(*sys.version_info))')"
-
-pip3 install --prefix ${INSTALLDIR} ../dist/gaphor-*.tar.gz
-
 # Package it up!
 
-# VERSION=`find . -name 'gaphor*egg' | sed -e 's|.*/gaphor-||' -e 's|-py.*egg$||'`
-# zip -r Gaphor-$VERSION-macos.zip $APP
+zip -r Gaphor-$VERSION-macos.zip $APP
 # hdiutil create -srcfolder $APP Gaphor-$VERSION.dmg
