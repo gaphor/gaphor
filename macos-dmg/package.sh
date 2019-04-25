493c916512b46ea4b4dce2be0a5f2b9ded4b1726
Arjan Molenaar <gaphor@gmail.com>
2019-04-25 20:24:31 +0200

    Python and libs are installed properly

--- macos-dmg/package.sh
+++ macos-dmg/package.sh
@@ -31,21 +31,11 @@ cp PkgInfo ${INSTALLDIR}
 cp gaphor ${INSTALLDIR}/MacOS
 cp gaphor.icns ${INSTALLDIR}/Resources
 
-log "Building venv for app bundle..."
-
-python3 -m venv --prompt Gaphor.app --copies ${INSTALLDIR}
-
-source ${INSTALLDIR}/bin/activate
-
 function rel_path {
   echo $1 | sed 's#/usr/local/Cellar/[^/]*/[^/]*/##'
 }
 
-{
-  brew deps gtk+3
-  echo "python"
-  brew deps python
-} | sort | uniq -u |\
+brew deps gtk+3 |\
 while read dep
 do
   log "Scanning Homebrew files for $dep"
@@ -56,33 +46,13 @@ while read f
 do
   echo "$(rel_path $f) $f"
 done |\
-grep '^lib/\|share/gir-1.0/\|share/locale/\|Frameworks/' |\
+grep '^bin\|^lib/\|^share/gir-1.0/\|^share/locale/\|^Frameworks/' |\
 while read rf f
 do
-  log "Adding ${INSTALLDIR}/${rf}"
+  # log "Adding ${INSTALLDIR}/${rf}"
   mkdir -p "${INSTALLDIR}/$(dirname $rf)"
   test -L "$f" || cp $f "${INSTALLDIR}/${rf}"
 done
-
-# grep -v '^[^/]* ' |\
-# grep -v '^share/doc/' |\
-# grep -v '^share/gtk-doc/' |\
-# grep -v '^share/gettext/' |\
-# grep -v '^share/emacs/' |\
-# grep -v '^share/man/' |\
-# grep -v '^include/' |\
-
-# # Check with Homebrew which version to pick
-# cp /usr/local/lib/libgtk-3.0.dylib ${INSTALLDIR}/lib
-# cp /usr/local/lib/libgobject-2.0.0.dylib ${INSTALLDIR}/lib
-
-
-# Modules, config, etc.
-# for dir in etc/fonts etc/gtk-3.0 lib/gtk-3.0 lib/gdk-pixbuf-2.0 lib/girepository-1.0 share/gir-1.0 share/themes/Default/gtk-3.0; do
-#   mkdir -p ${INSTALLDIR}/$dir
-#   cp -r ${LOCALDIR}/$dir/* ${INSTALLDIR}/$dir
-# done
-
 
 # Somehow files are writen with mode 444
 find $INSTALLDIR -type f -exec chmod u+w {} \;
@@ -91,7 +61,8 @@ function resolve_deps {
   local lib=$1
   local dep
   otool -L $lib | grep -e "^.$LOCALDIR/" |\
-      while read dep _; do
+  while read dep _
+  do
     echo $dep
   done
 }
@@ -99,30 +70,44 @@ function resolve_deps {
 function fix_paths {
   local lib=$1
   log Fixing $lib
-  for dep in `resolve_deps $lib`; do
-    #log Fixing `basename $lib`
+  for dep in `resolve_deps $lib`
+  do
     log "|  $dep"
-    install_name_tool -change $dep @executable_path/../lib/`basename $dep` $lib
+    # @executable_path is /path/to/Gaphor.app/MacOS
+    if [[ "$dep" =~ ^.*/Frameworks/.* ]]
+    then
+      log ">> Framework: @executable_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#')"
+      install_name_tool -change $dep @executable_path/../$(echo $dep | sed 's#^.*/\(Frameworks/.*$\)#\1#') $lib
+    else
+      log ">> Lib: @executable_path/../lib/$(basename $dep)"
+      install_name_tool -change $dep @executable_path/../lib/$(basename $dep) $lib
+    fi
   done
 }
 
 {
   find $INSTALLDIR -type f -name '*.so'
   find $INSTALLDIR -type f -name '*.dylib'
+  file $INSTALLDIR/bin/* | grep Mach-O | cut -f1 -d:
+  file $INSTALLDIR/Frameworks/Python.framework/Versions/*/bin/* | grep Mach-O | cut -f1 -d:
+  echo $INSTALLDIR/Frameworks/Python.framework/Versions/*/Python
 } |\
 while read lib
 do
+  # echo $lib
   fix_paths $lib
 done
 
-cp /usr/local/bin/gdk-pixbuf-query-loaders ${INSTALLDIR}/bin
-fix_paths ${INSTALLDIR}/bin/gdk-pixbuf-query-loaders
+# cp /usr/local/bin/gdk-pixbuf-query-loaders ${INSTALLDIR}/bin
+# fix_paths ${INSTALLDIR}/bin/gdk-pixbuf-query-loaders
 log "Installing Gaphor in ${INSTALLDIR}..."
 
-pip install ../dist/gaphor-*.tar.gz
+PYVER="$(python3 -c 'import sys; print("{}.{}".format(*sys.version_info))')"
+
+pip3 install --prefix ${INSTALLDIR} ../dist/gaphor-*.tar.gz
 
 # Package it up!
 
-VERSION=`find . -name 'gaphor*egg' | sed -e 's|.*/gaphor-||' -e 's|-py.*egg$||'`
-zip -r Gaphor-$VERSION-macos.zip $APP
-hdiutil create -srcfolder $APP Gaphor-$VERSION.dmg
+# VERSION=`find . -name 'gaphor*egg' | sed -e 's|.*/gaphor-||' -e 's|-py.*egg$||'`
+# zip -r Gaphor-$VERSION-macos.zip $APP
+# hdiutil create -srcfolder $APP Gaphor-$VERSION.dmg
