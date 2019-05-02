67d3739e8fc12d490fad485edf65b36b2f286521
Arjan Molenaar <gaphor@gmail.com>
2019-05-02 20:04:42 +0200

    Cleanup on behalf of IConnect

--- gaphor/adapters/states/tests/test_transition_connect.py
+++ gaphor/adapters/states/tests/test_transition_connect.py
@@ -6,7 +6,6 @@ from gaphor.tests import TestCase
 from zope import component
 from gaphor import UML
 from gaphor.diagram import items
-from gaphor.diagram.interfaces import IConnect
 
 
 class TransitionConnectorTestCase(TestCase):
