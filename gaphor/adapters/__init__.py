bc1dc9baccd1b4cee5621d40ebf8feff2c2185d2
Arjan Molenaar <gaphor@gmail.com>
2019-05-10 00:05:41 +0200

    Update adapters init, so stuff keeps loading

--- gaphor/adapters/__init__.py
+++ gaphor/adapters/__init__.py
@@ -7,7 +7,15 @@ import gaphor.adapters.interactions.messageconnect
 import gaphor.adapters.profiles.extensionconnect
 import gaphor.adapters.profiles.stereotypepage
 import gaphor.adapters.profiles.metaclasseditor
-import gaphor.adapters.usecases.usecaseconnect
 
 import gaphor.adapters.states.vertexconnect
 import gaphor.adapters.states.propertypages
+
+
+import gaphor.diagram.actions.flowconnect
+import gaphor.diagram.actions.partitionpage
+
+import gaphor.diagram.classes.classconnect
+import gaphor.diagram.classes.interfaceconnect
+
+import gaphor.diagram.usecases.usecaseconnect
