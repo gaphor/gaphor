6e0b384c7c040d8873d2e4d9691ece5c3d919478
Arjan Molenaar <gaphor@gmail.com>
2019-06-13 22:33:09 +0200

    Can run build_uml.py standalone

--- utils/model/build_uml.py
+++ utils/model/build_uml.py
@@ -9,59 +9,54 @@ import os.path
 from distutils.core import Command
 from distutils.dep_util import newer
 from distutils.util import byte_compile
+from distutils.dir_util import mkpath
+
+from utils.model import gen_uml
 
 
 class build_uml(Command):
 
     description = "Generate gaphor/UML/uml2.py."
 
-    user_options = [
-        ("build-lib=", "b", "build directory (where to install from)"),
-        ("force", "f", "force installation (overwrite existing files)"),
-    ]
+    user_options = [("force", "f", "force installation (overwrite existing files)")]
 
     boolean_options = ["force"]
 
     def initialize_options(self):
         # self.build_lib = None
         self.force = 0
-        self.data_dir = None
 
     def finalize_options(self):
-        self.set_undefined_options(
-            "build",
-            # ('build_lib', 'build_lib'),
-            ("force", "force"),
-        )
+        self.set_undefined_options(("force", "force"))
 
     def run(self):
-        # sys.path.insert(0, self.build_lib)
-        self.generate_uml2()
-
-    def generate_uml2(self):
-        """
-        Generate gaphor/UML/uml2.py in the build directory.
-        """
-        gen = os.path.join("utils", "model", "gen_uml.py")
-        overrides = os.path.join("gaphor", "UML", "uml2.override")
-        model = os.path.join("gaphor", "UML", "uml2.gaphor")
-        py_model = os.path.join("gaphor", "UML", "uml2.py")
-        outfile = py_model  # os.path.join(self.build_lib, py_model)
-        self.mkpath(os.path.dirname(outfile))
-        if (
-            self.force
-            or newer(model, outfile)
-            or newer(overrides, outfile)
-            or newer(gen, outfile)
-        ):
-            print("generating %s from %s..." % (py_model, model))
-            print("  (warnings can be ignored)")
-            from . import gen_uml
-
-            gen_uml.generate(model, outfile, overrides)
-        else:
-            print("not generating %s (up-to-date)" % py_model)
-        byte_compile([outfile])
+        generate_uml2(self.force)
 
 
-# vim:sw=4:et
+def generate_uml2(force=False):
+    """
+    Generate gaphor/UML/uml2.py in the source directory.
+    """
+    gen = os.path.join("utils", "model", "gen_uml.py")
+    overrides = os.path.join("gaphor", "UML", "uml2.override")
+    model = os.path.join("gaphor", "UML", "uml2.gaphor")
+    py_model = os.path.join("gaphor", "UML", "uml2.py")
+    outfile = py_model
+    mkpath(os.path.dirname(outfile))
+    if (
+        force
+        or newer(model, outfile)
+        or newer(overrides, outfile)
+        or newer(gen, outfile)
+    ):
+        print("generating %s from %s..." % (py_model, model))
+        print("  (warnings can be ignored)")
+
+        gen_uml.generate(model, outfile, overrides)
+    else:
+        print("not generating %s (up-to-date)" % py_model)
+    byte_compile([outfile])
+
+
+if __name__ == "__main__":
+    generate_uml2()
