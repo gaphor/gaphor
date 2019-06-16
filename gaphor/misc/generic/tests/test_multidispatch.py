17ea5a4a4bb85c54fb3ec7819a15ec74e43953eb
Dan Yeaw <dan@yeaw.me>
2019-06-16 16:15:13 -0400

    Convert to Pytest for multidispatch, xmlwriter, and gen_uml
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/misc/generic/tests/test_multidispatch.py
+++ gaphor/misc/generic/tests/test_multidispatch.py
@@ -1,186 +1,217 @@
 """ Tests for :module:`gaphor.misc.generic.multidispatch`."""
 
-import unittest
+import pytest
 
 from gaphor.misc.generic.multidispatch import multidispatch
 
-__all__ = ("DispatcherTests",)
+
+def create_dispatcher(
+    params_arity, args=None, varargs=None, keywords=None, defaults=None
+):
+    from inspect import ArgSpec
+    from gaphor.misc.generic.multidispatch import FunctionDispatcher
+
+    return FunctionDispatcher(
+        ArgSpec(args=args, varargs=varargs, keywords=keywords, defaults=defaults),
+        params_arity,
+    )
 
 
-class DispatcherTests(unittest.TestCase):
-    def createDispatcher(
-        self, params_arity, args=None, varargs=None, keywords=None, defaults=None
-    ):
-        from inspect import ArgSpec
-        from gaphor.misc.generic.multidispatch import FunctionDispatcher
+def test_one_argument():
+    dispatcher = create_dispatcher(1, args=["x"])
 
-        return FunctionDispatcher(
-            ArgSpec(args=args, varargs=varargs, keywords=keywords, defaults=defaults),
-            params_arity,
-        )
+    dispatcher.register_rule(lambda x: x + 1, int)
+    assert dispatcher(1) == 2
+    with pytest.raises(TypeError):
+        dispatcher("s")
 
-    def test_one_argument(self):
-        dispatcher = self.createDispatcher(1, args=["x"])
+    dispatcher.register_rule(lambda x: x + "1", str)
+    assert dispatcher(1) == 2
+    assert dispatcher("1") == "11"
+    with pytest.raises(TypeError):
+        dispatcher(tuple())
 
-        dispatcher.register_rule(lambda x: x + 1, int)
-        assert dispatcher(1) == 2
-        self.assertRaises(TypeError, dispatcher, "s")
 
-        dispatcher.register_rule(lambda x: x + "1", str)
-        assert dispatcher(1) == 2
-        assert dispatcher("1") == "11"
-        self.assertRaises(TypeError, dispatcher, tuple())
+def test_two_arguments():
+    dispatcher = create_dispatcher(2, args=["x", "y"])
 
-    def test_two_arguments(self):
-        dispatcher = self.createDispatcher(2, args=["x", "y"])
+    dispatcher.register_rule(lambda x, y: x + y + 1, int, int)
+    assert dispatcher(1, 2) == 4
+    with pytest.raises(TypeError):
+        dispatcher("s", "ss")
+    with pytest.raises(TypeError):
+        dispatcher(1, "ss")
+    with pytest.raises(TypeError):
+        dispatcher("s", 2)
 
-        dispatcher.register_rule(lambda x, y: x + y + 1, int, int)
-        assert dispatcher(1, 2) == 4
-        self.assertRaises(TypeError, dispatcher, "s", "ss")
-        self.assertRaises(TypeError, dispatcher, 1, "ss")
-        self.assertRaises(TypeError, dispatcher, "s", 2)
+    dispatcher.register_rule(lambda x, y: x + y + "1", str, str)
+    assert dispatcher(1, 2) == 4
+    assert dispatcher("1", "2") == "121"
+    with pytest.raises(TypeError):
+        dispatcher("1", 1)
+    with pytest.raises(TypeError):
+        dispatcher(1, "1")
 
-        dispatcher.register_rule(lambda x, y: x + y + "1", str, str)
-        assert dispatcher(1, 2) == 4
-        assert dispatcher("1", "2") == "121"
-        self.assertRaises(TypeError, dispatcher, "1", 1)
-        self.assertRaises(TypeError, dispatcher, 1, "1")
+    dispatcher.register_rule(lambda x, y: str(x) + y + "1", int, str)
+    assert dispatcher(1, 2) == 4
+    assert dispatcher("1", "2") == "121"
+    assert dispatcher(1, "2") == "121"
+    with pytest.raises(TypeError):
+        dispatcher("1", 1)
 
-        dispatcher.register_rule(lambda x, y: str(x) + y + "1", int, str)
-        assert dispatcher(1, 2) == 4
-        assert dispatcher("1", "2") == "121"
-        assert dispatcher(1, "2") == "121"
-        self.assertRaises(TypeError, dispatcher, "1", 1)
 
-    def test_bottom_rule(self):
-        dispatcher = self.createDispatcher(1, args=["x"])
+def test_bottom_rule():
+    dispatcher = create_dispatcher(1, args=["x"])
 
-        dispatcher.register_rule(lambda x: x, object)
-        assert dispatcher(1) == 1
-        assert dispatcher("1") == "1"
-        assert dispatcher([1]) == [1]
-        assert dispatcher((1,)) == (1,)
+    dispatcher.register_rule(lambda x: x, object)
+    assert dispatcher(1) == 1
+    assert dispatcher("1") == "1"
+    assert dispatcher([1]) == [1]
+    assert dispatcher((1,)) == (1,)
 
-    def test_subtype_evaluation(self):
-        class Super:
-            pass
 
-        class Sub(Super):
-            pass
+def test_subtype_evaluation():
+    class Super:
+        pass
 
-        dispatcher = self.createDispatcher(1, args=["x"])
+    class Sub(Super):
+        pass
 
-        dispatcher.register_rule(lambda x: x, Super)
-        o_super = Super()
-        assert dispatcher(o_super) == o_super
-        o_sub = Sub()
-        assert dispatcher(o_sub) == o_sub
-        self.assertRaises(TypeError, dispatcher, object())
+    dispatcher = create_dispatcher(1, args=["x"])
 
-        dispatcher.register_rule(lambda x: (x, x), Sub)
-        o_super = Super()
-        assert dispatcher(o_super) == o_super
-        o_sub = Sub()
-        assert dispatcher(o_sub) == (o_sub, o_sub)
+    dispatcher.register_rule(lambda x: x, Super)
+    o_super = Super()
+    assert dispatcher(o_super) == o_super
+    o_sub = Sub()
+    assert dispatcher(o_sub) == o_sub
+    with pytest.raises(TypeError):
+        dispatcher(object())
 
-    def test_register_rule_with_wrong_arity(self):
-        dispatcher = self.createDispatcher(1, args=["x"])
-        dispatcher.register_rule(lambda x: x, int)
-        self.assertRaises(TypeError, dispatcher.register_rule, lambda x, y: x, str)
+    dispatcher.register_rule(lambda x: (x, x), Sub)
+    o_super = Super()
+    assert dispatcher(o_super) == o_super
+    o_sub = Sub()
+    assert dispatcher(o_sub) == (o_sub, o_sub)
 
-    def test_register_rule_with_different_arg_names(self):
-        dispatcher = self.createDispatcher(1, args=["x"])
-        dispatcher.register_rule(lambda y: y, int)
-        assert dispatcher(1) == 1
 
-    def test_dispatching_with_varargs(self):
-        dispatcher = self.createDispatcher(1, args=["x"], varargs="va")
-        dispatcher.register_rule(lambda x, *va: x, int)
-        assert dispatcher(1) == 1
-        self.assertRaises(TypeError, dispatcher, "1", 2, 3)
-
-    def test_dispatching_with_varkw(self):
-        dispatcher = self.createDispatcher(1, args=["x"], keywords="vk")
-        dispatcher.register_rule(lambda x, **vk: x, int)
-        assert dispatcher(1) == 1
-        self.assertRaises(TypeError, dispatcher, "1", a=1, b=2)
-
-    def test_dispatching_with_kw(self):
-        dispatcher = self.createDispatcher(1, args=["x", "y"], defaults=["vk"])
-        dispatcher.register_rule(lambda x, y=1: x, int)
-        assert dispatcher(1) == 1
-        self.assertRaises(TypeError, dispatcher, "1", k=1)
-
-    def test_create_dispatcher_with_pos_args_less_multi_arity(self):
-        self.assertRaises(TypeError, self.createDispatcher, 2, args=["x"])
-        self.assertRaises(
-            TypeError, self.createDispatcher, 2, args=["x", "y"], defaults=["x"]
-        )
-
-    def test_register_rule_with_wrong_number_types_parameters(self):
-        dispatcher = self.createDispatcher(1, args=["x", "y"])
-        self.assertRaises(TypeError, dispatcher.register_rule, lambda x, y: x, int, str)
-
-    def test_register_rule_with_partial_dispatching(self):
-        dispatcher = self.createDispatcher(1, args=["x", "y"])
-        dispatcher.register_rule(lambda x, y: x, int)
-        assert dispatcher(1, 2) == 1
-        assert dispatcher(1, "2") == 1
-        self.assertRaises(TypeError, dispatcher, "2", 1)
+def test_register_rule_with_wrong_arity():
+    dispatcher = create_dispatcher(1, args=["x"])
+    dispatcher.register_rule(lambda x: x, int)
+    with pytest.raises(TypeError):
         dispatcher.register_rule(lambda x, y: x, str)
-        assert dispatcher(1, 2) == 1
-        assert dispatcher(1, "2") == 1
-        assert dispatcher("1", "2") == "1"
-        assert dispatcher("1", 2) == "1"
 
 
-class MultifunctionTests(unittest.TestCase):
-    def test_default_dispatcher(self):
-        @multidispatch(int, str)
-        def func(x, y):
-            return str(x) + y
+def test_register_rule_with_different_arg_names():
+    dispatcher = create_dispatcher(1, args=["x"])
+    dispatcher.register_rule(lambda y: y, int)
+    assert dispatcher(1) == 1
 
-        assert func(1, "2") == "12"
-        self.assertRaises(TypeError, func, 1, 2)
-        self.assertRaises(TypeError, func, "1", 2)
-        self.assertRaises(TypeError, func, "1", "2")
 
-    def test_multiple_functions(self):
-        @multidispatch(int, str)
-        def func(x, y):
-            return str(x) + y
+def test_dispatching_with_varargs():
+    dispatcher = create_dispatcher(1, args=["x"], varargs="va")
+    dispatcher.register_rule(lambda x, *va: x, int)
+    assert dispatcher(1) == 1
+    with pytest.raises(TypeError):
+        dispatcher("1", 2, 3)
 
-        @func.register(str, str)
-        def _(x, y):
-            return x + y
 
-        assert func(1, "2") == "12"
-        assert func("1", "2") == "12"
-        self.assertRaises(TypeError, func, 1, 2)
-        self.assertRaises(TypeError, func, "1", 2)
+def test_dispatching_with_varkw():
+    dispatcher = create_dispatcher(1, args=["x"], keywords="vk")
+    dispatcher.register_rule(lambda x, **vk: x, int)
+    assert dispatcher(1) == 1
+    with pytest.raises(TypeError):
+        dispatcher("1", a=1, b=2)
 
-    def test_default(self):
-        @multidispatch()
-        def func(x, y):
-            return x + y
 
-        @func.register(str, str)
-        def _(x, y):
-            return y + x
+def test_dispatching_with_kw():
+    dispatcher = create_dispatcher(1, args=["x", "y"], defaults=["vk"])
+    dispatcher.register_rule(lambda x, y=1: x, int)
+    assert dispatcher(1) == 1
+    with pytest.raises(TypeError):
+        dispatcher("1", k=1)
 
-        assert func(1, 1) == 2
-        assert func("1", "2") == "21"
 
-    def test_on_classes(self):
-        @multidispatch()
-        class A:
-            def __init__(self, a, b):
-                self.v = a + b
+def test_create_dispatcher_with_pos_args_less_multi_arity():
+    with pytest.raises(TypeError):
+        create_dispatcher(2, args=["x"])
+    with pytest.raises(TypeError):
+        create_dispatcher(2, args=["x", "y"], defaults=["x"])
 
-        @A.register(str, str)
-        class B:
-            def __init__(self, a, b):
-                self.v = b + a
 
-        assert A(1, 1).v == 2
-        assert A("1", "2").v == "21"
+def test_register_rule_with_wrong_number_types_parameters():
+    dispatcher = create_dispatcher(1, args=["x", "y"])
+    with pytest.raises(TypeError):
+        dispatcher.register_rule(lambda x, y: x, int, str)
+
+
+def test_register_rule_with_partial_dispatching():
+    dispatcher = create_dispatcher(1, args=["x", "y"])
+    dispatcher.register_rule(lambda x, y: x, int)
+    assert dispatcher(1, 2) == 1
+    assert dispatcher(1, "2") == 1
+    with pytest.raises(TypeError):
+        dispatcher("2", 1)
+    dispatcher.register_rule(lambda x, y: x, str)
+    assert dispatcher(1, 2) == 1
+    assert dispatcher(1, "2") == 1
+    assert dispatcher("1", "2") == "1"
+    assert dispatcher("1", 2) == "1"
+
+
+def test_default_dispatcher():
+    @multidispatch(int, str)
+    def func(x, y):
+        return str(x) + y
+
+    assert func(1, "2") == "12"
+    with pytest.raises(TypeError):
+        func(1, 2)
+    with pytest.raises(TypeError):
+        func("1", 2)
+    with pytest.raises(TypeError):
+        func("1", "2")
+
+
+def test_multiple_functions():
+    @multidispatch(int, str)
+    def func(x, y):
+        return str(x) + y
+
+    @func.register(str, str)
+    def _(x, y):
+        return x + y
+
+    assert func(1, "2") == "12"
+    assert func("1", "2") == "12"
+    with pytest.raises(TypeError):
+        func(1, 2)
+    with pytest.raises(TypeError):
+        func("1", 2)
+
+
+def test_default():
+    @multidispatch()
+    def func(x, y):
+        return x + y
+
+    @func.register(str, str)
+    def _(x, y):
+        return y + x
+
+    assert func(1, 1) == 2
+    assert func("1", "2") == "21"
+
+
+def test_on_classes():
+    @multidispatch()
+    class A:
+        def __init__(self, a, b):
+            self.v = a + b
+
+    @A.register(str, str)
+    class B:
+        def __init__(self, a, b):
+            self.v = b + a
+
+    assert A(1, 1).v == 2
+    assert A("1", "2").v == "21"
--- gaphor/misc/tests/test_xmlwriter.py
+++ gaphor/misc/tests/test_xmlwriter.py
@@ -1,5 +1,4 @@
 import sys
-import unittest
 from gaphor.misc.xmlwriter import XMLWriter
 
 
@@ -11,74 +10,75 @@ class Writer:
         self.s += text
 
 
-class XMLWriterTestCase(unittest.TestCase):
-    def test_elements_1(self):
-        w = Writer()
-        xml_w = XMLWriter(w)
-        xml_w.startDocument()
-        xml_w.startElement("foo", {})
-        xml_w.endElement("foo")
+def test_elements_1():
+    w = Writer()
+    xml_w = XMLWriter(w)
+    xml_w.startDocument()
+    xml_w.startElement("foo", {})
+    xml_w.endElement("foo")
 
-        xml = (
-            """<?xml version="1.0" encoding="%s"?>\n<foo/>""" % sys.getdefaultencoding()
-        )
-        assert w.s == xml, w.s + " != " + xml
+    xml = """<?xml version="1.0" encoding="%s"?>\n<foo/>""" % sys.getdefaultencoding()
+    assert w.s == xml, w.s + " != " + xml
 
-    def test_elements_2(self):
-        w = Writer()
-        xml_w = XMLWriter(w)
-        xml_w.startDocument()
-        xml_w.startElement("foo", {})
-        xml_w.startElement("bar", {})
-        xml_w.endElement("bar")
-        xml_w.endElement("foo")
 
-        xml = (
-            """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar/>\n</foo>"""
-            % sys.getdefaultencoding()
-        )
-        assert w.s == xml, w.s
+def test_elements_2():
+    w = Writer()
+    xml_w = XMLWriter(w)
+    xml_w.startDocument()
+    xml_w.startElement("foo", {})
+    xml_w.startElement("bar", {})
+    xml_w.endElement("bar")
+    xml_w.endElement("foo")
 
-    def test_elements_test(self):
-        w = Writer()
-        xml_w = XMLWriter(w)
-        xml_w.startDocument()
-        xml_w.startElement("foo", {})
-        xml_w.startElement("bar", {})
-        xml_w.characters("hello")
-        xml_w.endElement("bar")
-        xml_w.endElement("foo")
+    xml = (
+        """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar/>\n</foo>"""
+        % sys.getdefaultencoding()
+    )
+    assert w.s == xml, w.s
 
-        xml = (
-            """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar>hello</bar>\n</foo>"""
-            % sys.getdefaultencoding()
-        )
-        assert w.s == xml, w.s
 
-    def test_elements_ns_default(self):
-        w = Writer()
-        xml_w = XMLWriter(w)
-        xml_w.startDocument()
-        xml_w.startPrefixMapping(None, "http://gaphor.devjavu.com/schema")
-        xml_w.startElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn", {})
-        xml_w.endElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn")
+def test_elements_test():
+    w = Writer()
+    xml_w = XMLWriter(w)
+    xml_w.startDocument()
+    xml_w.startElement("foo", {})
+    xml_w.startElement("bar", {})
+    xml_w.characters("hello")
+    xml_w.endElement("bar")
+    xml_w.endElement("foo")
 
-        xml = (
-            """<?xml version="1.0" encoding="%s"?>\n<foo xmlns="http://gaphor.devjavu.com/schema"/>"""
-            % sys.getdefaultencoding()
-        )
-        assert w.s == xml, w.s
+    xml = (
+        """<?xml version="1.0" encoding="%s"?>\n<foo>\n<bar>hello</bar>\n</foo>"""
+        % sys.getdefaultencoding()
+    )
+    assert w.s == xml, w.s
 
-    def test_elements_ns_1(self):
-        w = Writer()
-        xml_w = XMLWriter(w)
-        xml_w.startDocument()
-        xml_w.startPrefixMapping("g", "http://gaphor.devjavu.com/schema")
-        xml_w.startElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn", {})
-        xml_w.endElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn")
 
-        xml = (
-            """<?xml version="1.0" encoding="%s"?>\n<g:foo xmlns:g="http://gaphor.devjavu.com/schema"/>"""
-            % sys.getdefaultencoding()
-        )
-        assert w.s == xml, w.s
+def test_elements_ns_default():
+    w = Writer()
+    xml_w = XMLWriter(w)
+    xml_w.startDocument()
+    xml_w.startPrefixMapping(None, "http://gaphor.devjavu.com/schema")
+    xml_w.startElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn", {})
+    xml_w.endElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn")
+
+    xml = (
+        """<?xml version="1.0" encoding="%s"?>\n<foo xmlns="http://gaphor.devjavu.com/schema"/>"""
+        % sys.getdefaultencoding()
+    )
+    assert w.s == xml, w.s
+
+
+def test_elements_ns_1():
+    w = Writer()
+    xml_w = XMLWriter(w)
+    xml_w.startDocument()
+    xml_w.startPrefixMapping("g", "http://gaphor.devjavu.com/schema")
+    xml_w.startElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn", {})
+    xml_w.endElementNS(("http://gaphor.devjavu.com/schema", "foo"), "qn")
+
+    xml = (
+        """<?xml version="1.0" encoding="%s"?>\n<g:foo xmlns:g="http://gaphor.devjavu.com/schema"/>"""
+        % sys.getdefaultencoding()
+    )
+    assert w.s == xml, w.s
