21f91e4ec934c6567c9a547e9c3d96e198f31494
Arjan Molenaar <gaphor@gmail.com>
2019-06-27 19:08:44 +0200

    Add functions for text alignment on line to text module

--- gaphor/diagram/tests/test_text.py
+++ gaphor/diagram/tests/test_text.py
@@ -2,7 +2,7 @@ import pytest
 import cairo
 
 import gaphor.diagram.text
-from gaphor.diagram.text import Text
+from gaphor.diagram.text import Text, text_point_at_line, TextAlign, VerticalAlign
 
 TEXT_SIZE = (60, 15)
 
@@ -50,3 +50,97 @@ def test_text_width_min_height(cr):
 
     _, h = text.size(cr)
     assert h == 40
+
+
+@pytest.mark.parametrize(
+    "points,point_at_line",
+    [
+        [[(0, 0), (20, 20)], (5, -10)],
+        [[(0, 0), (-20, 20)], (-15, -10)],
+        [[(0, 0), (20, -20)], (5, 5)],
+        [[(0, 0), (-20, -20)], (-15, 5)],
+    ],
+)
+def test_point_at_left_top(points, point_at_line):
+    x, y = text_point_at_line(
+        points, (10, 5), TextAlign.LEFT, VerticalAlign.TOP, (2, 2, 2, 2)
+    )
+
+    assert x == point_at_line[0]
+    assert y == point_at_line[1]
+
+
+@pytest.mark.parametrize(
+    "points,point_at_line",
+    [
+        [[(0, 0), (20, 20)], (5, 25)],
+        [[(0, 0), (-20, 20)], (-15, 25)],
+        [[(0, 0), (20, -20)], (5, -30)],
+        [[(0, 0), (-20, -20)], (-15, -30)],
+    ],
+)
+def test_point_at_right_top(points, point_at_line):
+    x, y = text_point_at_line(
+        points, (10, 5), TextAlign.RIGHT, VerticalAlign.TOP, (2, 2, 2, 2)
+    )
+
+    assert x == point_at_line[0]
+    assert y == point_at_line[1]
+
+
+@pytest.mark.parametrize(
+    "points,point_at_line",
+    [
+        [[(2.0, 2.0), (22.0, 7.0)], (7, -4.75)],
+        [[(2.0, 2.0), (22.0, -3.0)], (7, -9.75)],
+        [[(2.0, 2.0), (-18.0, 7.0)], (-13, -4.75)],
+        [[(2.0, 2.0), (-18.0, -3.0)], (-13, -9.75)],
+        [[(2.0, 2.0), (7.0, 22.0)], (7.125, 9.5)],
+        [[(2.0, 2.0), (7.0, -18.0)], (-8.125, -10.5)],
+        [[(2.0, 2.0), (-3.0, 22.0)], (-13.125, 9.5)],
+        [[(2.0, 2.0), (-3.0, -18.0)], (2.125, -10.5)],
+        [[(2.0, 2.0), (22.0, 2.0)], (7, -6)],
+        [[(2.0, 2.0), (-18.0, 2.0)], (-13, -6)],
+        [[(2.0, 2.0), (2.0, 22.0)], (-10, 9.5)],
+        [[(2.0, 2.0), (2.0, -18.0)], (-10, -10.5)],
+    ],
+)
+def test_point_at_center_top(points, point_at_line):
+    """
+    Test aligned at the line text position calculation, horizontal mode
+    """
+    x, y = text_point_at_line(
+        points, (10, 5), TextAlign.CENTER, VerticalAlign.TOP, (3, 2, 3, 2)
+    )
+
+    assert x == pytest.approx(point_at_line[0])
+    assert y == pytest.approx(point_at_line[1])
+
+
+@pytest.mark.parametrize(
+    "points,point_at_line",
+    [
+        [[(2.0, 2.0), (22.0, 7.0)], (7, 8.75)],
+        [[(2.0, 2.0), (22.0, -3.0)], (7, 3.75)],
+        [[(2.0, 2.0), (-18.0, 7.0)], (-13, 8.75)],
+        [[(2.0, 2.0), (-18.0, -3.0)], (-13, 3.75)],
+        [[(2.0, 2.0), (7.0, 22.0)], (-8.125, 9.5)],
+        [[(2.0, 2.0), (7.0, -18.0)], (7.125, -10.5)],
+        [[(2.0, 2.0), (-3.0, 22.0)], (2.125, 9.5)],
+        [[(2.0, 2.0), (-3.0, -18.0)], (-13.125, -10.5)],
+        [[(2.0, 2.0), (22.0, 2.0)], (7, 5)],
+        [[(2.0, 2.0), (-18.0, 2.0)], (-13, 5)],
+        [[(2.0, 2.0), (2.0, 22.0)], (4, 9.5)],
+        [[(2.0, 2.0), (2.0, -18.0)], (4, -10.5)],
+    ],
+)
+def test_point_at_center_bottom(points, point_at_line):
+    """
+    Test aligned at the line text position calculation, horizontal mode
+    """
+    x, y = text_point_at_line(
+        points, (10, 5), TextAlign.CENTER, VerticalAlign.BOTTOM, (3, 2, 3, 2)
+    )
+
+    assert x == pytest.approx(point_at_line[0])
+    assert y == pytest.approx(point_at_line[1])
--- gaphor/diagram/text.py
+++ gaphor/diagram/text.py
@@ -85,15 +85,6 @@ def Guard(presentation):
     return guard
 
 
-def _text_layout(cr, text, font, width):
-    layout = PangoCairo.create_layout(cr)
-    if font:
-        layout.set_font_description(Pango.FontDescription.from_string(font))
-    layout.set_text(text, length=-1)
-    layout.set_width(int(width * Pango.SCALE))
-    return layout
-
-
 def text_size(cr, text, font, width=-1):
     if not text:
         return 0, 0
@@ -141,3 +132,193 @@ def text_draw_in_box(cr, bounding_box, text, font, text_align, vertical_align):
     cr.move_to(x, y)
 
     PangoCairo.show_layout(cr, layout)
+
+
+def _text_layout(cr, text, font, width):
+    layout = PangoCairo.create_layout(cr)
+    if font:
+        layout.set_font_description(Pango.FontDescription.from_string(font))
+    layout.set_text(text, length=-1)
+    layout.set_width(int(width * Pango.SCALE))
+    return layout
+
+
+def text_point_at_line(points, size, text_align, vertical_align, padding):
+    """
+    Provide a position (x, y) to draw a text close to a line.
+
+    Parameters:
+     - points:  the line points, a list of (x, y) points
+     - size:    size of the text, a (width, height) tuple
+     - text_align: alignment to the line: left, beginning of the line, center, middle and right: end of the line
+     - vertical_align: vertical alignment of the text relative to the line
+     - padding: text padding, a (top, right, bottom, left) tuple
+    """
+
+    if text_align == TextAlign.LEFT:
+        p0 = points[0]
+        p1 = points[1]
+        x, y = _text_point_at_line_end(size, p0, p1, padding)
+    elif text_align == TextAlign.CENTER:
+        p0, p1 = middle_segment(points)
+        x, y = _text_point_at_line_center(size, p0, p1, vertical_align, padding)
+    elif text_align == TextAlign.RIGHT:
+        p0 = points[-1]
+        p1 = points[-2]
+        print("right", p0, p1)
+        x, y = _text_point_at_line_end(size, p0, p1, padding)
+
+    return x, y
+
+
+def middle_segment(points):
+    """
+    Get middle line segment.
+    """
+    m = len(points) // 2
+    assert m - 1 >= 0 and m < len(points)
+    return points[m - 1], points[m]
+
+
+def _text_point_at_line_end(size, p1, p2, padding):
+    """
+    Calculate position of the text relative to a line defined by points
+    p1 and p2. Text is aligned using align and padding information.
+
+    Parameters:
+     - size: text size, a (width, height) tuple
+     - p1:      beginning of line segment
+     - p2:      end of line segment
+     - padding: text padding, a (top, right, bottom, left) tuple
+    """
+    name_dx = 0.0
+    name_dy = 0.0
+    ofs = 5
+
+    print("in_func", size, p1, p2, padding)
+
+    dx = float(p2[0]) - float(p1[0])
+    dy = float(p2[1]) - float(p1[1])
+
+    name_w, name_h = size
+
+    if dy == 0:
+        rc = 1000.0  # quite a lot...
+    else:
+        rc = dx / dy
+    abs_rc = abs(rc)
+    h = dx > 0  # right side of the box
+    v = dy > 0  # bottom side
+
+    if abs_rc > 6:
+        # horizontal line
+        if h:
+            name_dx = ofs
+            name_dy = -ofs - name_h
+        else:
+            name_dx = -ofs - name_w
+            name_dy = -ofs - name_h
+    elif 0 <= abs_rc <= 0.2:
+        # vertical line
+        if v:
+            name_dx = -ofs - name_w
+            name_dy = ofs
+        else:
+            name_dx = -ofs - name_w
+            name_dy = -ofs - name_h
+    else:
+        # Should both items be placed on the same side of the line?
+        r = abs_rc < 1.0
+
+        # Find out alignment of text (depends on the direction of the line)
+        align_left = (h and not r) or (r and not h)
+        align_bottom = (v and not r) or (r and not v)
+        if align_left:
+            name_dx = ofs
+        else:
+            name_dx = -ofs - name_w
+        if align_bottom:
+            name_dy = -ofs - name_h
+        else:
+            name_dy = ofs
+    return p1[0] + name_dx, p1[1] + name_dy
+
+
+# padding
+PADDING_TOP, PADDING_RIGHT, PADDING_BOTTOM, PADDING_LEFT = list(range(4))
+
+# hint tuples to move text depending on quadrant
+WIDTH_HINT = (0, 0, -1)  # width hint tuple
+R_WIDTH_HINT = (-1, -1, 0)  # width hint tuple
+PADDING_HINT = (1, 1, -1)  # padding hint tuple
+
+EPSILON = 1e-6
+
+
+def _text_point_at_line_center(size, p1, p2, vertical_align, padding):
+    """
+    Calculate position of the text relative to a line defined by points
+    p1 and p2. Text is aligned using align and padding information.
+
+    Parameters:
+     - size:    text size, a (width, height) tuple
+     - p1:      beginning of line
+     - p2:      end of line
+     - vertical_align:   text align information, from VerticalAlign
+     - padding: text padding, a (top, right, bottom, left) tuple
+    """
+    x0 = (p1[0] + p2[0]) / 2.0
+    y0 = (p1[1] + p2[1]) / 2.0
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+
+    if abs(dx) < EPSILON:
+        d1 = -1.0
+        d2 = 1.0
+    elif abs(dy) < EPSILON:
+        d1 = 0.0
+        d2 = 0.0
+    else:
+        d1 = dy / dx
+        d2 = abs(d1)
+
+    width, height = size
+
+    # move to center and move by delta depending on line angle
+    if d2 < 0.5774:  # <0, 30>, <150, 180>, <-180, -150>, <-30, 0>
+        # horizontal mode
+        w2 = width / 2.0
+        hint = w2 * d2
+
+        x = x0 - w2
+        if vertical_align == VerticalAlign.TOP:
+            y = y0 - height - padding[PADDING_BOTTOM] - hint
+        else:
+            y = y0 + padding[PADDING_TOP] + hint
+    else:
+        # much better in case of vertical lines
+
+        # determine quadrant, we are interested in 1 or 3 and 2 or 4
+        # see hint tuples below
+        h2 = height / 2.0
+        q = (d1 > 0) - (d1 < 0)
+        if abs(dx) < EPSILON:
+            hint = 0
+        else:
+            hint = h2 / d2
+
+        if vertical_align == VerticalAlign.TOP:
+            x = (
+                x0
+                + PADDING_HINT[q] * (padding[PADDING_LEFT] + hint)
+                + width * WIDTH_HINT[q]
+            )
+        else:
+            x = (
+                x0
+                - PADDING_HINT[q] * (padding[PADDING_RIGHT] + hint)
+                + width * R_WIDTH_HINT[q]
+            )
+        y = y0 - h2
+
+    return x, y
