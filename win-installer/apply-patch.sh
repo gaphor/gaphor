072a7ec5264621425c1e4709466a26ff2168bcbb
Dan Yeaw <dan@yeaw.me>
2019-08-11 09:29:33 -0400

    Add patch for PyInstaller issue 4263

--- win-installer/apply-patch.sh
+++ win-installer/apply-patch.sh
@@ -1,2 +1,3 @@
-patch -u -b ../.venv/lib/python3.7/site-packages/importlib_metadata/__init__.py -i fix-pyinstaller-issue-4258.patch
+# patch -u -b ../.venv/lib/python3.7/site-packages/importlib_metadata/__init__.py -i fix-pyinstaller-issue-4258.patch
+patch -u -b ../.venv/lib/python3.7/site-packages/PyInstaller/building/build_main.py -i fix-pyinstaller-issue-4263.patch
 
