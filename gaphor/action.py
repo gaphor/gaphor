501bc8e21e2c957d1800a7e5268cc5c68e29a6ec
Arjan Molenaar <gaphor@gmail.com>
2019-10-24 23:02:11 +0200

    Remove radio_action

--- gaphor/action.py
+++ gaphor/action.py
@@ -39,14 +39,7 @@ class action:
     """
 
     def __init__(
-        self,
-        name,
-        label=None,
-        tooltip=None,
-        icon_name=None,
-        shortcut=None,
-        state=None,
-        **kwargs,
+        self, name, label=None, tooltip=None, icon_name=None, shortcut=None, state=None
     ):
         self.scope, self.name = name.split(".", 2) if "." in name else ("win", name)
         self.label = label
@@ -55,7 +48,6 @@ class action:
         self.shortcut = shortcut
         self.state = state
         self.arg_type = None
-        self.__dict__.update(kwargs)
 
     def __call__(self, func):
         type_hints = get_type_hints(func)
@@ -64,41 +56,6 @@ class action:
             self.arg_type = next(iter(type_hints.values()))
         func.__action__ = self
         return func
-
-
-class radio_action(action):
-    """
-    Radio buttons take a list of names, a list of labels and a list of
-    tooltips (and optionally, a list of icon names).
-    The callback function should have an extra value property, which is
-    given the index number of the activated radio button action.
-    """
-
-    names: Sequence[str]
-    labels: Sequence[Optional[str]]
-    tooltips: Sequence[Optional[str]]
-    icon_names: Sequence[Optional[str]]
-    shortcuts: Sequence[Optional[str]]
-    active: int
-
-    def __init__(
-        self,
-        names,
-        labels=None,
-        tooltips=None,
-        icon_names=None,
-        shortcuts=None,
-        active=0,
-    ):
-        super().__init__(
-            names[0],
-            names=names,
-            labels=labels,
-            tooltips=tooltips,
-            icon_names=icon_names,
-            shortcuts=shortcuts,
-            active=active,
-        )
 
 
 def is_action(func):
--- gaphor/ui/consolewindow.py
+++ gaphor/ui/consolewindow.py
@@ -48,12 +48,6 @@ class ConsoleWindow(UIComponent, ActionProvider):
         console = self.construct()
         self.load_console_py(console)
 
-    # @action(
-    #     name="console-window-close",
-    #     label="_Close",
-    #     icon_name="window-close",
-    #     shortcut="<Primary><Shift>w",
-    # )
     def close(self, widget=None):
         if self.window:
             self.window.destroy()
