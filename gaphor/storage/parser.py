9f8cd03a568c5f076c8d8b4597ead042163946d0
Arjan Molenaar <gaphor@gmail.com>
2019-11-09 21:51:57 +0100

    Simplify a bit of code

--- gaphor/storage/parser.py
+++ gaphor/storage/parser.py
@@ -178,9 +178,7 @@ class GaphorLoader(handler.ContentHandler):
         if state == GAPHOR:
             id = attrs["id"]
             e = element(id, name)
-            assert id not in list(self.elements.keys()), "%s already defined" % (
-                id
-            )  # , self.elements[id])
+            assert id not in self.elements.keys(), f"{id} already defined"
             self.elements[id] = e
             self.push(e, name == "Diagram" and DIAGRAM or ELEMENT)
 
@@ -194,7 +192,7 @@ class GaphorLoader(handler.ContentHandler):
         elif state in (CANVAS, ITEM) and name == "item":
             id = attrs["id"]
             ci = canvasitem(id, attrs["type"])
-            assert id not in list(self.elements.keys()), f"{id} already defined"
+            assert id not in self.elements.keys(), f"{id} already defined"
             self.elements[id] = ci
             self.peek().canvasitems.append(ci)
             self.push(ci, ITEM)
