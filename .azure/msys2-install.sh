6ab9c7f29a45cf16ececd25fefca7294eb5ae4cd
Arjan Molenaar <gaphor@gmail.com>
2019-11-16 21:50:01 +0100

    Use our custom version of babel-glade
    Drop lxml dependency.

--- .azure/msys2-install.sh
+++ .azure/msys2-install.sh
@@ -14,7 +14,6 @@ pacman --noconfirm -S --needed \
     mingw-w64-$MSYS2_ARCH-cairo \
     mingw-w64-$MSYS2_ARCH-gobject-introspection \
     mingw-w64-$MSYS2_ARCH-python3 \
-    mingw-w64-$MSYS2_ARCH-python3-lxml \
     mingw-w64-$MSYS2_ARCH-python3-gobject \
     mingw-w64-$MSYS2_ARCH-python3-cairo \
     mingw-w64-$MSYS2_ARCH-python3-pip
