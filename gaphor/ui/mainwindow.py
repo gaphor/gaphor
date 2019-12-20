2870d329afe0a119c946d023d6771379f32e124e
Arjan Molenaar <gaphor@gmail.com>
2019-12-20 08:19:09 +0100

    Move recent files menu to recentfiles module
    This seems like a more logical place.

--- gaphor/ui/mainwindow.py
+++ gaphor/ui/mainwindow.py
@@ -26,41 +26,10 @@ from gaphor.ui.event import (
     WindowClosed,
 )
 from gaphor.ui.layout import deserialize
+from gaphor.ui.recentfiles import HOME, RecentFilesMenu
 from gaphor.UML.event import AttributeUpdated, ModelFlushed, ModelReady
 
 log = logging.getLogger(__name__)
-
-HOME = str(Path.home())
-
-
-class RecentFilesMenu(Gio.Menu):
-    def __init__(self, recent_manager):
-        super().__init__()
-
-        self._on_recent_manager_changed(recent_manager)
-        # TODO: should unregister if the window is closed.
-        self._changed_id = recent_manager.connect(
-            "changed", self._on_recent_manager_changed
-        )
-
-    def _on_recent_manager_changed(self, recent_manager):
-        self.remove_all()
-        for item in recent_manager.get_items():
-            if APPLICATION_ID in item.get_applications():
-                menu_item = Gio.MenuItem.new(
-                    item.get_uri_display().replace(HOME, "~"), "win.file-open-recent"
-                )
-                filename, _host = GLib.filename_from_uri(item.get_uri())
-                menu_item.set_attribute_value(
-                    "target", GLib.Variant.new_string(filename)
-                )
-                self.append_item(menu_item)
-                if self.get_n_items() > 9:
-                    break
-        if self.get_n_items() == 0:
-            self.append_item(
-                Gio.MenuItem.new(gettext("No recently opened models"), None)
-            )
 
 
 def hamburger_menu(hamburger_model):
