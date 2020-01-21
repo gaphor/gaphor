a81b8801370da271ad9d501fc4ba18ad2ff4170e
Arjan Molenaar <gaphor@gmail.com>
2020-01-21 10:05:24 +0100

    More verbose windows test run
    Why does it hang on the collect step?

--- win-installer/msys2-test.sh
+++ win-installer/msys2-test.sh
@@ -5,4 +5,5 @@ set -e
 export PYTEST_ADDOPTS="--doctest-modules --junitxml=junit/test-results.xml"
 export PY_IGNORE_IMPORTMISMATCH=1
 
-poetry run pytest
+poetry run pytest --collect-only -vvv
+poetry run pytest -vvv
