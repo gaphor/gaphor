6cd1c7a95ca4162643b6d52f4bb82596178fde22
Arjan Molenaar <gaphor@gmail.com>
2020-02-05 08:48:34 +0100

    Reorder imports in UML module

--- gaphor/UML/__init__.py
+++ gaphor/UML/__init__.py
@@ -1,9 +1,10 @@
 # Here, order matters
 from gaphor.UML.uml2 import *  # noqa: isort:skip
 from gaphor.UML.presentation import Presentation  # noqa: isort:skip
-import gaphor.UML.uml2overrides  # noqa: isort:skip
 from gaphor.UML.elementfactory import ElementFactory  # noqa: isort:skip
 from gaphor.UML import modelfactory as model  # noqa: isort:skip
 
 from gaphor.UML.umlfmt import format
 from gaphor.UML.umllex import parse
+
+import gaphor.UML.uml2overrides  # noqa: isort:skip
