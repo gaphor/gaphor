23b6c0275ccaa971d4f8933b58f7c39aa925bed6
Arjan Molenaar <gaphor@gmail.com>
2020-03-14 22:54:16 +0100

    Construct component editor via Glade UI

--- gaphor/diagram/components/componentspropertypage.py
+++ gaphor/diagram/components/componentspropertypage.py
@@ -1,35 +1,35 @@
 from gi.repository import Gtk
 
 from gaphor import UML
-from gaphor.core import gettext, transactional
-from gaphor.diagram.components.component import ComponentItem
-from gaphor.diagram.propertypages import NamedItemPropertyPage, PropertyPages
+from gaphor.core import transactional
+from gaphor.diagram.propertypages import PropertyPageBase, PropertyPages, builder
 
 
-@PropertyPages.register(ComponentItem)
-class ComponentPropertyPage(NamedItemPropertyPage):
-    """
-    """
+@PropertyPages.register(UML.Component)
+class ComponentPropertyPage(PropertyPageBase):
+
+    name = "Component"
+    order = 15
 
     subject: UML.Component
 
-    def construct(self):
-        page = super().construct()
+    def __init__(self, subject):
+        self.subject = subject
+        self.builder = builder("component-editor")
 
+    def construct(self):
         subject = self.subject
 
         if not subject:
-            return page
+            return
 
-        hbox = Gtk.HBox()
-        page.pack_start(hbox, False, True, 0)
+        ii = self.builder.get_object("indirectly-instantiated")
+        ii.set_active(subject.isIndirectlyInstantiated)
 
-        button = Gtk.CheckButton(gettext("Indirectly instantiated"))
-        button.set_active(subject.isIndirectlyInstantiated)
-        button.connect("toggled", self._on_ii_change)
-        hbox.pack_start(button, False, True, 0)
-
-        return page
+        self.builder.connect_signals(
+            {"indirectly-instantiated-changed": (self._on_ii_change,)}
+        )
+        return self.builder.get_object("component-editor")
 
     @transactional
     def _on_ii_change(self, button):
