e3836bbd04263015e1d49e8f8b31658165d93638
Dan Yeaw <dan@yeaw.me>
2020-03-15 15:19:32 -0400

    Add echo statements
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- .github/scripts/metadata.sh
+++ .github/scripts/metadata.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "GITHUB_REF is $GITHUB_REF"
 TAG="${GITHUB_REF/\/refs\/tags\//}"
+echo "TAG is $TAG"
 if ! [ -x "$(command -v poetry)" ]; then
     echo 'Poetry not found, activating venv'
     source venv
 fi
 VERSION="$(poetry version --no-ansi | cut -d' ' -f2)"
+echo "VERSION is $VERSION"
 
 if [[ "$GITHUB_REF" =~ \/refs\/tags\/.* && "$TAG" == "$VERSION" ]]
 then
