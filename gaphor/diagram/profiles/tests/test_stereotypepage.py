09a4dee16f7f5cc78566b1070a228b7067c3611a
Dan Yeaw <dan@yeaw.me>
2020-03-22 22:08:34 -0400

    Convert Metaclass and Stereotype page tests to Pytest
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/diagram/profiles/tests/test_stereotypepage.py
+++ gaphor/diagram/profiles/tests/test_stereotypepage.py
@@ -1,33 +1,34 @@
+"""Test Stereotype Property Page."""
+
 from gaphor import UML
 from gaphor.diagram.classes.klass import ClassItem
 from gaphor.diagram.profiles.stereotypepropertypages import StereotypePage
-from gaphor.tests import TestCase
 
 
-class MetaclassEditorTest(TestCase):
-    def test_stereotype_page_with_no_stereotype(self):
-        ci = self.create(ClassItem, UML.Class)
-        ci.subject.name = "Class"
-        editor = StereotypePage(ci)
-        page = editor.construct()
-        assert page is None
+def test_stereotype_page_with_no_stereotype(element_factory, diagram):
+    ci = diagram.create(ClassItem, subject=element_factory.create(UML.Class))
+    ci.subject.name = "Class"
+    editor = StereotypePage(ci)
+    page = editor.construct()
+    assert page is None
 
-    def test_stereotype_page_with_stereotype(self):
-        # Create an stereotype applicable to Class types:
-        metaclass = self.element_factory.create(UML.Class)
-        metaclass.name = "Class"
-        stereotype = self.element_factory.create(UML.Stereotype)
-        stereotype.name = "NewStereotype"
-        UML.model.create_extension(metaclass, stereotype)
-        attr = self.element_factory.create(UML.Property)
-        attr.name = "Property"
-        # stereotype.ownedAttribute = attr
 
-        ci = self.create(ClassItem, UML.Class)
-        ci.subject.name = "Foo"
-        editor = StereotypePage(ci)
-        page = editor.construct()
+def test_stereotype_page_with_stereotype(element_factory, diagram):
+    # Create a stereotype applicable to Class types:
+    metaclass = element_factory.create(UML.Class)
+    metaclass.name = "Class"
+    stereotype = element_factory.create(UML.Stereotype)
+    stereotype.name = "NewStereotype"
+    UML.model.create_extension(metaclass, stereotype)
+    attr = element_factory.create(UML.Property)
+    attr.name = "Property"
+    # stereotype.ownedAttribute = attr
 
-        editor.refresh()
-        assert len(editor.model) == 1
-        assert page is not None
+    ci = diagram.create(ClassItem, subject=element_factory.create(UML.Class))
+    ci.subject.name = "Foo"
+    editor = StereotypePage(ci)
+    page = editor.construct()
+
+    editor.refresh()
+    assert len(editor.model) == 1
+    assert page is not None
