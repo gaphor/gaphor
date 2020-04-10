1ec69a8c87a677689118300a93f394a9a432d50d
Dan Yeaw <dan@yeaw.me>
2020-04-10 10:54:29 -0400

    Fix msys2 package names

--- win-installer/msys2-install.sh
+++ win-installer/msys2-install.sh
@@ -15,11 +15,10 @@ pacman --noconfirm -S --needed \
     mingw-w64-"$MSYS2_ARCH"-pkg-config \
     mingw-w64-"$MSYS2_ARCH"-cairo \
     mingw-w64-"$MSYS2_ARCH"-gobject-introspection \
-    mingw-w64-"$MSYS2_ARCH"-python3 \
-    mingw-w64-"$MSYS2_ARCH"-python3-gobject \
-    mingw-w64-"$MSYS2_ARCH"-python3-cairo \
-    mingw-w64-"$MSYS2_ARCH"-python3-pip \
-    mingw-w64-"$MSYS2_ARCH"-python3-setuptools
+    mingw-w64-"$MSYS2_ARCH"-python \
+    mingw-w64-"$MSYS2_ARCH"-python-gobject \
+    mingw-w64-"$MSYS2_ARCH"-python-cairo \
+    mingw-w64-"$MSYS2_ARCH"-python-pip \
 
 source "$DIR"/../venv
 pip install pyinstaller
