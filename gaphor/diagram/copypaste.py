2e9dc3838bcee87dab4d5ee5d26656dac8b0fa55
Arjan Molenaar <gaphor@gmail.com>
2020-04-27 23:17:47 +0200

    Make copies of diagram items
    Still assumes the model will not change underneath.

--- gaphor/diagram/copypaste.py
+++ gaphor/diagram/copypaste.py
@@ -6,24 +6,38 @@ values or reference id's.
 
 The `paste()` function will resolve those values. Based on the elements
 that are already in the model,
+
+The copy() function returns only data that has to be part of the copy buffer.
+the `paste()` function will load this data in a model.
 """
 
 from functools import singledispatch
-from typing import Callable, Dict, NamedTuple, Tuple, Type
+from typing import Callable, Dict, List, NamedTuple, Tuple, Type, TypeVar
 
 import gaphas
 
-from gaphor.core.modeling import Element, Presentation
+from gaphor.core.modeling import Diagram, Element, Presentation
 from gaphor.core.modeling.collection import collection
+
+T = TypeVar("T")
 
 
 @singledispatch
-def copy(obj):
+def copy(obj: Element) -> T:
+    """
+    Create a copy of an element (or list of elements).
+    The returned type should be distinct, so the `paste()`
+    function can properly dispatch.
+    """
     raise ValueError(f"No copier for {obj}")
 
 
 @singledispatch
-def paste(diagram, copy_data, lookup: Callable[[str], Element]):
+def paste(copy_data: T, diagram: Diagram, lookup: Callable[[str], Element]):
+    """
+    Paste previously copied data. Based on the data type created in the
+    `copy()` function, try to duplicate the copied elements.
+    """
     raise ValueError(f"No paster for {copy_data}")
 
 
@@ -32,8 +46,8 @@ class PresentationCopy(NamedTuple):
     data: Dict[str, Tuple[str, str]]
 
 
-@copy.register(Presentation)
-def _copy_presentation(item):
+@copy.register
+def _copy_presentation(item: Presentation) -> PresentationCopy:
     buffer = {}
 
     def save_func(name, value):
@@ -49,14 +63,45 @@ def _copy_presentation(item):
     return PresentationCopy(cls=item.__class__, data=buffer)
 
 
-@paste.register(PresentationCopy)
+@paste.register
 def _paste_presentation(copy_data: PresentationCopy, diagram, lookup):
     cls, data = copy_data
     item = diagram.create(cls)
     for key, (vtype, value) in data.items():
         if vtype == "r":
             other = lookup(value)
-            item.load(key, other)
+            if other:
+                item.load(key, other)
         elif vtype == "v":
             item.load(key, value)
+    item.canvas.update_matrices([item])
     item.postload()
+    return item
+
+
+class ListOfData(NamedTuple):
+    data: Dict[str, object]
+
+
+@copy.register
+def _copy_all(items: set) -> ListOfData:
+    return ListOfData(data={item.id: copy(item) for item in items})
+
+
+@paste.register
+def _paste_all(copy_data: ListOfData, diagram, lookup):
+    new_data: Dict[str, Element] = {}
+
+    def _lookup(ref: str):
+        if ref in new_data:
+            return new_data[ref]
+        elif ref in copy_data.data:
+            new_data[ref] = paste(copy_data.data[ref], diagram, _lookup)
+            return new_data[ref]
+        else:
+            return lookup(ref)
+
+    for old_id, data in copy_data.data.items():
+        new_data[old_id] = paste(data, diagram, _lookup)
+
+    return set(new_data.values())
--- gaphor/diagram/tests/test_copypaste.py
+++ gaphor/diagram/tests/test_copypaste.py
@@ -2,7 +2,8 @@ import pytest
 
 from gaphor import UML
 from gaphor.diagram.copypaste import copy, paste
-from gaphor.UML.classes import ClassItem
+from gaphor.diagram.tests.fixtures import connect
+from gaphor.UML.classes import ClassItem, GeneralizationItem
 
 
 def test_copy_item_adds_new_item_to_the_diagram(diagram, element_factory):
@@ -28,3 +29,32 @@ def test_copied_item_references_same_model_element(diagram, element_factory):
     item1, item2 = diagram.canvas.get_root_items()
 
     assert item1.subject is item2.subject
+
+
+def test_copy_multiple_items(diagram, element_factory):
+    cls = element_factory.create(UML.Class)
+    cls_item1 = diagram.create(ClassItem, subject=cls)
+    cls_item2 = diagram.create(ClassItem, subject=cls)
+
+    buffer = copy({cls_item1, cls_item2})
+
+    paste(buffer, diagram, element_factory.lookup)
+
+    assert len(diagram.canvas.get_root_items()) == 4
+    assert len(element_factory.lselect(lambda e: isinstance(e, UML.Class))) == 1
+
+
+def test_copy_item_without_copying_connection(diagram, element_factory):
+    cls = element_factory.create(UML.Class)
+    cls_item = diagram.create(ClassItem, subject=cls)
+    gen_item = diagram.create(GeneralizationItem)
+    connect(gen_item, gen_item.handles()[0], cls_item)
+
+    buffer = copy({cls_item})
+
+    new_items = paste(buffer, diagram, element_factory.lookup)
+
+    assert len(diagram.canvas.get_root_items()) == 3
+    assert len(element_factory.lselect(lambda e: isinstance(e, UML.Class))) == 1
+    assert type(new_items) is set
+    assert len(new_items) == 1
