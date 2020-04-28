a6fc7bd1e2184c290395551d09b156c224d10731
Arjan Molenaar <gaphor@gmail.com>
2020-04-28 23:52:01 +0200

    Can copy simple elements with relationships

--- gaphor/diagram/tests/test_copypaste.py
+++ gaphor/diagram/tests/test_copypaste.py
@@ -60,8 +60,61 @@ def test_copy_item_without_copying_connection(diagram, element_factory):
     assert len(new_items) == 1
 
 
+def two_classes_and_a_generalization(diagram, element_factory):
+    gen_cls = element_factory.create(UML.Class)
+    spc_cls = element_factory.create(UML.Class)
+    gen_cls_item = diagram.create(ClassItem, subject=gen_cls)
+    spc_cls_item = diagram.create(ClassItem, subject=spc_cls)
+    gen_item = diagram.create(GeneralizationItem)
+    connect(gen_item, gen_item.handles()[0], gen_cls_item)
+    connect(gen_item, gen_item.handles()[1], spc_cls_item)
+
+    return gen_cls_item, spc_cls_item, gen_item
+
+
+def clear_model(diagram, element_factory):
+    for element in list(element_factory.values()):
+        if element is not diagram:
+            element.unlink()
+
+    for item in diagram.canvas.get_all_items():
+        item.unlink()
+
+
+def test_copy_item_with_connection(diagram, element_factory):
+    gen_cls_item, spc_cls_item, gen_item = two_classes_and_a_generalization(
+        diagram, element_factory
+    )
+    buffer = copy({gen_cls_item, gen_item, spc_cls_item})
+
+    new_items = paste(buffer, diagram, element_factory.lookup)
+    new_gen_item = next(i for i in new_items if isinstance(i, GeneralizationItem))
+
+    new_cls_item1 = new_gen_item.canvas.get_connection(
+        new_gen_item.handles()[0]
+    ).connected
+    new_cls_item2 = new_gen_item.canvas.get_connection(
+        new_gen_item.handles()[1]
+    ).connected
+
+    assert new_cls_item1 in diagram.canvas.get_root_items()
+    assert new_cls_item2 in diagram.canvas.get_root_items()
+
+    assert len(new_items) == 3
+    assert new_cls_item1 in new_items
+    assert new_cls_item2 in new_items
+
+    # Model elements are not copied
+    assert len(element_factory.lselect(lambda e: isinstance(e, UML.Class))) == 2
+    assert (
+        len(element_factory.lselect(lambda e: isinstance(e, UML.Generalization))) == 1
+    )
+
+
 def test_copy_item_when_subject_has_been_removed(diagram, element_factory):
+    package = element_factory.create(UML.Package)
     cls = element_factory.create(UML.Class)
+    cls.package = package
     orig_cls_id = cls.id
     cls_item = diagram.create(ClassItem, subject=cls)
 
@@ -71,19 +124,38 @@ def test_copy_item_when_subject_has_been_removed(diagram, element_factory):
     cls.unlink()  # normally handled by the sanitizer service
 
     assert len(diagram.canvas.get_all_items()) == 0
-    assert len(element_factory.lselect()) == 1
+    assert len(element_factory.lselect()) == 2
     assert not element_factory.lookup(orig_cls_id)
 
-    try:
-        new_items = paste(buffer, diagram, element_factory.lookup)
-    except:
-        import pprint
+    print(buffer)
 
-        pprint.pprint(buffer)
-        raise
+    new_items = paste(buffer, diagram, element_factory.lookup)
+    new_cls = element_factory.lselect(lambda e: isinstance(e, UML.Class))[0]
     assert len(diagram.canvas.get_root_items()) == 1
-    assert len(element_factory.lselect(lambda e: isinstance(e, UML.Class))) == 1
-    assert element_factory.lookup(orig_cls_id)
+    assert new_cls.package is package
+    assert element_factory.lookup(orig_cls_id) is new_cls
+
+
+def test_copy_remove_paste_items_with_connections(diagram, element_factory):
+    gen_cls_item, spc_cls_item, gen_item = two_classes_and_a_generalization(
+        diagram, element_factory
+    )
+
+    buffer = copy({gen_cls_item, gen_item, spc_cls_item})
+
+    clear_model(diagram, element_factory)
+
+    print(buffer)
+
+    new_items = paste(buffer, diagram, element_factory.lookup)
+    new_cls1, new_cls2 = element_factory.lselect(lambda e: isinstance(e, UML.Class))
+    new_gen = element_factory.lselect(lambda e: isinstance(e, UML.Generalization))[0]
+
+    assert new_gen.general in {new_cls1, new_cls2}
+    assert new_gen.specific in {new_cls1, new_cls2}
+    assert new_cls1.presentation[0] in new_items
+    assert new_cls2.presentation[0] in new_items
+    assert new_gen.presentation[0] in new_items
 
 
 # copy/paste non-presentation element
