73c7839d31dce9903d7723e48de82d333610e388
Arjan Molenaar <gaphor@gmail.com>
2020-05-16 11:18:43 +0200

    Add element-item mapping to block and requirement

--- gaphor/SysML/blocks/block.py
+++ gaphor/SysML/blocks/block.py
@@ -1,6 +1,9 @@
+from gaphor.diagram.support import represents
+from gaphor.SysML.sysml import Block
 from gaphor.UML.classes import ClassItem
 
 
+@represents(Block)
 class BlockItem(ClassItem):
     def additional_stereotypes(self):
         return ["block"]
--- gaphor/SysML/requirements/requirement.py
+++ gaphor/SysML/requirements/requirement.py
@@ -1,6 +1,9 @@
+from gaphor.diagram.support import represents
+from gaphor.SysML.sysml import Requirement
 from gaphor.UML.classes import ClassItem
 
 
+@represents(Requirement)
 class RequirementItem(ClassItem):
     def additional_stereotypes(self):
         return ["requirement"]
