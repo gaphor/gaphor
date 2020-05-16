860f295b8928ae6d2039ccf6726a7ab29382851f
Arjan Molenaar <gaphor@gmail.com>
2020-05-16 11:53:01 +0200

    Add relationships for requirements

--- gaphor/SysML/requirements/__init__.py
+++ gaphor/SysML/requirements/__init__.py
@@ -1,1 +1,8 @@
+from gaphor.SysML.requirements.relationships import (
+    DeriveReqtItem,
+    RefineItem,
+    SatisfyItem,
+    TraceItem,
+    VerifyItem,
+)
 from gaphor.SysML.requirements.requirement import RequirementItem
