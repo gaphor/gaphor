bd10c37cf2c88a6289704456e97d3eddbd36b365
Arjan Molenaar <gaphor@gmail.com>
2020-05-17 22:33:55 +0200

    Remove old reminiscent from setup.py era

--- utils/install_lib.py
+++
@@ -1,8 +0,0 @@
-from setuptools.command.install_lib import install_lib as _install_lib
-
-
-class install_lib(_install_lib):
-    def build(self):
-        _install_lib.build(self)
-        self.run_command("build_uml")
-        self.run_command("build_mo")
