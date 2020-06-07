01001eceb60b157c7867e9403ff71a7cd7423133
Arjan Molenaar <gaphor@gmail.com>
2020-06-07 23:20:24 +0200

    Can edit style sheet through element editor

--- gaphor/ui/elementeditor.py
+++ gaphor/ui/elementeditor.py
@@ -9,8 +9,8 @@ from gi.repository import Gtk
 
 from gaphor.abc import ActionProvider
 from gaphor.core import action, event_handler, gettext
-from gaphor.core.modeling import Presentation
-from gaphor.core.modeling.event import AssociationUpdated
+from gaphor.core.modeling import Presentation, StyleSheet
+from gaphor.core.modeling.event import AssociationUpdated, AttributeUpdated, ModelReady
 from gaphor.diagram.propertypages import PropertyPages
 from gaphor.ui.abc import UIComponent
 from gaphor.ui.event import DiagramSelectionChanged
@@ -45,26 +45,31 @@ class ElementEditor(UIComponent, ActionProvider):
         self.vbox: Optional[Gtk.Box] = None
         self._current_item = None
         self._expanded_pages = {gettext("Properties"): True}
+        self._on_style_sheet_changed_id = -1
 
     def open(self):
         """Display the ElementEditor pane."""
-        builder = new_builder("elementeditor")
+        builder = new_builder("elementeditor", "style-sheet-buffer")
 
         self.revealer = builder.get_object("elementeditor")
+        self.editor_stack = builder.get_object("editor-stack")
         self.vbox = builder.get_object("editors")
 
         current_view = self.diagrams.get_current_view()
         self._selection_change(focused_item=current_view and current_view.focused_item)
 
-        # Make sure we receive
         self.event_manager.subscribe(self._selection_change)
         self.event_manager.subscribe(self._element_changed)
 
-        return self.revealer
+        self.enable_style_sheet = builder.get_object("enable-style-sheet")
+        self.style_sheet_buffer = builder.get_object("style-sheet-buffer")
 
-    @action(name="win.show-editors", shortcut="<Primary>e", state=True)
-    def toggle_editor_visibility(self, active):
-        self.revealer.set_reveal_child(active)
+        self.event_manager.subscribe(self._model_ready)
+        self._on_style_sheet_changed_id = self.style_sheet_buffer.connect(
+            "changed", self.on_style_sheet_changed
+        )
+
+        return self.revealer
 
     def close(self, widget=None):
         """Hide the element editor window and deactivate the toolbar button.
@@ -73,9 +78,16 @@ class ElementEditor(UIComponent, ActionProvider):
 
         self.event_manager.unsubscribe(self._selection_change)
         self.event_manager.unsubscribe(self._element_changed)
+        self.event_manager.unsubscribe(self._model_ready)
         self.vbox = None
         self._current_item = None
         return True
+
+    @action(name="show-editors", shortcut="<Primary>e", state=True)
+    def toggle_editor_visibility(self, active):
+        self.revealer.set_reveal_child(active)
+
+    ## Diagram item editor
 
     def _get_adapters(self, item):
         """
@@ -161,9 +173,49 @@ class ElementEditor(UIComponent, ActionProvider):
             show_tips.set_active(self.properties.get("show-tips", True))
 
     @event_handler(AssociationUpdated)
-    def _element_changed(self, event):
+    def _element_changed(self, event: AssociationUpdated):
         if event.property is Presentation.subject:  # type: ignore[misc] # noqa: F821
             element = event.element
             if element is self._current_item:
                 self.clear_pages()
                 self.create_pages(self._current_item)
+
+    ## Settings stack
+
+    @action(name="show-settings", state=False)
+    def toggle_editor_settings(self, active):
+        self.editor_stack.set_visible_child_name("settings" if active else "editors")
+
+    @action(name="enable-style-sheet", state=False)
+    def toggle_enable_style_sheet(self, active):
+        style_sheets = self.element_factory.lselect(StyleSheet)
+        if active and not style_sheets:
+            style_sheet = self.element_factory.create(StyleSheet)
+            style_sheet.style_sheet = "* { }"
+        elif not active and style_sheets:
+            for style_sheet in style_sheets:
+                # Trigger style update on diagrams:
+                style_sheet.styleSheet = ""
+                style_sheet.unlink()
+
+    def on_style_sheet_changed(self, buffer):
+        style_sheets = self.element_factory.lselect(StyleSheet)
+        if style_sheets:
+            text = buffer.get_text(
+                buffer.get_start_iter(), buffer.get_end_iter(), False
+            )
+            style_sheet = style_sheets[0]
+            if style_sheet.styleSheet != text:
+                style_sheet.styleSheet = text
+
+    @event_handler(ModelReady)
+    def _model_ready(self, event):
+        style_sheets = self.element_factory.lselect(StyleSheet)
+
+        self.enable_style_sheet.set_active(bool(style_sheets))
+
+        self.style_sheet_buffer.handler_block(self._on_style_sheet_changed_id)
+        self.style_sheet_buffer.set_text(
+            style_sheets[0].styleSheet if style_sheets else ""
+        )
+        self.style_sheet_buffer.handler_unblock(self._on_style_sheet_changed_id)
