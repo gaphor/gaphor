94c8ce576e4076b54a835a83a7b5ebb8b7c32396
Arjan Molenaar <gaphor@gmail.com>
2020-07-23 22:29:33 +0200

    Only allow grouping for association properties in own block
    You can group properties in a block, but if the property was defined as
    part of an association, you can only add it to the currently owning
    block.

--- gaphor/SysML/blocks/grouping.py
+++ gaphor/SysML/blocks/grouping.py
@@ -9,8 +9,16 @@ class PropertyGroup(AbstractGroup):
     Add Property to a Block.
     """
 
+    def can_contain(self) -> bool:
+        return (
+            not self.item.subject.association
+            or self.item.subject.owner is self.parent.subject
+        )
+
     def group(self):
-        self.parent.subject.ownedAttribute = self.item.subject
+        if not self.item.subject.association:
+            self.parent.subject.ownedAttribute = self.item.subject
 
     def ungroup(self):
-        del self.parent.subject.ownedAttribute[self.item.subject]
+        if not self.item.subject.association:
+            del self.parent.subject.ownedAttribute[self.item.subject]
