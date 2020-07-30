de0a831858494311938d7ffed122cb476afd5e10
Dan Yeaw <dan@yeaw.me>
2020-07-29 20:34:26 -0400

    Remove hook for csssselect2

--- win-installer/hooks/hook-cssselect2.py
+++
@@ -1,7 +0,0 @@
-"""This hook can be removed onces integrated in PyInstaller"""
-
-from PyInstaller.utils.hooks import collect_data_files
-
-
-def hook(hook_api):
-    hook_api.add_datas(collect_data_files(hook_api.__name__))
