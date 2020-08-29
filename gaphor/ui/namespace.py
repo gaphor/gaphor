7be6bf5ab3d3cfd6ddbaa7fdd412379710404650
Arjan Molenaar <gaphor@gmail.com>
2020-08-29 16:03:02 +0200

    Allow diagram to be created on every element in a package

--- gaphor/ui/namespace.py
+++ gaphor/ui/namespace.py
@@ -493,7 +493,6 @@ class Namespace(UIComponent):
 
     def _on_view_event(self, view, event):
         """Show a popup menu if button3 was pressed on the TreeView."""
-        # handle mouse button 3:"
         if event.type == Gdk.EventType.BUTTON_PRESS and event.button.button == 3:
             menu = Gtk.Menu.new_from_model(self.namespace_popup_model())
             menu.attach_to_widget(view, None)
@@ -512,10 +511,7 @@ class Namespace(UIComponent):
         action_group.lookup_action("open").set_enabled(isinstance(element, Diagram))
         action_group.lookup_action("create-diagram").set_enabled(
             isinstance(element, UML.Package)
-            or (
-                isinstance(element, UML.Namespace)
-                and isinstance(element.namespace, UML.Package)
-            )
+            or (element and isinstance(element.owner, UML.Package))
         )
         action_group.lookup_action("create-package").set_enabled(
             isinstance(element, UML.Package)
