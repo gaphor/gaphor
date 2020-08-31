17b41c92be1d89bf952166a90a775a52bac53586
Dan Yeaw <dan@yeaw.me>
2020-08-30 21:15:23 -0400

    Fix typelib for HarfBuzz not found error

---
+++ win-installer/hooks/pre_safe_import_module/hook-gi.repository.HarfBuzz.py
@@ -0,0 +1,12 @@
+"""Temporary hook to support HarfBuzz
+
+Work around for https://github.com/pyinstaller/pyinstaller/issues/5129
+
+"""
+
+def pre_safe_import_module(api):
+    # PyGObject modules loaded through the gi repository are marked as
+    # MissingModules by modulegraph so we convert them to
+    # RuntimeModules so their hooks are loaded and run.
+    api.add_runtime_module(api.module_name)
+
