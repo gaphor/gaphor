a5526ebd21999555776cfe312e23ef69da22f192
Sourcery AI <>
2020-09-29 20:29:18 +0000

    'Refactored by Sourcery'

--- gaphor/ui/errorhandler.py
+++ gaphor/ui/errorhandler.py
@@ -16,13 +16,7 @@ from gaphor.i18n import gettext
 def error_handler(message, secondary_message="", window=None):
     exc_type, exc_value, exc_traceback = sys.exc_info()
 
-    if 0 and __debug__ and exc_traceback and sys.stdin.isatty():
-        buttons = Gtk.ButtonsType.YES_NO
-        secondary_message += ("\n\n" if secondary_message else "") + gettext(
-            "It looks like Gaphor is started from the command line.\nDo you want to open a debug session?"
-        )
-    else:
-        buttons = Gtk.ButtonsType.OK
+    buttons = Gtk.ButtonsType.OK
 
     dialog = Gtk.MessageDialog(
         None,
