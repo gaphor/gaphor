ef7d84677969f304157e2890629b4e676901bd15
Arjan Molenaar <gaphor@gmail.com>
2020-09-30 21:00:19 +0200

    More descriptive buttons

--- gaphor/ui/errorhandler.py
+++ gaphor/ui/errorhandler.py
@@ -16,25 +16,26 @@ from gaphor.i18n import gettext
 def error_handler(message, secondary_message="", window=None):
     exc_type, exc_value, exc_traceback = sys.exc_info()
 
-    if __debug__ and exc_traceback and sys.stdin.isatty():
-        buttons = Gtk.ButtonsType.YES_NO
-        secondary_message += ("\n\n" if secondary_message else "") + gettext(
-            "It looks like Gaphor is started from the command line.\nDo you want to open a debug session?"
-        )
-    else:
-        buttons = Gtk.ButtonsType.OK
-
     dialog = Gtk.MessageDialog(
         None,
         Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
         Gtk.MessageType.ERROR,
-        buttons,
     )
     dialog.props.text = message
-    dialog.props.secondary_text = secondary_message
+
+    if __debug__ and exc_traceback and sys.stdin.isatty():
+        dialog.props.secondary_text = ("\n\n" if secondary_message else "") + gettext(
+            "It looks like Gaphor is started from the command line.\nDo you want to open a debug session?"
+        )
+        dialog.add_button(gettext("Close"), 0)
+        dialog.add_button(gettext("Start debug session"), 100)
+    else:
+        dialog.props.secondary_text = secondary_message
+        dialog.add_button(gettext("OK"), 0)
+
     dialog.set_transient_for(window)
     answer = dialog.run()
     dialog.destroy()
 
-    if exc_traceback and answer == Gtk.ResponseType.YES:
+    if exc_traceback and answer == 100:
         pdb.post_mortem(exc_traceback)
