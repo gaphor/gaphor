d99027d0d3aad17b80a77d75f5e420a8c3897fce
Dan Yeaw <dan@yeaw.me>
2020-11-03 06:43:40 -0500

    Remove grouping of partitions

--- gaphor/UML/actions/actionsgrouping.py
+++ gaphor/UML/actions/actionsgrouping.py
@@ -5,35 +5,6 @@ from gaphor.UML.actions.objectnode import ObjectNodeItem
 from gaphor.UML.actions.partition import PartitionItem
 
 
-@Group.register(PartitionItem, PartitionItem)
-class ActivityPartitionsGroup(AbstractGroup):
-    """Group activity partitions."""
-
-    def can_contain(self):
-        return not self.parent.subject or (
-            self.parent.subject and len(self.parent.subject.node) == 0
-        )
-
-    def group(self):
-        # self.item.parent = self.parent
-        pass
-
-    def ungroup(self):
-        # self.item.parent = None
-        # ungroup activity nodes
-        canvas = self.item.canvas
-        if canvas:
-            nodes = [
-                n
-                for n in canvas.get_children(self.item)
-                if isinstance(
-                    n, (ActivityNodeItem, ActionItem, ObjectNodeItem, ForkNodeItem)
-                )
-            ]
-            for n in nodes:
-                canvas.reparent(n, None)
-
-
 class ActivityNodePartitionGroup(AbstractGroup):
     """Group activity nodes within activity partition."""
 
