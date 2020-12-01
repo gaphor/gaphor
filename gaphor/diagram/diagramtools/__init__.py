cd3608077ed6064f82d2f6b22ab5795ba64330c1
Arjan Molenaar <gaphor@gmail.com>
2020-12-01 21:10:46 +0100

    Convert text edit tool to a gesture/controller

--- gaphor/diagram/diagramtools/__init__.py
+++ gaphor/diagram/diagramtools/__init__.py
@@ -10,12 +10,12 @@ from gaphas.tool import hover_tool, item_tool, rubberband_tool
 
 from gaphor.diagram.diagramtools.dropzone import DropZoneMove
 from gaphor.diagram.diagramtools.placement import Connector, PlacementTool
-from gaphor.diagram.diagramtools.textedit import TextEditTool
+from gaphor.diagram.diagramtools.textedit import text_edit_tools
 
 
 def apply_default_tool_set(view, event_manager, rubberband_state):
     """The default tool set."""
     view.add_controller(hover_tool(view))
     view.add_controller(item_tool(view))
-    # chain.append(TextEditTool(view))
+    view.add_controller(*text_edit_tools(view))
     view.add_controller(rubberband_tool(view, rubberband_state))
--- gaphor/diagram/diagramtools/textedit.py
+++ gaphor/diagram/diagramtools/textedit.py
@@ -1,30 +1,26 @@
-from gi.repository import Gdk
+from gi.repository import Gdk, Gtk
 
 from gaphor.diagram.inlineeditors import InlineEditor
 
 
-class TextEditTool:
-    """Text edit tool.
+def text_edit_tools(view):
+    key_tool = Gtk.EventControllerKey.new(view)
+    key_tool.connect("key-pressed", on_key_pressed)
+    click_tool = Gtk.GestureMultiPress.new(view)
+    click_tool.connect("released", on_double_click)
+    return key_tool, click_tool
 
-    Allows for elements that can adapt to the IEditable interface to be
-    edited.
-    """
 
-    def on_key_press(self, event):
-        view = self.view
-        item = view.selection.hovered_item
-        if (
-            item
-            and event.type == Gdk.EventType.KEY_PRESS
-            and event.key.keyval == Gdk.KEY_F2
-        ):
-            return InlineEditor(item, view)
-        return False
+def on_key_pressed(controller, keyval, keycode, state):
+    view = controller.get_widget()
+    item = view.selection.hovered_item
+    if item and keyval == Gdk.KEY_F2:
+        return InlineEditor(item, view)
 
-    def on_double_click(self, event):
-        view = self.view
-        item = view.selection.hovered_item
-        if item:
-            x, y = view.get_matrix_v2i(item).transform_point(event.x, event.y)
-            return InlineEditor(item, view, (x, y))
-        return False
+
+def on_double_click(gesture, n_press, x, y):
+    view = gesture.get_widget()
+    item = view.selection.hovered_item
+    if item and n_press == 2:
+        ix, iy = view.get_matrix_v2i(item).transform_point(x, y)
+        return InlineEditor(item, view, (ix, iy))
