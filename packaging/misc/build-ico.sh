eee37b2f84b7eb59bb2e7f762d46f55d120d6099
Dan Yeaw <dan@yeaw.me>
2020-12-13 15:19:10 -0500

    Deconflict convert with disk utility in Windows

--- packaging/misc/build-ico.sh
+++ packaging/misc/build-ico.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-rsvg-convert ../../logos/org.gaphor.Gaphor.svg | convert -scale 48x48 - gaphor.ico
+rsvg-convert ../../logos/org.gaphor.Gaphor.svg | magick convert -scale 48x48 - gaphor.ico
 
-# ../_build_root/mingw64/bin/magick convert ../../logos/gaphor-48x48.png gaphor2.ico
