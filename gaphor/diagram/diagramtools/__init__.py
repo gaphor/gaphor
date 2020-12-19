2ec189e7bd88aec2ed9ad861011bfee2fb3ac514
Arjan Molenaar <gaphor@gmail.com>
2020-12-19 15:23:08 +0100

    Update Gaphas to b2, now we have a tool to grab focus again

--- gaphor/diagram/diagramtools/__init__.py
+++ gaphor/diagram/diagramtools/__init__.py
@@ -4,7 +4,14 @@ TODO: make tools transactional.
 """
 
 from gaphas.segment import segment_tool
-from gaphas.tool import hover_tool, item_tool, rubberband_tool, scroll_tool, zoom_tool
+from gaphas.tool import (
+    hover_tool,
+    item_tool,
+    rubberband_tool,
+    scroll_tool,
+    view_focus_tool,
+    zoom_tool,
+)
 
 from gaphor.diagram.diagramtools.dropzone import drop_zone_tool
 from gaphor.diagram.diagramtools.placement import new_item_factory, placement_tool
@@ -26,6 +33,7 @@ def apply_default_tool_set(view, modeling_language, event_manager, rubberband_st
     view.add_controller(rubberband_tool(view, rubberband_state))
     view.add_controller(scroll_tool(view))
     view.add_controller(zoom_tool(view))
+    view.add_controller(view_focus_tool(view))
     view.add_controller(shortcut_tool(view, modeling_language, event_manager))
 
 
@@ -40,3 +48,4 @@ def apply_placement_tool_set(view, item_factory, event_manager, handle_index):
     view.add_controller(drop_zone_tool(view, item_factory.item_class))
     view.add_controller(scroll_tool(view))
     view.add_controller(zoom_tool(view))
+    view.add_controller(view_focus_tool(view))
