010ceb75de5510e28a05968df952e7aeaf2c5a51
Arjan Molenaar <gaphor@gmail.com>
2021-02-14 21:12:47 +0100

    Add database shape

--- gaphor/C4Model/diagramitems/__init__.py
+++ gaphor/C4Model/diagramitems/__init__.py
@@ -1,3 +1,3 @@
 from gaphor.C4Model.diagramitems.container import C4ContainerItem
+from gaphor.C4Model.diagramitems.database import C4ContainerDatabaseItem
 from gaphor.C4Model.diagramitems.person import C4PersonItem
-from gaphor.UML.components import ComponentItem as C4ContainerDatabaseItem
---
+++ gaphor/C4Model/diagramitems/database.py
@@ -0,0 +1,63 @@
+from gaphas.util import path_ellipse
+
+from gaphor.C4Model import c4model
+from gaphor.core.styling import FontWeight, TextAlign, VerticalAlign
+from gaphor.diagram.presentation import ElementPresentation, Named
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
+from gaphor.diagram.support import represents
+
+
+@represents(c4model.C4Container)
+class C4ContainerDatabaseItem(ElementPresentation, Named):
+    def __init__(self, diagram, id=None):
+        super().__init__(diagram, id)
+
+        self.watch("subject[NamedElement].name")
+        self.watch("subject[C4Container].technology")
+        self.watch("subject[C4Container].description")
+        self.watch("subject[C4Container].type")
+
+    def update_shapes(self, event=None):
+        self.shape = Box(
+            Box(
+                EditableText(
+                    text=lambda: self.subject.name or "",
+                    style={"font-weight": FontWeight.BOLD},
+                ),
+                Text(
+                    text=lambda: self.subject.technology
+                    and f"[{self.subject.type}: {self.subject.technology}]"
+                    or f"[{self.subject.type}]",
+                    style={"font-size": "x-small"},
+                ),
+                Text(
+                    text=lambda: self.subject.description or "",
+                ),
+                style={"padding": (4, 4, 4, 4)},
+            ),
+            style={
+                "text-align": TextAlign.LEFT
+                if self.diagram and self.children
+                else TextAlign.CENTER,
+                "vertical-align": VerticalAlign.BOTTOM
+                if self.diagram and self.children
+                else VerticalAlign.MIDDLE,
+            },
+            draw=draw_database,
+        )
+
+
+def draw_database(box, context, bounding_box):
+    cr = context.cairo
+    d = 0.38
+    x, y, width, height = bounding_box
+
+    x1 = width + x
+    y1 = height + y
+    cr.move_to(x1, y)
+    path_ellipse(cr, width / 2, y, width, height * d)
+    cr.line_to(x1, y1)
+    cr.curve_to(x1, y1, width / 2, y1 + height * d, x, y1)
+    cr.line_to(x, y)
+
+    stroke(context)
