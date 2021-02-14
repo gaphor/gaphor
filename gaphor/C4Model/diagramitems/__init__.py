2b7823e9de8568754832734d0a060c6b1c8ef893
Arjan Molenaar <gaphor@gmail.com>
2021-02-14 20:10:31 +0100

    Add shape for person

--- gaphor/C4Model/diagramitems/__init__.py
+++ gaphor/C4Model/diagramitems/__init__.py
@@ -1,3 +1,3 @@
 from gaphor.C4Model.diagramitems.container import C4ContainerItem
+from gaphor.C4Model.diagramitems.person import C4PersonItem
 from gaphor.UML.components import ComponentItem as C4ContainerDatabaseItem
-from gaphor.UML.components import ComponentItem as C4PersonItem
