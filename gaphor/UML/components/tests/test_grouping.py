35130b025c6efc1c22e6f55a3cf4898d16a98d90
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2021-02-15 19:02:08 +0100

    Convert gaphor/UML/components/tests/test_grouping.py to pytest

--- gaphor/UML/components/tests/test_grouping.py
+++ gaphor/UML/components/tests/test_grouping.py
@@ -1,46 +1,45 @@
 from gaphor import UML
-from gaphor.tests import TestCase
 from gaphor.UML.components import ArtifactItem, ComponentItem, NodeItem
 from gaphor.UML.usecases import UseCaseItem
 
 
-class NodesGroupTestCase(TestCase):
+class TestNodesGroup:
     """Nodes grouping tests."""
 
-    def test_grouping(self):
+    def test_grouping(self, case):
         """Test node within another node composition."""
-        n1 = self.create(NodeItem, UML.Node)
-        n2 = self.create(NodeItem, UML.Node)
+        n1 = case.create(NodeItem, UML.Node)
+        n2 = case.create(NodeItem, UML.Node)
 
-        self.group(n1, n2)
+        case.group(n1, n2)
 
         assert n2.subject in n1.subject.nestedNode
         assert n1.subject not in n2.subject.nestedNode
 
-    def test_ungrouping(self):
+    def test_ungrouping(self, case):
         """Test decomposition of component from node."""
-        n1 = self.create(NodeItem, UML.Node)
-        n2 = self.create(NodeItem, UML.Node)
+        n1 = case.create(NodeItem, UML.Node)
+        n2 = case.create(NodeItem, UML.Node)
 
-        self.group(n1, n2)
-        self.ungroup(n1, n2)
+        case.group(n1, n2)
+        case.ungroup(n1, n2)
 
         assert n2.subject not in n1.subject.nestedNode
         assert n1.subject not in n2.subject.nestedNode
 
 
-class NodeComponentGroupTestCase(TestCase):
-    def test_grouping(self):
+class TestNodeComponentGroup:
+    def test_grouping(self, case):
         """Test component within node composition."""
-        n = self.create(NodeItem, UML.Node)
-        c = self.create(ComponentItem, UML.Component)
+        n = case.create(NodeItem, UML.Node)
+        c = case.create(ComponentItem, UML.Component)
 
-        self.group(n, c)
+        case.group(n, c)
 
         assert len(n.subject.ownedAttribute) == 1
         assert len(n.subject.ownedConnector) == 1
         assert len(c.subject.ownedAttribute) == 1
-        assert len(self.kindof(UML.ConnectorEnd)) == 2
+        assert len(case.kindof(UML.ConnectorEnd)) == 2
 
         a1 = n.subject.ownedAttribute[0]
         a2 = c.subject.ownedAttribute[0]
@@ -52,80 +51,80 @@ class NodeComponentGroupTestCase(TestCase):
         assert connector.end[0].role is a1
         assert connector.end[1].role is a2
 
-    def test_ungrouping(self):
+    def test_ungrouping(self, case):
         """Test decomposition of component from node."""
-        n = self.create(NodeItem, UML.Node)
-        c = self.create(ComponentItem, UML.Component)
+        n = case.create(NodeItem, UML.Node)
+        c = case.create(ComponentItem, UML.Component)
 
-        self.group(n, c)
-        self.ungroup(n, c)
+        case.group(n, c)
+        case.ungroup(n, c)
 
         assert len(n.subject.ownedAttribute) == 0
         assert len(c.subject.ownedAttribute) == 0
-        assert len(self.kindof(UML.Property)) == 0
-        assert len(self.kindof(UML.Connector)) == 0
-        assert len(self.kindof(UML.ConnectorEnd)) == 0
+        assert len(case.kindof(UML.Property)) == 0
+        assert len(case.kindof(UML.Connector)) == 0
+        assert len(case.kindof(UML.ConnectorEnd)) == 0
 
 
-class NodeArtifactGroupTestCase(TestCase):
-    def test_grouping(self):
+class TestNodeArtifactGroup:
+    def test_grouping(self, case):
         """Test artifact within node deployment."""
-        n = self.create(NodeItem, UML.Node)
-        a = self.create(ArtifactItem, UML.Artifact)
+        n = case.create(NodeItem, UML.Node)
+        a = case.create(ArtifactItem, UML.Artifact)
 
-        self.group(n, a)
+        case.group(n, a)
 
         assert len(n.subject.deployment) == 1
         assert n.subject.deployment[0].deployedArtifact[0] is a.subject
 
-    def test_ungrouping(self):
+    def test_ungrouping(self, case):
         """Test removal of artifact from node."""
-        n = self.create(NodeItem, UML.Node)
-        a = self.create(ArtifactItem, UML.Artifact)
+        n = case.create(NodeItem, UML.Node)
+        a = case.create(ArtifactItem, UML.Artifact)
 
-        self.group(n, a)
-        self.ungroup(n, a)
+        case.group(n, a)
+        case.ungroup(n, a)
 
         assert len(n.subject.deployment) == 0
-        assert len(self.kindof(UML.Deployment)) == 0
+        assert len(case.kindof(UML.Deployment)) == 0
 
 
-class SubsystemUseCaseGroupTestCase(TestCase):
-    def test_grouping(self):
+class TestSubsystemUseCaseGroup:
+    def test_grouping(self, case):
         """Test adding an use case to a subsystem."""
-        s = self.create(ComponentItem, UML.Component)
-        uc1 = self.create(UseCaseItem, UML.UseCase)
-        uc2 = self.create(UseCaseItem, UML.UseCase)
+        s = case.create(ComponentItem, UML.Component)
+        uc1 = case.create(UseCaseItem, UML.UseCase)
+        uc2 = case.create(UseCaseItem, UML.UseCase)
 
-        self.group(s, uc1)
+        case.group(s, uc1)
         assert len(uc1.subject.subject) == 1
-        self.group(s, uc2)
+        case.group(s, uc2)
         assert len(uc2.subject.subject) == 1
 
-        self.assertEqual(2, len(s.subject.useCase))
+        assert len(s.subject.useCase) == 2
 
-    def test_grouping_with_namespace(self):
+    def test_grouping_with_namespace(self, case):
         """Test adding an use case to a subsystem (with namespace)"""
-        s = self.create(ComponentItem, UML.Component)
-        uc = self.create(UseCaseItem, UML.UseCase)
+        s = case.create(ComponentItem, UML.Component)
+        uc = case.create(UseCaseItem, UML.UseCase)
 
-        self.group(s, uc)
+        case.group(s, uc)
         assert len(uc.subject.subject) == 1
         assert s.subject in uc.subject.subject
 
-    def test_ungrouping(self):
+    def test_ungrouping(self, case):
         """Test removal of use case from subsystem."""
-        s = self.create(ComponentItem, UML.Component)
-        uc1 = self.create(UseCaseItem, UML.UseCase)
-        uc2 = self.create(UseCaseItem, UML.UseCase)
+        s = case.create(ComponentItem, UML.Component)
+        uc1 = case.create(UseCaseItem, UML.UseCase)
+        uc2 = case.create(UseCaseItem, UML.UseCase)
 
-        self.group(s, uc1)
-        self.group(s, uc2)
+        case.group(s, uc1)
+        case.group(s, uc2)
 
-        self.ungroup(s, uc1)
+        case.ungroup(s, uc1)
         assert len(uc1.subject.subject) == 0
-        self.assertEqual(1, len(s.subject.useCase))
+        assert len(s.subject.useCase) == 1
 
-        self.ungroup(s, uc2)
+        case.ungroup(s, uc2)
         assert len(uc2.subject.subject) == 0
-        self.assertEqual(0, len(s.subject.useCase))
+        assert len(s.subject.useCase) == 0
