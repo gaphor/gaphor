f4c0d89e2f1d1e7e472b5a19a66402291790db2c
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2021-02-15 19:02:04 +0100

    Convert gaphor/UML/actions/tests/test_grouping.py to pytest

--- gaphor/UML/actions/tests/test_grouping.py
+++ gaphor/UML/actions/tests/test_grouping.py
@@ -1,70 +1,69 @@
 """Tests for grouping functionality in Gaphor."""
 
 from gaphor import UML
-from gaphor.tests import TestCase
 from gaphor.UML.actions import ActionItem, PartitionItem
 
 
-class PartitionGroupTestCase(TestCase):
-    def test_no_subpartition_when_nodes_in(self):
+class TestPartitionGroup:
+    def test_no_subpartition_when_nodes_in(self, case):
         """Test adding subpartition when nodes added."""
-        p = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        a1 = self.create(ActionItem, UML.Action)
-        p1 = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        p2 = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        p = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        a1 = case.create(ActionItem, UML.Action)
+        p1 = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        p2 = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
 
-        self.group(p, p1)
-        self.group(p1, a1)
-        assert not self.can_group(p1, p2)
+        case.group(p, p1)
+        case.group(p1, a1)
+        assert not case.can_group(p1, p2)
 
-    def test_action_grouping(self):
+    def test_action_grouping(self, case):
         """Test adding action to partition."""
-        p1 = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        p2 = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        a1 = self.create(ActionItem, UML.Action)
-        a2 = self.create(ActionItem, UML.Action)
+        p1 = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        p2 = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        a1 = case.create(ActionItem, UML.Action)
+        a2 = case.create(ActionItem, UML.Action)
 
-        assert self.can_group(p1, a1)
-        self.group(p1, a1)
+        assert case.can_group(p1, a1)
+        case.group(p1, a1)
         assert len(p1.subject.node) == 1
 
-        self.group(p1, p2)
-        self.group(p2, a1)
+        case.group(p1, p2)
+        case.group(p2, a1)
 
-        assert self.can_group(p2, a1)
+        assert case.can_group(p2, a1)
         assert len(p2.subject.node) == 1
-        self.group(p2, a2)
+        case.group(p2, a2)
         assert len(p2.subject.node) == 2
 
-    def test_ungrouping(self):
+    def test_ungrouping(self, case):
         """Test action and partition removal."""
-        p = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        a1 = self.create(ActionItem, UML.Action)
-        a2 = self.create(ActionItem, UML.Action)
+        p = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        a1 = case.create(ActionItem, UML.Action)
+        a2 = case.create(ActionItem, UML.Action)
 
-        self.group(p, a1)
-        self.group(p, a2)
+        case.group(p, a1)
+        case.group(p, a2)
 
-        self.ungroup(p, a1)
+        case.ungroup(p, a1)
         assert len(p.subject.node) == 1
-        self.ungroup(p, a2)
+        case.ungroup(p, a2)
         assert len(p.subject.node) == 0
 
-    def test_ungrouping_with_actions(self):
+    def test_ungrouping_with_actions(self, case):
         """Test partition with actions removal."""
-        p = self.create(PartitionItem, subject_cls=UML.ActivityPartition)
-        a1 = self.create(ActionItem, UML.Action)
-        a2 = self.create(ActionItem, UML.Action)
+        p = case.create(PartitionItem, subject_cls=UML.ActivityPartition)
+        a1 = case.create(ActionItem, UML.Action)
+        a2 = case.create(ActionItem, UML.Action)
 
-        self.group(p, a1)
-        self.group(p, a2)
+        case.group(p, a1)
+        case.group(p, a2)
 
         partition = p.subject
         assert len(partition.node) == 2, partition.node
         assert len(p.children) == 2, p.children
 
-        self.ungroup(p, a1)
-        self.ungroup(p, a2)
+        case.ungroup(p, a1)
+        case.ungroup(p, a2)
 
         assert 0 == len(partition.node)
         assert len(p.children) == 0
