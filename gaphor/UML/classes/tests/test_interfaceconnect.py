b3370370cf84af8a053b51025c0532e66de47c6c
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2021-02-15 19:02:06 +0100

    Convert gaphor/UML/classes/tests/test_interfaceconnect.py to pytest

--- gaphor/UML/classes/tests/test_interfaceconnect.py
+++ gaphor/UML/classes/tests/test_interfaceconnect.py
@@ -1,82 +1,81 @@
 """Test connections to folded interface."""
 
 from gaphor import UML
-from gaphor.tests import TestCase
 from gaphor.UML.classes.dependency import DependencyItem
 from gaphor.UML.classes.implementation import ImplementationItem
 from gaphor.UML.classes.interface import Folded, InterfaceItem
 from gaphor.UML.classes.klass import ClassItem
 
 
-class ImplementationTestCase(TestCase):
-    def test_default_line_style(self):
-        impl = self.create(ImplementationItem)
+class TestImplementation:
+    def test_default_line_style(self, case):
+        impl = case.create(ImplementationItem)
 
         assert impl.style["dash-style"]
 
-    def test_folded_interface_connection(self):
+    def test_folded_interface_connection(self, case):
         """Test connecting implementation to folded interface."""
-        iface = self.create(InterfaceItem, UML.Interface)
+        iface = case.create(InterfaceItem, UML.Interface)
         iface.folded = Folded.PROVIDED
-        impl = self.create(ImplementationItem)
+        impl = case.create(ImplementationItem)
 
-        self.connect(impl, impl.head, iface)
-        self.diagram.update_now((iface, impl))
+        case.connect(impl, impl.head, iface)
+        case.diagram.update_now((iface, impl))
 
         assert not impl.style["dash-style"]
 
-    def test_folded_interface_disconnection(self):
+    def test_folded_interface_disconnection(self, case):
         """Test disconnection implementation from folded interface."""
-        iface = self.create(InterfaceItem, UML.Interface)
+        iface = case.create(InterfaceItem, UML.Interface)
         iface.folded = Folded.PROVIDED
-        impl = self.create(ImplementationItem)
+        impl = case.create(ImplementationItem)
 
-        self.connect(impl, impl.head, iface)
-        self.disconnect(impl, impl.head)
+        case.connect(impl, impl.head, iface)
+        case.disconnect(impl, impl.head)
         impl.request_update()
 
         assert impl.style["dash-style"]
 
 
-class DependencyTestCase(TestCase):
-    def test_default_line_style(self):
-        dep = self.create(DependencyItem)
+class TestDependency:
+    def test_default_line_style(self, case):
+        dep = case.create(DependencyItem)
 
         assert dep.style["dash-style"]
 
-    def test_folded_interface_connection(self):
+    def test_folded_interface_connection(self, case):
         """Test connecting dependency to folded interface."""
-        clazz = self.create(ClassItem, UML.Class)
-        iface = self.create(InterfaceItem, UML.Interface)
+        clazz = case.create(ClassItem, UML.Class)
+        iface = case.create(InterfaceItem, UML.Interface)
         iface.folded = Folded.PROVIDED
-        dep = self.create(DependencyItem)
+        dep = case.create(DependencyItem)
 
-        self.connect(dep, dep.head, iface)
-        self.connect(dep, dep.tail, clazz)
+        case.connect(dep, dep.head, iface)
+        case.connect(dep, dep.tail, clazz)
         iface.request_update()
-        self.diagram.update_now((clazz, iface, dep))
+        case.diagram.update_now((clazz, iface, dep))
 
         assert dep.subject
         assert not dep.style["dash-style"]
         assert iface.folded == Folded.REQUIRED
 
-    def test_folded_interface_disconnection(self):
+    def test_folded_interface_disconnection(self, case):
         """Test disconnection dependency from folded interface."""
-        iface = self.create(InterfaceItem, UML.Interface)
+        iface = case.create(InterfaceItem, UML.Interface)
         iface.folded = Folded.PROVIDED
-        dep = self.create(DependencyItem)
+        dep = case.create(DependencyItem)
 
-        self.connect(dep, dep.head, iface)
-        self.disconnect(dep, dep.head)
+        case.connect(dep, dep.head, iface)
+        case.disconnect(dep, dep.head)
         dep.request_update()
 
         assert dep.style["dash-style"]
         assert iface.folded == Folded.PROVIDED
 
-    def test_unfolded_interface_connection(self):
+    def test_unfolded_interface_connection(self, case):
         """Test disconnection dependency from unfolded interface."""
-        iface = self.create(InterfaceItem, UML.Interface)
-        dep = self.create(DependencyItem)
+        iface = case.create(InterfaceItem, UML.Interface)
+        dep = case.create(DependencyItem)
 
-        self.connect(dep, dep.head, iface)
+        case.connect(dep, dep.head, iface)
         assert (7.0, 5.0) == dep.style["dash-style"]
