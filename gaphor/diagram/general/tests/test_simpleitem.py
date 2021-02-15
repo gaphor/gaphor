8a02dd61e4da550a3ece51eb884920f6e7b916cb
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2021-02-15 19:02:01 +0100

    Convert gaphor/diagram/general/tests/test_simpleitem.py to pytest

--- gaphor/diagram/general/tests/test_simpleitem.py
+++ gaphor/diagram/general/tests/test_simpleitem.py
@@ -1,21 +1,18 @@
 """Unit tests for simple items."""
 
 from gaphor.diagram.general.simpleitem import Ellipse, Line
-from gaphor.tests import TestCase
 
 
-class SimpleItemTestCase(TestCase):
-    def setUp(self):
-        super().setUp()
+def test_line(case):
+    """"""
+    case.diagram.create(Line)
 
-    def test_line(self):
-        """"""
-        self.diagram.create(Line)
 
-    def test_box(self):
-        """"""
-        self.diagram.create(Line)
+def test_box(case):
+    """"""
+    case.diagram.create(Line)
 
-    def test_ellipse(self):
-        """"""
-        self.diagram.create(Ellipse)
+
+def test_ellipse(case):
+    """"""
+    case.diagram.create(Ellipse)
