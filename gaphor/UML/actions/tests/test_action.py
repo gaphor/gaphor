756c55da9553b7868ac8be335bb4a02557742430
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2021-02-15 19:02:02 +0100

    Convert gaphor/UML/actions/tests/test_action.py to pytest

--- gaphor/UML/actions/tests/test_action.py
+++ gaphor/UML/actions/tests/test_action.py
@@ -1,11 +1,9 @@
 """Test actions."""
 
 from gaphor import UML
-from gaphor.tests.testcase import TestCase
 from gaphor.UML.actions.action import ActionItem
 
 
-class ActionTestCase(TestCase):
-    def test_action(self):
-        """Test creation of actions."""
-        self.create(ActionItem, UML.Action)
+def test_action(case):
+    """Test creation of actions."""
+    case.create(ActionItem, UML.Action)
