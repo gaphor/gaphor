c55fb72f351876881b687884c1ca0d40c9bed9ef
Dan Yeaw <dan@yeaw.me>
2021-03-01 22:32:37 -0500

    Add shapes for basic and conditional events
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/RAAML/fta/conditionalevent.py
+++ gaphor/RAAML/fta/conditionalevent.py
@@ -1,14 +1,16 @@
 """Conditional Event item definition."""
 
 from gaphas.geometry import Rectangle
+from gaphas.util import path_ellipse
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling.properties import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +44,22 @@ class ConditionalEventItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_conditional_event,
         )
 
 
 def draw_conditional_event(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+
+    rx = bounding_box.width - 40
+    ry = bounding_box.height - 50
+
+    cr.move_to(bounding_box.width, ry)
+    path_ellipse(cr, bounding_box.width / 2.0, ry / 2.0, rx, ry)
+    stroke(context)
