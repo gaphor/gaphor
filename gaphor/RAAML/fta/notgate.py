8455a06a22a0ad0db58beed1f05ffe480c9a18ef
Dan Yeaw <dan@yeaw.me>
2021-03-02 21:14:54 -0500

    Add shapes for not and transfer in gates
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/RAAML/fta/notgate.py
+++ gaphor/RAAML/fta/notgate.py
@@ -3,12 +3,13 @@
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +43,24 @@ class NOTItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_not_gate,
         )
 
 
 def draw_not_gate(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+    x1 = bounding_box.width / 4.0
+    x2 = bounding_box.width * 3.0 / 4.0
+    height = bounding_box.height - 40
+    cr.move_to(x1, height)
+    cr.line_to(x2, 4)
+    cr.line_to(x1, 4)
+    cr.line_to(x2, height)
+    cr.line_to(x1, height)
+    stroke(context)
--- gaphor/RAAML/fta/transferingate.py
+++ gaphor/RAAML/fta/transferingate.py
@@ -3,12 +3,13 @@
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +43,23 @@ class TransferInItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_transfer_in_gate,
         )
 
 
 def draw_transfer_in_gate(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+    x1 = bounding_box.width / 4.0
+    x2 = bounding_box.width * 3.0 / 4.0
+    height = bounding_box.height - 40
+    cr.move_to(x1, height)
+    cr.line_to(x2, height)
+    cr.line_to(bounding_box.width / 2.0, 4)
+    cr.line_to(x1, height)
+    stroke(context)
