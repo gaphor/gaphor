6688c150d7dfea99dad2edbd6bc4c4ff1dd94965
Dan Yeaw <dan@yeaw.me>
2021-03-03 22:17:46 -0500

    Add shapes for dormant, house, zero events
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/RAAML/fta/dormantevent.py
+++ gaphor/RAAML/fta/dormantevent.py
@@ -3,12 +3,13 @@
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +43,41 @@ class DormantEventItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_dormant_event,
         )
 
 
 def draw_dormant_event(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+
+    # Outer diamond
+    outer_x1 = bounding_box.width / 4.0
+    outer_x2 = bounding_box.width * 3.0 / 4.0
+    center_x = bounding_box.width / 2.0
+    center_y = bounding_box.height / 2.0 - 25.0
+    outer_y1 = 4
+    outer_y2 = bounding_box.height - 50
+    cr.move_to(outer_x1, center_y)
+    cr.line_to(center_x, outer_y2)
+    cr.line_to(outer_x2, center_y)
+    cr.line_to(center_x, outer_y1)
+    cr.line_to(outer_x1, center_y)
+
+    # Inner diamond
+    inner_x1 = bounding_box.width / 3.0
+    inner_x2 = bounding_box.width * 2.0 / 3.0
+    inner_y1 = bounding_box.height / 8.0
+    inner_y2 = bounding_box.height * 7.0 / 8.0 - 46
+    cr.move_to(inner_x1, center_y)
+    cr.line_to(center_x, inner_y2)
+    cr.line_to(inner_x2, center_y)
+    cr.line_to(center_x, inner_y1)
+    cr.line_to(inner_x1, center_y)
+
+    stroke(context)
--- gaphor/RAAML/fta/zeroevent.py
+++ gaphor/RAAML/fta/zeroevent.py
@@ -3,12 +3,13 @@
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +43,28 @@ class ZeroEventItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_zero_event,
         )
 
 
 def draw_zero_event(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+    left = bounding_box.width / 4.0
+    right = bounding_box.width * 3.0 / 4.0
+    wall_top = bounding_box.height / 4.0
+    wall_bottom = bounding_box.height - 40
+    cr.move_to(left, wall_bottom)
+    cr.line_to(right, wall_bottom)
+    cr.line_to(right, wall_top)
+    cr.line_to(bounding_box.width / 2.0, 4)
+    cr.line_to(left, wall_top)
+    cr.line_to(left, wall_bottom)
+    cr.move_to(left, wall_top)
+    cr.line_to(right, wall_bottom)
+    stroke(context)
