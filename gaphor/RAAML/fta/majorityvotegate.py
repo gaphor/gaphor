014830bf15516936f9fc9aca37defb862e6a2e86
Dan Yeaw <dan@yeaw.me>
2021-03-06 22:40:33 -0500

    Add shapes for majority vote and sequence gates
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/RAAML/fta/majorityvotegate.py
+++ gaphor/RAAML/fta/majorityvotegate.py
@@ -1,14 +1,17 @@
-"""XOR gate item definition."""
+"""Majority vote gate item definition."""
+
+from math import pi
 
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -26,6 +29,15 @@ class MajorityVoteItem(ElementPresentation, Classified):
 
     def update_shapes(self, event=None):
         self.shape = Box(
+            Text(
+                # TODO: Make this text align to center
+                text="m",
+                style={
+                    "font-weight": FontWeight.BOLD,
+                    "font-size": "large",
+                    "padding": (25, 4, 0, 4),
+                },
+            ),
             Box(
                 Text(
                     text=lambda: stereotypes_str(self.subject, ["MAJORITY_VOTE"]),
@@ -42,11 +54,48 @@ class MajorityVoteItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (20, 4, 0, 4),
+                    "min-height": 30,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_majority_vote_gate,
         )
 
 
 def draw_majority_vote_gate(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+    left = bounding_box.width / 3.0
+    right = bounding_box.width * 2.0 / 3.0
+    wall_top = bounding_box.height / 4.0 + 4.0
+    wall_bottom = bounding_box.height - 40
+
+    # Left wall
+    cr.move_to(left, wall_bottom)
+    cr.line_to(left, wall_top)
+
+    # Right wall
+    cr.move_to(right, wall_bottom)
+    cr.line_to(right, wall_top)
+
+    # Top arc
+    rx = right - left
+    ry = bounding_box.height - 50
+    cr.move_to(left, wall_top)
+    cr.save()
+    cr.translate(left + rx / 2.0, wall_top)
+    cr.scale(rx / 2.0, ry / 2.0)
+    cr.arc(0.0, 0.0, 1.0, pi, 0)
+    cr.restore()
+
+    # Bottom arc
+    ry = bounding_box.height / 6.0
+    cr.move_to(left, wall_bottom)
+    cr.save()
+    cr.translate(left + rx / 2.0, wall_bottom)
+    cr.scale(rx / 2.0, ry / 2.0)
+    cr.arc(0.0, 0.0, 1.0, pi, 0)
+    cr.restore()
+
+    stroke(context)
--- gaphor/RAAML/fta/seqgate.py
+++ gaphor/RAAML/fta/seqgate.py
@@ -1,14 +1,17 @@
 """SEQ gate item definition."""
 
+from math import pi
+
 from gaphas.geometry import Rectangle
 
 from gaphor.core.modeling import DrawContext
+from gaphor.core.styling import VerticalAlign
 from gaphor.diagram.presentation import (
     Classified,
     ElementPresentation,
     from_package_str,
 )
-from gaphor.diagram.shapes import Box, EditableText, Text, draw_border
+from gaphor.diagram.shapes import Box, EditableText, Text, stroke
 from gaphor.diagram.support import represents
 from gaphor.diagram.text import FontStyle, FontWeight
 from gaphor.RAAML import raaml
@@ -42,11 +45,58 @@ class SEQItem(ElementPresentation, Classified):
                     text=lambda: from_package_str(self),
                     style={"font-size": "x-small"},
                 ),
-                style={"padding": (12, 4, 12, 4)},
+                style={
+                    "padding": (55, 4, 0, 4),
+                    "min-height": 100,
+                },
             ),
+            style={"vertical-align": VerticalAlign.BOTTOM},
             draw=draw_seq_gate,
         )
 
 
 def draw_seq_gate(box, context: DrawContext, bounding_box: Rectangle):
-    draw_border(box, context, bounding_box)
+    cr = context.cairo
+    left = bounding_box.width / 3.0
+    right = bounding_box.width * 2.0 / 3.0
+    shape_height = bounding_box.height - 44
+    wall_top = bounding_box.height / 4.0 + 4.0
+    wall_bottom = shape_height * 4.0 / 5.0 + 4.0
+
+    # Left wall
+    cr.move_to(left, wall_bottom)
+    cr.line_to(left, wall_top)
+
+    # Right wall
+    cr.move_to(right, wall_bottom)
+    cr.line_to(right, wall_top)
+
+    # Top arc
+    rx = right - left
+    ry = bounding_box.height * 2.0 / 5.0
+    cr.move_to(left, wall_top)
+    cr.save()
+    cr.translate(left + rx / 2.0, wall_top)
+    cr.scale(rx / 2.0, ry / 2.0)
+    cr.arc(0.0, 0.0, 1.0, pi, 0)
+    cr.restore()
+
+    # Triangle
+    cr.move_to(left, wall_bottom)
+    cr.line_to(right, wall_bottom)
+    cr.line_to(bounding_box.width / 2.0, wall_top - ry / 2.0)
+    cr.line_to(left, wall_bottom)
+
+    # Bottom vertical lines
+    left_line = left + rx / 5.0
+    cr.move_to(left_line, wall_bottom)
+    cr.line_to(left_line, bounding_box.height - 40)
+    right_line = right - rx / 5.0
+    cr.move_to(right_line, wall_bottom)
+    cr.line_to(right_line, bounding_box.height - 40)
+
+    # Top vertical line
+    center = bounding_box.width / 2.0
+    cr.move_to(center, wall_top - ry / 2.0)
+    cr.line_to(center, 0)
+    stroke(context)
