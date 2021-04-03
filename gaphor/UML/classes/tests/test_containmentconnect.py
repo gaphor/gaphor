acd4238dce39464e99964227dca7758cffedca39
Dan Yeaw <dan@yeaw.me>
2021-04-03 14:35:43 -0400

    Add test for connecting containment to package and a class [skip ci]
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/UML/classes/tests/test_containmentconnect.py
+++ gaphor/UML/classes/tests/test_containmentconnect.py
@@ -2,7 +2,7 @@
 
 from gaphor import UML
 from gaphor.diagram.tests.fixtures import allow, connect
-from gaphor.UML.classes import PackageItem
+from gaphor.UML.classes import ClassItem, PackageItem
 from gaphor.UML.classes.containment import ContainmentItem
 
 
@@ -19,3 +19,16 @@ def test_containment_package_glue(create):
 
     glued = allow(containment, containment.tail, pkg2)
     assert glued
+
+
+def test_containment_package_class(create, diagram):
+    """Test containment connecting to a package and a class."""
+    package = create(ContainmentItem, UML.Package)
+    line = create(ContainmentItem)
+    ac = create(ClassItem, UML.Class)
+
+    connect(line, line.head, package)
+    connect(line, line.tail, ac)
+    assert diagram.connections.get_connection(line.tail).connected is ac
+    assert len(package.subject.ownedElement) == 1
+    assert ac.subject in package.subject.ownedElement
