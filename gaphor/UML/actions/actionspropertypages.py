1877a18dcd7039a65e78456d1d428b39a3a89df3
Arjan Molenaar <gaphor@gmail.com>
2021-04-18 12:55:04 +0200

    Create separate glade file for actions property pages

--- gaphor/UML/actions/actionspropertypages.py
+++ gaphor/UML/actions/actionspropertypages.py
@@ -1,10 +1,23 @@
+import importlib
 import math
+
+from gi.repository import Gtk
 
 from gaphor import UML
 from gaphor.core import transactional
-from gaphor.diagram.propertypages import PropertyPageBase, PropertyPages, new_builder
+from gaphor.diagram.propertypages import PropertyPageBase, PropertyPages
 from gaphor.UML.actions.activitynodes import ForkNodeItem
 from gaphor.UML.actions.objectnode import ObjectNodeItem
+
+
+def new_builder(*object_ids):
+    builder = Gtk.Builder()
+    builder.set_translation_domain("gaphor")
+    with importlib.resources.path(
+        "gaphor.UML.actions", "propertypages.glade"
+    ) as glade_file:
+        builder.add_objects_from_file(str(glade_file), object_ids)
+    return builder
 
 
 @PropertyPages.register(ObjectNodeItem)
