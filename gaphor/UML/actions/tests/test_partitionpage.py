3cef23ce4e9434c2d89aa9b255e44c2a1194974f
Arjan Molenaar <gaphor@gmail.com>
2021-04-25 21:39:58 +0200

    Add tests for partition property page

---
+++ gaphor/UML/actions/tests/test_partitionpage.py
@@ -0,0 +1,17 @@
+from gaphor import UML
+from gaphor.diagram.tests.fixtures import find
+from gaphor.UML.actions.partitionpage import PartitionPropertyPage
+
+
+def test_partition_page(diagram, element_factory):
+    item = diagram.create(
+        UML.actions.PartitionItem, subject=element_factory.create(UML.ActivityPartition)
+    )
+    property_page = PartitionPropertyPage(item)
+
+    widget = property_page.construct()
+
+    num_partitions = find(widget, "num-partitions")
+    num_partitions.set_value(2)
+
+    assert len(item.partition) == 1
