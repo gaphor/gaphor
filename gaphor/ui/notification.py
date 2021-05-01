6682a20e357b4fc1b447dec336cefcae67b3edf1
Arjan Molenaar <gaphor@gmail.com>
2021-05-01 16:19:09 +0200

    Fix notification in GTK3

--- gaphor/ui/notification.py
+++ gaphor/ui/notification.py
@@ -10,20 +10,19 @@ class InAppNotifier:
     def __init__(self, builder):
         self.revealer = builder.get_object("notification-revealer")
         self.message_label = builder.get_object("notification-message")
+        self.auto_hide_id = 0
         close = builder.get_object("notification-close")
-        close.connect("clicked", close_notification)
+        close.connect("clicked", self.close_notification)
 
     @event_handler(Notification)
     def handle(self, event: Notification):
         if not self.revealer.get_reveal_child():
             self.message_label.set_text(event.message)
             self.revealer.set_reveal_child(True)
-            self.revealer.auto_hide_id = GLib.timeout_add(
-                DELAY, close_notification, self.revealer
-            )
+            self.auto_hide_id = GLib.timeout_add(DELAY, self.close_notification)
 
-
-def close_notification(notification_revealer):
-    notification_revealer.set_reveal_child(False)
-    GLib.source_remove(notification_revealer.auto_hide_id)
-    del notification_revealer.auto_hide_id
+    def close_notification(self, *args):
+        self.revealer.set_reveal_child(False)
+        if self.auto_hide_id:
+            GLib.source_remove(self.auto_hide_id)
+            self.auto_hide_id = 0
