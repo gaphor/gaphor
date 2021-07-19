9fea3f7481590b8ae2e35a8339b03300475401a8
Dan Yeaw <dan@yeaw.me>
2021-07-18 22:19:00 -0400

    Replace main Makefiles with poethepoet

--- packaging/make-script.py
+++ packaging/make-script.py
@@ -1,21 +1,42 @@
 import sys
 
 import tomlkit
+from pathlib import Path
+import pyinstaller_versionfile
+
+packaging_path = Path(__file__).resolve().parent 
 
 
-def main(toml_file):
-    with open(toml_file) as f:
+def get_version() -> str:
+    project_dir = Path(__file__).resolve().parent.parent
+    f = project_dir / "pyproject.toml"
+    return str(tomlkit.parse(f.read_text())["tool"]["poetry"]["version"])
+
+
+def make_gaphor_script():
+    pyproject_toml = packaging_path.parent / "pyproject.toml" 
+    with open(pyproject_toml) as f:
         toml = tomlkit.parse(f.read())
 
-    plugins = toml["tool"]["poetry"]["plugins"]
-    for cat in plugins.values():
-        for entrypoint in cat.values():
-            print(f"import {entrypoint.split(':')[0]}")
+    gaphor_script = packaging_path / "gaphor-script.py"
+    with open(gaphor_script, "w") as file:
+        plugins = toml["tool"]["poetry"]["plugins"]
+        for cat in plugins.values():
+            for entrypoint in cat.values():
+                file.write(f"import {entrypoint.split(':')[0]}\n")
 
-    print("from gaphor.ui import main")
-    print("import sys")
-    print("main(sys.argv)")
+        file.write("from gaphor.ui import main\n")
+        file.write("import sys\n")
+        file.write("main(sys.argv)\n")
 
 
-if __name__ == "__main__":
-    main(sys.argv[1])
+def make_file_version_info():
+    win_packaging_path = packaging_path / "windows"
+    metadata = win_packaging_path / "versionfile_metadata.yml"
+    file_version_out = win_packaging_path / "file_version_info.txt"
+
+    pyinstaller_versionfile.create_versionfile_from_input_file(
+        output_file=file_version_out,
+        input_file=metadata,
+        version=get_version(),
+    )
