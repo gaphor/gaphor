6bd7891e0cfcedc1a5d0813b644b5d6bb941045a
Dan Yeaw <dan@yeaw.me>
2021-08-20 10:31:11 -0700

    Remove use of deprecated abstractproperty
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- gaphor/abc.py
+++ gaphor/abc.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import abc
+from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -8,31 +8,33 @@ if TYPE_CHECKING:
     from gaphor.diagram.diagramtoolbox import ToolboxDefinition
 
 
-class Service(metaclass=abc.ABCMeta):
+class Service(metaclass=ABCMeta):
     """Base interface for all services in Gaphor."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def shutdown(self) -> None:
         """Shutdown the services, free resources."""
 
 
-class ActionProvider(metaclass=abc.ABCMeta):
+class ActionProvider(metaclass=ABCMeta):
     """An action provider is a special service that provides actions (see
     gaphor/action.py)."""
 
 
-class ModelingLanguage(metaclass=abc.ABCMeta):
+class ModelingLanguage(metaclass=ABCMeta):
     """A model provider is a special service that provides an entrypoint to a
     model implementation, such as UML, SysML, RAAML."""
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def name(self) -> str:
         """Human readable name of the model."""
 
-    @abc.abstractproperty
+    @property
+    @abstractmethod
     def toolbox_definition(self) -> ToolboxDefinition:
         """Get structure for the toolbox."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def lookup_element(self, name: str) -> type[Element] | None:
         """Look up a model element type (class) by name."""
