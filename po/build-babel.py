cd329f670cbf405132fc5d1f92f6f594ac0ab1f3
Arjan Molenaar <gaphor@gmail.com>
2021-09-24 22:45:58 +0200

    locale are built and loaded in the gaphor/locale package

--- po/build-babel.py
+++ po/build-babel.py
@@ -25,7 +25,14 @@ def update_po_files():
 
 def compile_mo_files():
     for path in (path for path in po_path.iterdir() if path.suffix == ".po"):
-        mo_path = po_path.parent / "locale" / path.stem / "LC_MESSAGES" / "gaphor.mo"
+        mo_path = (
+            po_path.parent
+            / "gaphor"
+            / "locale"
+            / path.stem
+            / "LC_MESSAGES"
+            / "gaphor.mo"
+        )
         mo_path.parent.mkdir(parents=True, exist_ok=True)
         run_babel("compile", path, mo_path, path.stem)
 
