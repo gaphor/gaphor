eba10079cf317fdf8a0d5e2266f0c34ebd001ff8
Arjan Molenaar <gaphor@gmail.com>
2021-09-26 23:06:36 +0200

    Use locale, instead of gettext module
    So locale settings are also available from C libs

--- gaphor/i18n.py
+++ gaphor/i18n.py
@@ -4,8 +4,8 @@ Translate text in to your native language using the gettext() function.
 """
 __all__ = ["gettext"]
 
-import gettext as _gettext
 import importlib.resources
+import locale
 import logging
 
 log = logging.getLogger(__name__)
@@ -13,8 +13,9 @@ log = logging.getLogger(__name__)
 try:
     with importlib.resources.path("gaphor", "__init__.py") as path:
         localedir = path.parent / "locale"
-        translate = _gettext.translation("gaphor", localedir=localedir, fallback=True)
-        gettext = translate.gettext
+        locale.bindtextdomain("gaphor", localedir)  # type: ignore[attr-defined]
+        locale.textdomain("gaphor")  # type: ignore[attr-defined]
+        gettext = locale.gettext  # type: ignore[attr-defined]
 
 except OSError as e:
     log.warning(f"No translations were found: {e}")
