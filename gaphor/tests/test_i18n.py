264f4f3ce1ea6a211ff0c4a27be9998149f0f0c4
Arjan Molenaar <gaphor@gmail.com>
2021-10-03 12:07:47 +0200

    Add missing test case for translate_ui_string()

---
+++ gaphor/tests/test_i18n.py
@@ -0,0 +1,7 @@
+from gaphor.i18n import translated_ui_string
+
+
+def test_ui_builder_builds_from_resources():
+    ui_xml = translated_ui_string("gaphor.diagram", "propertypages.glade")
+
+    assert "translatable=" not in ui_xml
