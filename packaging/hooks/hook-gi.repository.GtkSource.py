1eef8c28fa12f8698a9a281fb9c20aa9cba602b3
Sourcery AI <>
2021-10-18 00:57:57 +0000

    'Refactored by Sourcery'

--- packaging/hooks/hook-gi.repository.GtkSource.py
+++ packaging/hooks/hook-gi.repository.GtkSource.py
@@ -10,10 +10,7 @@ from PyInstaller.utils.hooks.gi import collect_glib_share_files, get_gi_typelibs
 
 def hook(hook_api):
     module_versions = get_hook_config(hook_api, "gi", "module-versions")
-    if module_versions:
-        version = module_versions.get("GtkSource", "3.0")
-    else:
-        version = "3.0"
+    version = module_versions.get("GtkSource", "3.0") if module_versions else "3.0"
     logger.info(f"GtkSource version is {version}")
 
     binaries, datas, hiddenimports = get_gi_typelibs("GtkSource", version)
