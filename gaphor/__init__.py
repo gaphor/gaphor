860988b88ae43b552c9eb3b0bd634d1a0d55a6db
Arjan Molenaar <gaphor@gmail.com>
2021-11-03 21:36:29 +0100

    Monkey patch PyGObject to make Gaphor boot on GTK4
    Until https://gitlab.gnome.org/GNOME/pygobject/-/merge_requests/175 is merged.

--- gaphor/__init__.py
+++ gaphor/__init__.py
@@ -24,9 +24,16 @@ import gi
 gtk_version = "4.0" if os.getenv("GAPHOR_USE_GTK") == "4" else "3.0"
 gtk_source_version = "5" if os.getenv("GAPHOR_USE_GTK") == "4" else "4"
 
+if gtk_version == "4.0":
+    # Monkey patch PyGObject
+    import gi.overrides.Gtk
+
+    del gi.overrides.Gtk.TreeView.enable_model_drag_source
+    del gi.overrides.Gtk.TreeView.enable_model_drag_dest
+
 gi.require_version("PangoCairo", "1.0")
 gi.require_version("Gtk", gtk_version)
 gi.require_version("Gdk", gtk_version)
 
-if os.getenv("GAPHOR_USE_GTK") != "4":
+if gtk_version == "3.0":
     gi.require_version("GtkSource", gtk_source_version)
