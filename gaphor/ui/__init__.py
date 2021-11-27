1421b8c1f4c36ceab05ca891e537a88c3e170617
Arjan Molenaar <gaphor@gmail.com>
2021-11-27 17:01:03 +0100

    Explicitly refuse gi.require_version() when generating docs
    The `gi` module is already loaded during diagram creation. Hence, there's no
    "proper" way to avoid `require_version()` calls.

--- gaphor/ui/__init__.py
+++ gaphor/ui/__init__.py
@@ -5,12 +5,14 @@ import os
 
 import gi
 
-gtk_version = "4.0" if os.getenv("GAPHOR_USE_GTK") == "4" else "3.0"
-gtk_source_version = "5" if os.getenv("GAPHOR_USE_GTK") == "4" else "4"
+if os.getenv("GAPHOR_USE_GTK") != "NONE":
+    # Allow to explicitly *not* initialize GTK (for docs, mainly)
+    gtk_version = "4.0" if os.getenv("GAPHOR_USE_GTK") == "4" else "3.0"
+    gtk_source_version = "5" if os.getenv("GAPHOR_USE_GTK") == "4" else "4"
 
-gi.require_version("Gtk", gtk_version)
-gi.require_version("Gdk", gtk_version)
-gi.require_version("GtkSource", gtk_source_version)
+    gi.require_version("Gtk", gtk_version)
+    gi.require_version("Gdk", gtk_version)
+    gi.require_version("GtkSource", gtk_source_version)
 
 import importlib.resources
 import logging
