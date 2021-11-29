c4e60ee395d1372bfb3a62effb665441c5e2189a
Arjan Molenaar <gaphor@gmail.com>
2021-11-29 21:36:55 +0100

    Add support for overrides

--- gaphor/codegen/coder.py
+++ gaphor/codegen/coder.py
@@ -17,18 +17,21 @@ Notes:
 from __future__ import annotations
 
 import argparse
+import sys
 from pathlib import Path
 from typing import Iterable
 
 from gaphor import UML
+from gaphor.codegen.override import Overrides
 from gaphor.core.modeling import ElementFactory
 from gaphor.storage import storage
 from gaphor.UML.modelinglanguage import UMLModelingLanguage
 
 
 class Coder:
-    def __init__(self, class_):
+    def __init__(self, class_, overrides):
         self._class = class_
+        self.overrides = overrides
 
     def __str__(self):
         base_classes = ", ".join(
@@ -39,8 +42,13 @@ class Coder:
     def __iter__(self):
         if self._class.attribute:
             for attr in sorted(self._class.attribute, key=lambda a: a.name or ""):
-                if attr.association and is_simple_attribute(attr.type):
-                    yield f"{attr.name}: attribute[{attr.typeValue}]"
+                full_name = f"{self._class.name}.{attr.name}"
+                if self.overrides.has_override(full_name):
+                    return f"{attr.name}: {self.overrides.get_type(full_name)}"
+                elif attr.isDerived:
+                    pass
+                elif attr.association and is_simple_attribute(attr.type):
+                    yield f"{attr.name}: attribute[str]"
                 elif attr.association:
                     mult = "one" if attr.upper == "1" else "many"
                     yield f"{attr.name}: relation_{mult}[{attr.type.name}]"
@@ -50,9 +58,6 @@ class Coder:
                     yield f"{attr.name}: attribute[{attr.typeValue}]"
         else:
             yield "pass"
-
-    # get used property types
-    # handle overrides
 
 
 def order_classes(classes: Iterable[UML.Class]) -> Iterable[UML.Class]:
@@ -105,7 +110,7 @@ def is_in_toplevel_package(c: UML.Class, package_name: str):
     return test(c.owningPackage)
 
 
-def coder(modelfile, outfile):
+def coder(modelfile, overrides, outfile):
     element_factory = ElementFactory()
     uml_modeling_language = UMLModelingLanguage()
     storage.load(
@@ -125,19 +130,29 @@ def coder(modelfile, outfile):
     )
 
     for c in classes:
-        coder = Coder(c)
-        print(coder)
-        for a in coder:
-            print("    " + a)
+        if overrides.has_override(c.name):
+            print(overrides.get_override(c.name))
+        else:
+            coder = Coder(c, overrides)
+            print(coder)
+            for a in coder:
+                print("    " + a)
         print()
         print()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("modelfile", type=Path, help="gaphor model filename")
-    parser.add_argument("outfile", type=Path, help="python data model filename")
-
+    parser.add_argument("modelfile", type=Path, help="Gaphor model filename")
+    parser.add_argument(
+        "-o", dest="outfile", type=Path, help="Python data model filename"
+    )
+    parser.add_argument("-r", dest="overridesfile", type=Path, help="Override filename")
     args = parser.parse_args()
 
-    coder(args.modelfile, args.outfile)
+    overrides = Overrides(args.overridesfile)
+    if args.outfile:
+        with open(args.outfile, "w") as outfile:
+            coder(args.modelfile, overrides, outfile)
+    else:
+        coder(args.modelfile, overrides, sys.stdout)
