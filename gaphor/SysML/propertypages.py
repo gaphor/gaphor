dfde388e03f96005348c776ddc2c47356eeb3277
Arjan Molenaar <gaphor@gmail.com>
2021-12-12 12:53:51 +0100

    Remove UML.format and UML.parse
    Both functions have moved to gaphor.core.format since Gaphor 2.1.

--- gaphor/SysML/propertypages.py
+++ gaphor/SysML/propertypages.py
@@ -1,5 +1,6 @@
 from gaphor import UML
 from gaphor.core import transactional
+from gaphor.core.format import format, parse
 from gaphor.diagram.propertypages import (
     PropertyPageBase,
     PropertyPages,
@@ -189,7 +190,7 @@ class ItemFlowPropertyPage(PropertyPageBase):
         ):
             iflow = self.subject.informationFlow[0]
             assert isinstance(iflow, sysml.ItemFlow)
-            self.entry.set_text(UML.format(iflow.itemProperty))
+            self.entry.set_text(format(iflow.itemProperty))
 
         return builder.get_object("item-flow-editor")
 
@@ -213,7 +214,7 @@ class ItemFlowPropertyPage(PropertyPageBase):
         if self.subject.informationFlow:
             iflow = self.subject.informationFlow[0]
             assert isinstance(iflow, sysml.ItemFlow)
-            UML.parse(iflow.itemProperty, entry.get_text())
+            parse(iflow.itemProperty, entry.get_text())
 
     @transactional
     def _invert_direction_changed(self, button):
--- gaphor/UML/__init__.py
+++ gaphor/UML/__init__.py
@@ -2,7 +2,3 @@
 import gaphor.UML.umloverrides
 from gaphor.UML import modelfactory as model
 from gaphor.UML.uml import *
-from gaphor.UML.umlfmt import format
-from gaphor.UML.umllex import parse
-
-# gaphor.UML.format() and gaphor.UML.parse() are deprecated as of 2.1, use gaphor.core.format.{format|parse} instead.
