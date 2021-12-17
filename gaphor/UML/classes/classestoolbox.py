54e3ebb56f4e2bb04dcbdaa91c1c5d04add94f44
Arjan Molenaar <gaphor@gmail.com>
2021-12-17 21:24:07 +0100

    Fix: do not assign a name to composite/shared associations

--- gaphor/UML/classes/classestoolbox.py
+++ gaphor/UML/classes/classestoolbox.py
@@ -6,7 +6,12 @@ from gaphas.item import SE
 
 from gaphor import UML
 from gaphor.core import gettext
-from gaphor.diagram.diagramtoolbox import ToolDef, ToolSection, namespace_config
+from gaphor.diagram.diagramtoolbox import (
+    ToolDef,
+    ToolSection,
+    default_namespace,
+    namespace_config,
+)
 from gaphor.diagram.tools import new_item_factory
 from gaphor.UML import diagramitems
 
@@ -19,7 +24,7 @@ class AssociationType(Enum):
 def create_association(
     assoc_item: diagramitems.AssociationItem, association_type: AssociationType
 ) -> None:
-    namespace_config(assoc_item)
+    default_namespace(assoc_item)
     assoc = assoc_item.subject
     assoc.memberEnd.append(assoc_item.model.create(UML.Property))
     assoc.memberEnd.append(assoc_item.model.create(UML.Property))
