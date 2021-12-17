02d5e7dbc776062e8fcbde3944515b443209dddc
Arjan Molenaar <gaphor@gmail.com>
2021-12-17 18:15:46 +0100

    Add seperate sections for each swimlane

--- gaphor/UML/actions/partitionpage.py
+++ gaphor/UML/actions/partitionpage.py
@@ -1,5 +1,7 @@
 """Activity partition property page."""
 
+from gi.repository import Gtk
+
 from gaphor import UML
 from gaphor.core import gettext, transactional
 from gaphor.diagram.propertypages import (
@@ -30,7 +32,44 @@ class PartitionPropertyPage(PropertyPageBase):
             signals={"partitions-changed": (self._on_num_partitions_changed,)},
         )
 
+        num_partitions = builder.get_object("num-partitions")
+        num_partitions.set_value(len(self.item.partition))
+
+        self.partitions = builder.get_object("partitions")
+        for partition in self.item.partition:
+            self.partitions.pack_start(
+                self.construct_partition(partition), expand=False, fill=False, padding=0
+            )
+
         return builder.get_object("partition-editor")
+
+    def construct_partition(self, partition):
+        builder = new_builder(
+            "partition",
+            signals={
+                "partition-name-changed": (self._on_partition_name_changed, partition),
+                "partition-type-changed": (self._on_partition_type_changed, partition),
+            },
+        )
+
+        builder.get_object("partition-name").set_text(partition.name or "")
+
+        combo = builder.get_object("partition-type")
+        for c in self.item.model.select(UML.Classifier):
+            if c.name:
+                combo.append(c.id, c.name)
+
+        completion = Gtk.EntryCompletion()
+        completion.set_model(combo.get_model())
+        completion.set_minimum_key_length(1)
+        completion.set_text_column(0)
+
+        entry = combo.get_child()
+        entry.set_completion(completion)
+        if partition.represents:
+            combo.set_active_id(partition.represents.id)
+
+        return builder.get_object("partition")
 
     @transactional
     def _on_num_partitions_changed(self, spin_button):
@@ -39,9 +78,19 @@ class PartitionPropertyPage(PropertyPageBase):
         self.update_partitions(num_partitions)
 
     @transactional
-    def _on_partition_name_changed(self, widget, path, text):
+    def _on_partition_name_changed(self, entry, partition):
         """Event handler for editing partition names."""
-        self.item.partition[int(path)].name = text
+        partition.name = entry.get_text()
+
+    @transactional
+    def _on_partition_type_changed(self, combo, partition):
+        """Event handler for editing partition names."""
+        id = combo.get_active_id()
+        if id:
+            element = self.item.model.lookup(id)
+            partition.represents = element
+        else:
+            del partition.represents
 
     def update_partitions(self, num_partitions) -> None:
         """Add and remove partitions.
@@ -52,11 +101,21 @@ class PartitionPropertyPage(PropertyPageBase):
         """
         if not self.item.subject:
             return
-        if num_partitions > len(self.item.partition):
+
+        while num_partitions > len(self.item.partition):
             partition = self.item.subject.model.create(UML.ActivityPartition)
             partition.name = gettext("New Swimlane")
             partition.activity = self.item.subject.activity
             self.item.partition.append(partition)
-        elif num_partitions < len(self.item.partition):
+
+            self.partitions.pack_start(
+                self.construct_partition(partition), expand=False, fill=False, padding=0
+            )
+
+        while num_partitions < len(self.item.partition):
             partition = self.item.partition[-1]
             partition.unlink()
+
+            last_child = self.partitions.get_children()[-1]
+            self.partitions.remove(last_child)
+            last_child.destroy()
