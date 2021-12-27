57e88e893c209dca39538ec5478c00a69a424c96
Arjan Molenaar <gaphor@gmail.com>
2021-12-27 11:36:41 +0100

    Fix main window action for recent files

--- gaphor/ui/mainwindow.py
+++ gaphor/ui/mainwindow.py
@@ -41,7 +41,7 @@ def create_hamburger_model(export_menu, tools_menu):
     model = Gio.Menu.new()
 
     part = Gio.Menu.new()
-    part.append(gettext("Open a Model…"), "app.recent-model")
+    part.append(gettext("Open a Model…"), "app.recent-files")
     part.append(gettext("New Model…"), "app.new-model")
     model.append_section(None, part)
 
