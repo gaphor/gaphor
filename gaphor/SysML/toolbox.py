e12a39a3ddd2c18ddd1657e5a8a734fb5e2804f8
Arjan Molenaar <gaphor@gmail.com>
2021-12-27 14:43:44 +0100

    Fix sysml block diagram abbreviations

--- gaphor/SysML/toolbox.py
+++ gaphor/SysML/toolbox.py
@@ -65,8 +65,8 @@ sysml_toolbox_actions: ToolboxDefinition = (
 
 # Not implemented: Parameter Diagram
 sysml_diagram_types: DiagramTypes = (
-    DiagramType("blk", gettext("New Block Definition Diagram"), (blocks,)),
-    DiagramType("ibl", gettext("New Internal Block Diagram"), (internal_blocks,)),
+    DiagramType("bdd", gettext("New Block Definition Diagram"), (blocks,)),
+    DiagramType("ibd", gettext("New Internal Block Diagram"), (internal_blocks,)),
     DiagramType("pkg", gettext("New Package Diagram"), (blocks,)),
     DiagramType("req", gettext("New Requirement Diagram"), (requirements,)),
     DiagramType("act", gettext("New Activity Diagram"), (actions,)),
