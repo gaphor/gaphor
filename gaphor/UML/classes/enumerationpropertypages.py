e1a465706b1d47a7c603b7265a1de32aeabcd6b5
Arjan Molenaar <gaphor@gmail.com>
2021-12-29 14:06:57 +0100

    Add info button for Enumerations

--- gaphor/UML/classes/enumerationpropertypages.py
+++ gaphor/UML/classes/enumerationpropertypages.py
@@ -8,6 +8,7 @@ from gaphor.diagram.propertypages import (
     on_text_cell_edited,
 )
 from gaphor.transaction import transactional
+from gaphor.ui.hoversupport import widget_add_hover_support
 from gaphor.UML.classes.classespropertypages import (
     AttributesPage,
     ClassEnumerationLiterals,
@@ -39,12 +40,17 @@ class EnumerationPage(PropertyPageBase):
 
         builder = new_builder(
             "enumerations-editor",
+            "enumerations-info",
             signals={
                 "show-enumerations-changed": (self._on_show_enumerations_change,),
                 "enumerations-name-edited": (on_text_cell_edited, self.model, 0),
                 "tree-view-destroy": (self.watcher.unsubscribe_all,),
+                "enumerations-info-clicked": (self.on_enumerations_info_clicked),
             },
         )
+
+        self.info = builder.get_object("enumerations-info")
+        widget_add_hover_support(builder.get_object("enumerations-info-icon"))
 
         show_enumerations = builder.get_object("show-enumerations")
         show_enumerations.set_active(self.item.show_enumerations)
@@ -73,6 +79,9 @@ class EnumerationPage(PropertyPageBase):
     def _on_show_enumerations_change(self, button, gparam):
         self.item.show_enumerations = button.get_active()
         self.item.request_update()
+
+    def on_enumerations_info_clicked(self, image, event):
+        self.info.show()
 
 
 PropertyPages.register(EnumerationItem)(AttributesPage)
