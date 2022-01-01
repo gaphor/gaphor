04ef13def11a3d852d664eef974f425de8b1b36e
Arjan Molenaar <gaphor@gmail.com>
2022-01-01 15:27:29 +0100

    Fix saving fork node height
    Fixes #1246.

---
+++ gaphor/UML/actions/tests/test_forknode.py
@@ -0,0 +1,49 @@
+import pytest
+
+from gaphor.UML.actions.activitynodes import ForkNodeItem
+
+
+class Saver:
+    def __call__(self, name, val):
+        setattr(self, name, val)
+
+
+@pytest.fixture
+def saver():
+    return Saver()
+
+
+def test_forknode_save_default_height(diagram, saver):
+    fork: ForkNodeItem = diagram.create(ForkNodeItem)
+    diagram.update_now({fork})
+
+    fork.save(saver)
+
+    assert saver.height == 30
+    assert saver.matrix[5] == 0
+
+
+def test_forknode_save_height_0(diagram, saver):
+    fork: ForkNodeItem = diagram.create(ForkNodeItem)
+
+    fork.handles()[0].pos.y = -100
+    fork.handles()[1].pos.y = 0
+    diagram.update_now({fork})
+
+    fork.save(saver)
+
+    assert saver.height == 100
+    assert saver.matrix[5] == -100
+
+
+def test_forknode_save_height_1(diagram, saver):
+    fork: ForkNodeItem = diagram.create(ForkNodeItem)
+
+    fork.handles()[0].pos.y = 0
+    fork.handles()[1].pos.y = 100
+    diagram.update_now({fork})
+
+    fork.save(saver)
+
+    assert saver.height == 100
+    assert saver.matrix[5] == 0
