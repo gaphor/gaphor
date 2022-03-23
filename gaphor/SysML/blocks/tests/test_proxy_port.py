dee1a64bdc333a4578692968ad5ff3dd0c4232d5
Arjan Molenaar <gaphor@gmail.com>
2022-03-23 20:54:25 +0100

    Test ProxyItem.point()

--- gaphor/SysML/blocks/tests/test_proxy_port.py
+++ gaphor/SysML/blocks/tests/test_proxy_port.py
@@ -7,3 +7,9 @@ def test_proxy_port_item_conforms_to_item_protocol(diagram):
     item = ProxyPortItem(diagram)
 
     assert isinstance(item, Item)
+
+
+def test_proxy_port_item_point(diagram):
+    item = ProxyPortItem(diagram)
+
+    assert item.point(0, 0) == 0.0
