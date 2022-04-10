1fd2a8c5568f40c0e8899c3a369139099344d263
Arjan Molenaar <gaphor@gmail.com>
2022-04-10 16:09:10 +0200

    Extract AttachedPresentation from ProxyPort
    It can be reused for Pins and Parameter Nodes.

--- gaphor/SysML/blocks/proxyport.py
+++ gaphor/SysML/blocks/proxyport.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import Generic, Optional
 
 from gaphas.connector import Handle, LinePort, Position
 from gaphas.constraint import constraint
 from gaphas.geometry import Rectangle, distance_rectangle_point
 
 from gaphor.core import gettext
-from gaphor.core.modeling import Presentation
+from gaphor.core.modeling.presentation import Presentation, S
 from gaphor.diagram.presentation import HandlePositionUpdate, Named, postload_connect
 from gaphor.diagram.shapes import (
     Box,
@@ -31,30 +31,38 @@ def text_position(position):
     }
 
 
-@represents(sysml.ProxyPort)
-class ProxyPortItem(Named, Presentation[sysml.ProxyPort], HandlePositionUpdate):
-    def __init__(self, diagram, id=None):
+class AttachedPresentation(Presentation[S], HandlePositionUpdate, Generic[S]):
+    """An attached presentation is a base type for all sorts of element-like
+    presentations that can be attached to an element.
+
+    E.g. ports, pins and parameter nodes.
+    """
+
+    def __init__(self, diagram, id=None, width=16, height=16):
         super().__init__(diagram, id)
         self._connections = diagram.connections
+        self._width = width
+        self._height = height
 
         handle = self._handle = Handle(connectable=True)
         self.watch_handle(handle)
 
-        d = self.dimensions()
-        top_left = Position(d.x, d.y)
-        top_right = Position(d.x1, d.y)
-        bottom_right = Position(d.x1, d.y1)
-        bottom_left = Position(d.x, d.y1)
+        rh = width / 2
+        rv = height / 2
+        top_left = Position(-rh, -rv)
+        top_right = Position(rh, -rv)
+        bottom_right = Position(rh, rv)
+        bottom_left = Position(-rh, rv)
 
         add = diagram.connections.add_constraint
-        add(self, constraint(horizontal=(handle.pos, top_left), delta=-8))
-        add(self, constraint(horizontal=(handle.pos, top_right), delta=-8))
-        add(self, constraint(horizontal=(handle.pos, bottom_right), delta=8))
-        add(self, constraint(horizontal=(handle.pos, bottom_left), delta=8))
-        add(self, constraint(vertical=(handle.pos, top_left), delta=-8))
-        add(self, constraint(vertical=(handle.pos, top_right), delta=8))
-        add(self, constraint(vertical=(handle.pos, bottom_right), delta=8))
-        add(self, constraint(vertical=(handle.pos, bottom_left), delta=-8))
+        add(self, constraint(horizontal=(top_left, handle.pos), delta=rh))
+        add(self, constraint(horizontal=(handle.pos, top_right), delta=-rh))
+        add(self, constraint(horizontal=(handle.pos, bottom_right), delta=rh))
+        add(self, constraint(horizontal=(bottom_left, handle.pos), delta=-rh))
+        add(self, constraint(vertical=(top_left, handle.pos), delta=rv))
+        add(self, constraint(vertical=(top_right, handle.pos), delta=-rv))
+        add(self, constraint(vertical=(handle.pos, bottom_right), delta=rv))
+        add(self, constraint(vertical=(handle.pos, bottom_left), delta=-rv))
         self._ports = [
             LinePort(top_left, top_right),
             LinePort(top_right, bottom_right),
@@ -62,9 +70,14 @@ class ProxyPortItem(Named, Presentation[sysml.ProxyPort], HandlePositionUpdate):
             LinePort(bottom_left, top_left),
         ]
 
+        self._shape = None
         self._last_connected_side = None
-        self.watch("subject[NamedElement].name")
-        self.update_shapes()
+
+    def _set_shape(self, shape):
+        self._shape = shape
+        self.request_update()
+
+    shape = property(lambda s: s._shape, _set_shape)
 
     def handles(self):
         return [self._handle]
@@ -72,14 +85,23 @@ class ProxyPortItem(Named, Presentation[sysml.ProxyPort], HandlePositionUpdate):
     def ports(self):
         return self._ports
 
-    def update_shapes(self):
-        self.shape = IconBox(
-            Box(style={"background-color": (1, 1, 1, 1)}, draw=draw_border),
-            Text(text=lambda: stereotypes_str(self.subject, [gettext("proxy")])),
-            Text(text=lambda: self.subject and self.subject.name or ""),
-            style=text_position(self.connected_side()),
-        )
-        self.request_update()
+    def update_shapes(self, event=None):
+        """Updating the shape configuration, e.g. when extra elements have to
+        be drawn or when styling changes."""
+
+    def update(self, context):
+        side = self.connected_side()
+        if not self.shape or self._last_connected_side != side:
+            self._last_connected_side = side
+            self.update_shapes()
+
+        self.shape.size(context)
+
+    def point(self, x, y):
+        return distance_rectangle_point(self.dimensions(), (x, y))
+
+    def draw(self, context):
+        self.shape.draw(context, self.dimensions())
 
     def connected_side(self) -> Optional[str]:
         cinfo = self._connections.get_connection(self._handle)
@@ -87,12 +109,10 @@ class ProxyPortItem(Named, Presentation[sysml.ProxyPort], HandlePositionUpdate):
         return cinfo.connected.port_side(cinfo.port) if cinfo else None
 
     def dimensions(self):
+        w = self._width
+        h = self._height
         x, y = self._handle.pos
-        # TODO: This is wrong: it returns variables that do not get updated
-        return Rectangle(x - 8, y - 8, 16, 16)
-
-    def point(self, x, y):
-        return distance_rectangle_point(self.dimensions(), (x, y))
+        return Rectangle(x - w / 2, y - h / 2, w, h)
 
     def save(self, save_func):
         save_func("matrix", tuple(self.matrix))
@@ -116,13 +136,17 @@ class ProxyPortItem(Named, Presentation[sysml.ProxyPort], HandlePositionUpdate):
 
         self.update_shapes()
 
-    def update(self, context):
-        side = self.connected_side()
-        if self._last_connected_side != side:
-            self._last_connected_side = side
-            self.update_shapes()
 
-        self.shape.size(context)
+@represents(sysml.ProxyPort)
+class ProxyPortItem(Named, AttachedPresentation[sysml.ProxyPort]):
+    def __init__(self, diagram, id=None):
+        super().__init__(diagram, id, width=16, height=16)
+        self.watch("subject[NamedElement].name")
 
-    def draw(self, context):
-        self.shape.draw(context, self.dimensions())
+    def update_shapes(self):
+        self.shape = IconBox(
+            Box(style={"background-color": (1, 1, 1, 1)}, draw=draw_border),
+            Text(text=lambda: stereotypes_str(self.subject, [gettext("proxy")])),
+            Text(text=lambda: self.subject and self.subject.name or ""),
+            style=text_position(self.connected_side()),
+        )
--- gaphor/SysML/blocks/tests/test_proxy_port.py
+++ gaphor/SysML/blocks/tests/test_proxy_port.py
@@ -1,6 +1,10 @@
 from gaphas import Item
 
+from gaphor.core.modeling import Diagram
+from gaphor.diagram.tests.fixtures import connect
+from gaphor.SysML.blocks.block import BlockItem
 from gaphor.SysML.blocks.proxyport import ProxyPortItem
+from gaphor.SysML.sysml import Block
 
 
 def test_proxy_port_item_conforms_to_item_protocol(diagram):
@@ -13,3 +17,39 @@ def test_proxy_port_item_point(diagram):
     item = ProxyPortItem(diagram)
 
     assert item.point(0, 0) == 0.0
+
+
+def test_ports(diagram):
+    proxy_port = diagram.create(ProxyPortItem)
+
+    diagram.update_now({proxy_port})
+
+    top, right, bottom, left = proxy_port.ports()
+
+    def pos(p):
+        return p.x, p.y
+
+    assert pos(proxy_port.handles()[0].pos) == (0, 0)
+    assert pos(left.end) == pos(top.start) == (-8, -8)
+    assert pos(top.end) == pos(right.start) == (8, -8)
+    assert pos(right.end) == pos(bottom.start) == (8, 8)
+    assert pos(bottom.end) == pos(left.start) == (-8, 8)
+
+
+def test_save_load(create, saver, loader, element_factory):
+    proxy_port = create(ProxyPortItem)
+    block = create(BlockItem, Block)
+
+    connect(proxy_port, proxy_port.handles()[0], block)
+
+    data = saver()
+    loader(data)
+
+    new_diagram = next(element_factory.select(Diagram))
+    new_proxy_port = next(element_factory.select(ProxyPortItem))
+    new_block = next(element_factory.select(BlockItem))
+    connected = new_diagram.connections.get_connection(
+        new_proxy_port.handles()[0]
+    ).connected
+
+    assert connected is new_block
