d834cef643d184082d8b3549d4305cf548db53dc
Arjan Molenaar <gaphor@gmail.com>
2022-05-01 11:44:10 +0200

    Add diagram type block to exported images
    Also add a bit of extra padding in case of a diagram type.
    So it's not on top of the elements.
    
    Fixes #1491.

--- gaphor/diagram/export.py
+++ gaphor/diagram/export.py
@@ -1,10 +1,10 @@
 """Service dedicated to exporting diagrams to a variety of file formats."""
 
 import cairo
-from gaphas.painter import BoundingBoxPainter, FreeHandPainter
+from gaphas.painter import BoundingBoxPainter, FreeHandPainter, PainterChain
 
 from gaphor.core.modeling.diagram import StyledDiagram
-from gaphor.diagram.painter import ItemPainter
+from gaphor.diagram.painter import DiagramTypePainter, ItemPainter
 
 
 def render(diagram, new_surface, padding=8):
@@ -14,15 +14,13 @@ def render(diagram, new_surface, padding=8):
 
     # Update bounding boxes with a temporary Cairo Context
     # (used for stuff like calculating font metrics)
-    tmpsurface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 0, 0)
-    tmpcr = cairo.Context(tmpsurface)
-    bounding_box = BoundingBoxPainter(painter).bounding_box(
-        diagram.get_all_items(), tmpcr
-    )
-    tmpcr.show_page()
-    tmpsurface.flush()
+    bounding_box = calc_bounding_box(diagram, painter)
+    type_padding = diagram_type_height(diagram)
 
-    w, h = bounding_box.width + 2 * padding, bounding_box.height + 2 * padding
+    w, h = (
+        bounding_box.width + 2 * padding,
+        bounding_box.height + 2 * padding + type_padding,
+    )
     surface = new_surface(w, h)
     cr = cairo.Context(surface)
 
@@ -32,10 +30,30 @@ def render(diagram, new_surface, padding=8):
         cr.set_source_rgba(*bg_color)
         cr.fill()
 
-    cr.translate(-bounding_box.x + padding, -bounding_box.y + padding)
+    cr.translate(-bounding_box.x + padding, -bounding_box.y + padding + type_padding)
     painter.paint(diagram.get_all_items(), cr)
     cr.show_page()
     return surface
+
+
+def diagram_type_height(diagram):
+    if not diagram.diagramType:
+        return 0
+
+    bounding_box = calc_bounding_box(diagram, DiagramTypePainter(diagram))
+
+    return bounding_box.height
+
+
+def calc_bounding_box(diagram, painter):
+    tmpsurface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 0, 0)
+    tmpcr = cairo.Context(tmpsurface)
+    bounding_box = BoundingBoxPainter(painter).bounding_box(
+        diagram.get_all_items(), tmpcr
+    )
+    tmpcr.show_page()
+    tmpsurface.flush()
+    return bounding_box
 
 
 def save_svg(filename, diagram):
@@ -60,10 +78,8 @@ def save_pdf(filename, diagram):
 
 def new_painter(diagram):
     style = diagram.style(StyledDiagram(diagram))
-
     sloppiness = style.get("line-style", 0.0)
-
-    if sloppiness:
-        return FreeHandPainter(ItemPainter(), sloppiness)
-    else:
-        return ItemPainter()
+    item_painter = (
+        FreeHandPainter(ItemPainter(), sloppiness) if sloppiness else ItemPainter()
+    )
+    return PainterChain().append(item_painter).append(DiagramTypePainter(diagram))
