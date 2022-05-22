657da5f8752ae72751d4ef88439d2626bdeebe94
Arjan Molenaar <gaphor@gmail.com>
2022-05-22 22:01:48 +0200

    Add and remove elements from list model

--- gaphor/ui/tests/test_treemodel.py
+++ gaphor/ui/tests/test_treemodel.py
@@ -1,5 +1,27 @@
+import pytest
+
 from gaphor import UML
-from gaphor.ui.treemodel import TreeItem, TreeModel
+from gaphor.ui.treemodel import TreeComponent, TreeItem, TreeModel
+
+
+@pytest.fixture
+def tree_component(event_manager, element_factory):
+    tree_component = TreeComponent(event_manager, element_factory)
+    tree_component.open()
+    yield tree_component
+    tree_component.close()
+
+
+class ItemChangedHandler:
+    def __init__(self):
+        self.added = 0
+        self.removed = 0
+        self.positions = []
+
+    def __call__(self, _obj, position, removed, added):
+        self.positions.append(position)
+        self.removed += removed
+        self.added += added
 
 
 def test_tree_item_gtype():
@@ -17,5 +39,38 @@ def test_tree_model_with_element(element_factory):
     element = element_factory.create(UML.Class)
     tree_model = TreeModel(element_factory, owner=None)
 
-    assert tree_model.get_n_items() == 1, tree_model.items
+    assert tree_model.get_n_items() == 1
     assert tree_model.get_item(0).element is element
+
+
+def test_tree_component_add_element(tree_component, element_factory):
+    tree_model = tree_component.model
+    items_changed = ItemChangedHandler()
+    tree_model.connect("items-changed", items_changed)
+
+    element = element_factory.create(UML.Class)
+
+    assert tree_model.get_n_items() == 1
+    assert tree_model.get_item(0).element is element
+    assert items_changed.added == 1
+
+
+def test_tree_component_remove_element(tree_component, element_factory):
+    tree_model = tree_component.model
+    element = element_factory.create(UML.Class)
+    items_changed = ItemChangedHandler()
+    tree_model.connect("items-changed", items_changed)
+
+    element.unlink()
+
+    assert tree_model.get_n_items() == 0
+    assert items_changed.removed == 1
+
+
+# Test: delete element in tree component
+# Test: create nested element in tree component
+# Test: delete nested element in tree component
+# Test: ownership changes
+# Formatting: diagrams bold, abstract elements italic
+# Test: name change in (nested) element
+# Test: relationships in separate subtree
--- gaphor/ui/treemodel.py
+++ gaphor/ui/treemodel.py
@@ -2,8 +2,13 @@ from __future__ import annotations
 
 from gi.repository import Gio, GLib, GObject, Gtk
 
+from gaphor.abc import ActionProvider
+from gaphor.core import event_handler
+from gaphor.core.modeling import ElementCreated
+from gaphor.core.modeling.event import ElementDeleted
 from gaphor.diagram.iconname import get_icon_name
 from gaphor.i18n import gettext, translated_ui_string
+from gaphor.ui.abc import UIComponent
 
 
 class TreeItem(GObject.GObject):
@@ -32,6 +37,15 @@ class TreeModel(GObject.Object, Gio.ListModel):
         super().__init__()
         self.items = [TreeItem(e) for e in element_factory.select() if e.owner is owner]
 
+    def add_element(self, element):
+        self.items.append(TreeItem(element))
+        self.items_changed(len(self.items), 0, 1)
+
+    def remove_element(self, element):
+        index = next(i for i, e in enumerate(self.items) if e.element is element)
+        del self.items[index]
+        self.items_changed(index, 1, 0)
+
     def do_get_item_type(self) -> GObject.GType:
         return TreeItem.__gtype__
 
@@ -46,26 +60,49 @@ class TreeModel(GObject.Object, Gio.ListModel):
 
 
 def new_list_item_ui():
-    b = translated_ui_string("gaphor.ui", "viewitem.ui")
+    b = translated_ui_string("gaphor.ui", "treeitem.ui")
     return GLib.Bytes.new(b.encode("utf-8"))
 
 
-def tree_view(element_factory):
-    def child_model(item, user_data):
-        if not item.child_model:
-            item.child_model = TreeModel(element_factory, owner=item.element)
-        return item.child_model
+class TreeComponent(UIComponent, ActionProvider):
+    def __init__(self, event_manager, element_factory):
+        self.event_manager = event_manager
+        self.element_factory = element_factory
+        self.model = TreeModel(self.element_factory, owner=None)
 
-    model = TreeModel(element_factory, owner=None)
-    tree = Gtk.TreeListModel.new(
-        model,
-        passthrough=False,
-        autoexpand=False,
-        create_func=child_model,
-        user_data=None,
-    )
+    def open(self):
+        self.event_manager.subscribe(self.on_element_create)
+        self.event_manager.subscribe(self.on_element_delete)
 
-    selection = Gtk.SingleSelection.new(tree)
-    factory = Gtk.BuilderListItemFactory.new_from_bytes(None, new_list_item_ui())
+        def child_model(item, user_data):
+            if not item.child_model:
+                item.child_model = TreeModel(self.element_factory, owner=item.element)
+            return item.child_model
 
-    return Gtk.ListView.new(selection, factory)
+        tree = Gtk.TreeListModel.new(
+            self.model,
+            passthrough=False,
+            autoexpand=False,
+            create_func=child_model,
+            user_data=None,
+        )
+
+        selection = Gtk.SingleSelection.new(tree)
+        factory = Gtk.BuilderListItemFactory.new_from_bytes(None, new_list_item_ui())
+
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        scrolled_window.set_child(Gtk.ListView.new(selection, factory))
+        return scrolled_window
+
+    def close(self):
+        self.event_manager.unsubscribe(self.on_element_create)
+        self.event_manager.unsubscribe(self.on_element_delete)
+
+    @event_handler(ElementCreated)
+    def on_element_create(self, event):
+        self.model.add_element(event.element)
+
+    @event_handler(ElementDeleted)
+    def on_element_delete(self, event):
+        self.model.remove_element(event.element)
