6d658b1529e06a29fb926f1b00c953dd84c455d8
Arjan Molenaar <gaphor@gmail.com>
2022-06-08 10:16:55 +0200

    Let CI do more hypothesis tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,5 @@ from gaphor.conftest import test_models
 settings.register_profile(
     "test", derandomize=True, max_examples=50, stateful_step_count=20
 )
-settings.register_profile("ci", max_examples=2000)
+settings.register_profile("ci", max_examples=5000)
 settings.load_profile("test")
