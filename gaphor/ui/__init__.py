d1a7f23bcc6f69e2bbc396e505f0a89234ff4be7
Dan Yeaw <dan@yeaw.me>
2022-07-10 13:13:07 -0400

    Fix loading Gtk 3 when Gtk 4 is already loaded

--- gaphor/ui/__init__.py
+++ gaphor/ui/__init__.py
@@ -13,7 +13,7 @@ import gi
 if os.getenv("GAPHOR_USE_GTK") != "NONE":
     # Allow to explicitly *not* initialize GTK (for docs, mainly)
     gtk_version = "3.0" if os.getenv("GAPHOR_USE_GTK") == "3" else "4.0"
-    gtk_source_version = "4" if os.getenv("GAPHOR_USE_GTK") == "4" else "5"
+    gtk_source_version = "4" if os.getenv("GAPHOR_USE_GTK") == "3" else "5"
 
     gi.require_version("Gtk", gtk_version)
     gi.require_version("Gdk", gtk_version)
