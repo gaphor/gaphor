0b6d16b3c5bb14757598b0ecbda92d0f6291c264
Arjan Molenaar <gaphor@gmail.com>
2022-08-04 15:47:20 +0200

    Support >2 handles on lines

--- gaphor/plugins/autolayout/pydot.py
+++ gaphor/plugins/autolayout/pydot.py
@@ -2,6 +2,7 @@ from functools import singledispatch
 
 import pydot
 from gaphas.connector import ConnectionSink, Connector
+from gaphas.segment import Segment
 
 from gaphor.abc import ActionProvider, Service
 from gaphor.action import action
@@ -75,11 +76,16 @@ class AutoLayout(Service, ActionProvider):
                 )
                 if presentation:
                     points = parse_edge_pos(edge.get_pos())
-                    assert len(points) == 2
-                    assert len(presentation.handles()) == 2
 
                     if isinstance(presentation, GeneralizationItem):
                         points.reverse()
+
+                    presentation.orthogonal = False
+                    segment = Segment(presentation, diagram)
+                    while len(points) > len(presentation.handles()):
+                        segment.split_segment(0)
+                    while 2 < len(points) < len(presentation.handles()):
+                        segment.merge_segment(0)
 
                     matrix = presentation.matrix_i2c.inverse()
                     for handle, point in zip(presentation.handles(), points):
