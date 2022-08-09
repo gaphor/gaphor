1fb2dcb137243beb79a53e5ae43c668e10c27fde
Arjan Molenaar <gaphor@gmail.com>
2022-08-09 17:35:59 +0200

    Provide presentations for all kinds of pseudostate

--- gaphor/UML/states/pseudostates.py
+++ gaphor/UML/states/pseudostates.py
@@ -33,7 +33,7 @@ class PseudostateItem(ElementPresentation, Named):
     subject: relation_one[UML.Pseudostate]
 
     def __init__(self, diagram, id=None):
-        super().__init__(diagram, id, width=20, height=20)
+        super().__init__(diagram, id, width=6, height=20)
         for h in self.handles():
             h.movable = False
 
@@ -55,7 +55,7 @@ class PseudostateItem(ElementPresentation, Named):
         )
 
 
-def draw_initial_pseudostate(box, context, bounding_box):
+def draw_initial_or_junction_pseudostate(box, context, bounding_box):
     """Draw initial pseudostate symbol."""
     cr = context.cairo
     if stroke := context.style["color"]:
@@ -67,13 +67,16 @@ def draw_initial_pseudostate(box, context, bounding_box):
     cr.fill()
 
 
-def draw_history_pseudostate(box, context, bounding_box):
-    cr = context.cairo
-    r = 15
+def _circle(context, r=15):
     d = r * 2
-    path_ellipse(cr, r, r, d, d)
+    path_ellipse(context.cairo, r, r, d, d)
     stroke(context)
 
+
+def draw_deep_history_pseudostate(box, context, bounding_box):
+    _circle(context)
+
+    cr = context.cairo
     cr.move_to(12, 10)
     cr.line_to(12, 20)
     cr.move_to(18, 10)
@@ -83,15 +86,72 @@ def draw_history_pseudostate(box, context, bounding_box):
     stroke(context, fill=False)
 
 
+def draw_shallow_history_pseudostate(box, context, bounding_box):
+    _circle(context)
+
+    cr = context.cairo
+    cr.move_to(12, 10)
+    cr.line_to(12, 20)
+    cr.move_to(18, 10)
+    cr.line_to(18, 20)
+    cr.move_to(12, 15)
+    cr.line_to(18, 15)
+    stroke(context, fill=False)
+
+
+def draw_join_or_fork_pseudostate(box, context, bounding_box):
+    cr = context.cairo
+    if stroke := context.style.get("color"):
+        cr.set_source_rgba(*stroke)
+
+    cr.set_line_width(6)
+    cr.move_to(3, 0)
+    cr.line_to(3, 40)
+    cr.stroke()
+
+
+def draw_choice_pseudostate(box, context, bounding_box):
+    cr = context.cairo
+    cr.move_to(15, 0)
+    cr.line_to(30, 15)
+    cr.line_to(15, 30)
+    cr.line_to(0, 15)
+    cr.close_path()
+    stroke(context, fill=False)
+
+
+def draw_entry_point_pseudostate(box, context, bounding_box):
+    _circle(context)
+
+
+def draw_exit_point_pseudostate(box, context, bounding_box):
+    _circle(context)
+    cr = context.cairo
+    cr.move_to(5, 5)
+    cr.line_to(25, 25)
+    cr.move_to(5, 25)
+    cr.line_to(25, 5)
+    stroke(context, fill=False)
+
+
+def draw_terminate_pseudostate(box, context, bounding_box):
+    cr = context.cairo
+    cr.move_to(0, 0)
+    cr.line_to(30, 20)
+    cr.move_to(0, 20)
+    cr.line_to(30, 0)
+    stroke(context, fill=False)
+
+
 PSEUSOSTATE_SHAPE = {
-    "initial": (draw_initial_pseudostate, 20, 20),
-    "deepHistory": (draw_history_pseudostate, 30, 30),
-    "shallowHistory": (draw_history_pseudostate, 30, 30),
-    "join": (draw_history_pseudostate, 30, 30),
-    "fork": (draw_history_pseudostate, 30, 30),
-    "junction": (draw_history_pseudostate, 30, 30),
-    "choice": (draw_history_pseudostate, 30, 30),
-    "entryPoint": (draw_history_pseudostate, 30, 30),
-    "exitPoint": (draw_history_pseudostate, 30, 30),
-    "terminate": (draw_history_pseudostate, 30, 30),
+    "initial": (draw_initial_or_junction_pseudostate, 20, 20),
+    "deepHistory": (draw_deep_history_pseudostate, 30, 30),
+    "shallowHistory": (draw_shallow_history_pseudostate, 30, 30),
+    "join": (draw_join_or_fork_pseudostate, 6, 40),
+    "fork": (draw_join_or_fork_pseudostate, 6, 40),
+    "junction": (draw_initial_or_junction_pseudostate, 20, 20),
+    "choice": (draw_choice_pseudostate, 30, 30),
+    "entryPoint": (draw_entry_point_pseudostate, 30, 30),
+    "exitPoint": (draw_exit_point_pseudostate, 30, 30),
+    "terminate": (draw_terminate_pseudostate, 30, 20),
 }
