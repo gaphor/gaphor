8a05b2b41f50f0597c3bd686e58b7628ee96129c
Arjan Molenaar <gaphor@gmail.com>
2022-08-11 13:26:34 +0200

    Add presentation for state machine

---
+++ gaphor/UML/states/statemachine.py
@@ -0,0 +1,43 @@
+from gaphor import UML
+from gaphor.core import gettext
+from gaphor.core.modeling.properties import attribute
+from gaphor.core.styling import FontWeight, VerticalAlign
+from gaphor.diagram.presentation import Classified, ElementPresentation
+from gaphor.diagram.shapes import Box, Text, draw_border
+from gaphor.diagram.support import represents
+from gaphor.UML.classes.stereotype import stereotype_compartments, stereotype_watches
+
+
+@represents(UML.StateMachine)
+class StateMachineItem(Classified, ElementPresentation):
+    def __init__(self, diagram, id=None):
+        super().__init__(diagram, id)
+
+        self.watch("show_stereotypes", self.update_shapes)
+        self.watch("subject[NamedElement].name")
+        stereotype_watches(self)
+
+    show_stereotypes: attribute[int] = attribute("show_stereotypes", int)
+
+    def update_shapes(self, event=None):
+        self.shape = Box(
+            Box(
+                Text(
+                    text=lambda: UML.recipes.stereotypes_str(
+                        self.subject, [gettext("statemachine")]
+                    ),
+                ),
+                Text(
+                    text=lambda: self.subject.name or "",
+                    style={"font-weight": FontWeight.BOLD},
+                ),
+                style={"padding": (4, 4, 4, 4)},
+            ),
+            *(self.show_stereotypes and stereotype_compartments(self.subject) or []),
+            style={
+                "vertical-align": VerticalAlign.TOP
+                if self.diagram and self.children
+                else VerticalAlign.MIDDLE,
+            },
+            draw=draw_border
+        )
