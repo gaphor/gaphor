191c3c879405f48540db5f093ee75cf27e43b60b
Arjan Molenaar <gaphor@gmail.com>
2022-08-11 13:26:34 +0200

    Add presentation for state machine

--- gaphor/UML/states/__init__.py
+++ gaphor/UML/states/__init__.py
@@ -2,11 +2,13 @@ from gaphor.UML.states import copypaste, propertypages, vertexconnect
 from gaphor.UML.states.finalstate import FinalStateItem
 from gaphor.UML.states.pseudostates import PseudostateItem
 from gaphor.UML.states.state import StateItem
+from gaphor.UML.states.statemachine import StateMachineItem
 from gaphor.UML.states.transition import TransitionItem
 
 __all__ = [
     "FinalStateItem",
     "PseudostateItem",
     "StateItem",
+    "StateMachineItem",
     "TransitionItem",
 ]
