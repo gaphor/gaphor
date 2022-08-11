a87b85528710bea3b0b32e397b3d3fc6c88edd39
Arjan Molenaar <gaphor@gmail.com>
2022-08-11 14:44:49 +0200

    Grouping for composite states

--- gaphor/UML/states/group.py
+++ gaphor/UML/states/group.py
@@ -1,24 +1,28 @@
+from __future__ import annotations
+
 from gaphor.core import gettext
 from gaphor.diagram.group import group, ungroup
-from gaphor.UML.uml import Region, StateMachine, Vertex
+from gaphor.UML.uml import Region, State, StateMachine, Vertex
 
 
+@group.register(State, Vertex)
 @group.register(StateMachine, Vertex)
-def state_machine_vertex_group(state_machine, vertex):
+def state_machine_vertex_group(state_machine: State | StateMachine, vertex: Vertex):
     """Add State to a State Machine, add a Region if necessary."""
     if state_machine.region:
         region = state_machine.region[0]
     else:
         region = state_machine.model.create(Region)
         region.name = gettext("Default Region")
-        region.stateMachine = state_machine
+        state_machine.region = region
 
     vertex.container = region
     return True
 
 
+@ungroup.register(State, Vertex)
 @ungroup.register(StateMachine, Vertex)
-def state_machine_vertex_ungroup(state_machine, vertex):
+def state_machine_vertex_ungroup(state_machine: State | StateMachine, vertex: Vertex):
     if vertex.container and vertex.container in state_machine.region:
         del vertex.container
         return True
