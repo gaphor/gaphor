36b1ea9776787018a7235be206b3bc8023e88efb
Arjan Molenaar <gaphor@gmail.com>
2022-08-13 16:19:02 +0200

    Draw regions in state machine

--- gaphor/UML/states/statemachine.py
+++ gaphor/UML/states/statemachine.py
@@ -3,7 +3,7 @@ from gaphor.core import gettext
 from gaphor.core.modeling.properties import attribute
 from gaphor.core.styling import FontWeight, VerticalAlign
 from gaphor.diagram.presentation import Classified, ElementPresentation
-from gaphor.diagram.shapes import Box, Text, draw_border
+from gaphor.diagram.shapes import Box, Text, draw_border, draw_top_separator
 from gaphor.diagram.support import represents
 from gaphor.UML.classes.stereotype import stereotype_compartments, stereotype_watches
 
@@ -19,6 +19,7 @@ class StateMachineItem(Classified, ElementPresentation):
         stereotype_watches(self)
 
     show_stereotypes: attribute[int] = attribute("show_stereotypes", int)
+    show_regions: attribute[int] = attribute("show_regions", int, default=True)
 
     def update_shapes(self, event=None):
         self.shape = Box(
@@ -35,10 +36,22 @@ class StateMachineItem(Classified, ElementPresentation):
                 style={"padding": (4, 4, 4, 4)},
             ),
             *(self.show_stereotypes and stereotype_compartments(self.subject) or []),
+            *(self.show_regions and region_compartment(self.subject) or []),
             style={
                 "vertical-align": VerticalAlign.TOP
-                if self.diagram and self.children
+                if (self.diagram and self.children) or self.show_regions
                 else VerticalAlign.MIDDLE,
             },
             draw=draw_border
+        )
+
+
+def region_compartment(subject):
+    if not subject:
+        return
+
+    for index, region in enumerate(subject.region):
+        yield Box(
+            style={"min-height": 100, "dash-style": (7, 3)},
+            draw=draw_top_separator if index > 0 else None,
         )
