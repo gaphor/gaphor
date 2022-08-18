d2aa0974af9bbca42de93550025a47e0a9b03559
Arjan Molenaar <gaphor@gmail.com>
2022-08-18 17:28:02 +0200

    Group transitions

--- gaphor/UML/states/group.py
+++ gaphor/UML/states/group.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from gaphor.core import gettext
 from gaphor.diagram.group import group, ungroup
-from gaphor.UML.uml import Region, State, StateMachine, Vertex
+from gaphor.UML.uml import Region, State, StateMachine, Transition, Vertex
 
 
 @group.register(State, Vertex)
@@ -30,14 +30,16 @@ def state_machine_vertex_ungroup(state_machine: State | StateMachine, vertex: Ve
 
 
 @group.register(Region, Vertex)
-def region_vertex_group(region, vertex):
-    vertex.container = region
+@group.register(Region, Transition)
+def region_vertex_group(region, vertex_or_transition):
+    vertex_or_transition.container = region
     return True
 
 
 @ungroup.register(Region, Vertex)
-def region_vertex_ungroup(region, vertex):
-    if vertex.container is region:
-        del vertex.container
+@ungroup.register(Region, Transition)
+def region_vertex_ungroup(region, vertex_or_transition):
+    if vertex_or_transition.container is region:
+        del vertex_or_transition.container
         return True
     return False
