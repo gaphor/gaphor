32e4d8128553eaa6f76dbdd237dbb2fa0a2439ae
Dan Yeaw <dan@yeaw.me>
2022-10-16 16:56:14 -0400

    Change to adwaita for switch to model dialog

--- gaphor/ui/appfilemanager.py
+++ gaphor/ui/appfilemanager.py
@@ -3,6 +3,11 @@ import os.path
 
 from gi.repository import Gtk
 
+if Gtk.get_major_version() == 4:
+    from gi.repository import Adw
+
+from pathlib import Path
+
 from gaphor.abc import ActionProvider, Service
 from gaphor.core import action, gettext
 from gaphor.ui.filedialog import open_file_dialog
@@ -40,24 +45,44 @@ class AppFileManager(Service, ActionProvider):
         def open_files(filenames):
             for filename in filenames:
                 if self.application.has_session(filename):
-                    dialog = Gtk.MessageDialog(
-                        message_type=Gtk.MessageType.QUESTION,
-                        buttons=Gtk.ButtonsType.YES_NO,
-                        text=gettext(
-                            "{filename} is already opened. Do you want to switch to the opened window instead?"
-                        ).format(filename=filename),
-                    )
-                    dialog.set_transient_for(self.parent_window)
+                    name = Path(filename).name
+                    title = gettext("Switch to {name}?").format(name=name)
+                    body = gettext(
+                        "{name} is already opened. Do you want to switch to the opened window instead?"
+                    ).format(name=name)
+                    if Gtk.get_major_version() == 3:
+                        dialog = Gtk.MessageDialog(
+                            message_type=Gtk.MessageType.QUESTION,
+                            buttons=Gtk.ButtonsType.YES_NO,
+                            text=title,
+                            secondary_text=body,
+                        )
+                        dialog.set_transient_for(self.parent_window)
+
+                        dialog.set_modal(True)
+                    else:
+                        dialog = Adw.MessageDialog.new(
+                            self.parent_window,
+                            title,
+                        )
+                        dialog.set_body(body)
+                        dialog.add_response("open", gettext("Open Again"))
+                        dialog.add_response("switch", gettext("Switch"))
+                        dialog.set_response_appearance(
+                            "switch", Adw.ResponseAppearance.SUGGESTED
+                        )
+                        dialog.set_default_response("switch")
+                        dialog.set_close_response("open")
 
                     def response(dialog, answer):
-                        force_new_session = answer != Gtk.ResponseType.YES
+                        # Gtk.ResponseType.NO is for GTK3, open is for GTK4
+                        force_new_session = answer in [Gtk.ResponseType.NO, "open"]
                         dialog.destroy()
                         self.application.new_session(
                             filename=filename, force=force_new_session
                         )
 
                     dialog.connect("response", response)
-                    dialog.set_modal(True)
                     dialog.show()
                 else:
                     self.application.new_session(filename=filename)
