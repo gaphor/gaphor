ad1c50c518d2c616abb2353da53d805784fc0df8
Arjan Molenaar <gaphor@gmail.com>
2022-10-26 23:06:34 +0200

    Speed up unit test runs
    Run fewer PBT examples, but run more steps.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from hypothesis import settings
 from gaphor.conftest import test_models
 
 settings.register_profile(
-    "test", derandomize=True, max_examples=50, stateful_step_count=20
+    "test", derandomize=True, max_examples=10, stateful_step_count=100
 )
 settings.register_profile("ci", max_examples=5000)
 settings.load_profile("test")
