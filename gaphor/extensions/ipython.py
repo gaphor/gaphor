aec742ca9c18b67601ef44344a4786fc60ffcad5
Arjan Molenaar <gaphor@gmail.com>
2022-10-28 16:10:53 +0200

    Render images as png in jupyter notebooks
    Rendering subsequent images as svg causes render errors in the
    Pango rendered text.
    
    Also use cairo.Surface as context, so it's always freed.

--- gaphor/extensions/ipython.py
+++ gaphor/extensions/ipython.py
@@ -2,11 +2,10 @@ from __future__ import annotations
 
 from io import BytesIO
 
-import cairo
-from IPython.display import SVG
+from IPython.display import SVG, DisplayObject, Image
 
 from gaphor.core.modeling import Diagram
-from gaphor.diagram.export import render
+from gaphor.diagram.export import save_png, save_svg
 from gaphor.plugins.autolayout import AutoLayout
 
 
@@ -15,9 +14,15 @@ def auto_layout(diagram: Diagram) -> None:
     auto_layout.layout(diagram)
 
 
-def draw(diagram: Diagram) -> SVG:
+def draw(diagram: Diagram, format="png") -> DisplayObject:
     buffer = BytesIO()
-    surface = render(diagram, lambda w, h: cairo.SVGSurface(buffer, w, h))
-    surface.flush()
-    surface.finish()
-    return SVG(buffer.getvalue())
+    if format == "svg":
+        save_svg(buffer, diagram)
+        return SVG(buffer.getvalue())
+    elif format == "png":
+        save_png(buffer, diagram)
+        return Image(buffer.getvalue())
+    else:
+        raise ValueError(
+            f"Unsupported file format {format}. Should be either svg or png"
+        )
