a52c98b1d7911711193d582637a98b9819f77601
Arjan Molenaar <gaphor@gmail.com>
2022-10-29 17:31:51 +0200

    Add extra test cases for diagrams component
    So it can easily support AdwTabView.

--- gaphor/ui/tests/test_mainwindow.py
+++ gaphor/ui/tests/test_mainwindow.py
@@ -1,9 +1,9 @@
 import pytest
 
 from gaphor.application import Session
-from gaphor.core.modeling import Diagram
+from gaphor.core.modeling import Comment, Diagram
 from gaphor.ui.abc import UIComponent
-from gaphor.ui.event import DiagramOpened
+from gaphor.ui.event import DiagramOpened, ElementOpened
 
 
 @pytest.fixture
@@ -24,6 +24,8 @@ def session():
             "tools_menu",
         ]
     )
+    main_w = session.get_service("main_window")
+    main_w.open()
     yield session
     session.shutdown()
 
@@ -37,17 +39,60 @@ def get_current_diagram(session):
 
 
 def test_creation(session):
-    # MainWindow should be created as resource
-    main_w = session.get_service("main_window")
-    main_w.open()
     assert get_current_diagram(session) is None
 
 
 def test_show_diagram(session):
     element_factory = session.get_service("element_factory")
     diagram = element_factory.create(Diagram)
-    main_w = session.get_service("main_window")
-    main_w.open()
+
     event_manager = session.get_service("event_manager")
     event_manager.handle(DiagramOpened(diagram))
     assert get_current_diagram(session) == diagram
+
+
+def test_close_diagram(session):
+    element_factory = session.get_service("element_factory")
+    diagram = element_factory.create(Diagram)
+
+    event_manager = session.get_service("event_manager")
+    event_manager.handle(DiagramOpened(diagram))
+
+    diagrams = session.get_service("diagrams")
+    diagrams.close_current_tab()
+
+    assert not get_current_diagram(session)
+
+
+def test_open_element_on_diagram(session):
+    element_factory = session.get_service("element_factory")
+    diagram = element_factory.create(Diagram)
+    comment = element_factory.create(Comment)
+
+    event_manager = session.get_service("event_manager")
+    event_manager.handle(DiagramOpened(diagram))
+
+    event_manager.handle(ElementOpened(comment))
+
+    assert comment.presentation
+    assert comment.presentation[0] in get_current_diagram(session).ownedPresentation
+
+
+def test_update_diagram_name(session):
+    element_factory = session.get_service("element_factory")
+    diagram = element_factory.create(Diagram)
+
+    event_manager = session.get_service("event_manager")
+    event_manager.handle(DiagramOpened(diagram))
+
+    diagram.name = "Foo"
+
+
+def test_flush_model(session):
+    element_factory = session.get_service("element_factory")
+    diagram = element_factory.create(Diagram)
+
+    event_manager = session.get_service("event_manager")
+    event_manager.handle(DiagramOpened(diagram))
+
+    element_factory.flush()
