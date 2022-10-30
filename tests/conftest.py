f558c09a7a366c635785d3c7ccf55b3f05109caf
Arjan Molenaar <gaphor@gmail.com>
2022-10-30 13:09:27 +0100

    Run fewer tests for hypothesis
    To prevent timeout.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ from gaphor.conftest import test_models
 settings.register_profile(
     "test", derandomize=True, max_examples=10, stateful_step_count=100
 )
-settings.register_profile("ci", max_examples=5000)
+settings.register_profile("ci", max_examples=4000)
 settings.load_profile("test")
