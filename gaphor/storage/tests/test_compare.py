df5072069c7a39e726636739d95ce3e9d412e38e
Arjan Molenaar <gaphor@gmail.com>
2022-11-01 23:11:30 +0100

    Add changes for values and references

--- gaphor/storage/tests/test_compare.py
+++ gaphor/storage/tests/test_compare.py
@@ -1,6 +1,6 @@
 import pytest
 
-from gaphor.core.modeling import Diagram, ElementFactory
+from gaphor.core.modeling import Diagram, Element, ElementFactory
 from gaphor.storage.compare import ADD, REMOVE, compare
 
 
@@ -15,34 +15,74 @@ def incoming():
 
 
 def test_empty_element_factories(current, incoming):
-    change_set = compare(current, incoming)
+    change_set = list(compare(current, incoming))
 
-    assert not change_set.changes
+    assert not change_set
+
+
+def test_similar_element_factories(current, incoming):
+    element = incoming.create(Element)
+    current.create_as(Element, element.id)
+
+    change_set = list(compare(current, incoming))
+
+    assert not change_set
 
 
 def test_added_element(current, incoming):
     diagram = incoming.create(Diagram)
 
-    change_set = compare(current, incoming)
+    change = next(compare(current, incoming))
 
-    change = change_set.changes[0]
-
-    assert len(change_set.changes) == 1
     assert change.type is ADD
+    assert change.element_id == diagram.id
     assert change.element_name == "Diagram"
     assert change.element_type == Diagram
+
+
+def test_added_element_with_attribute(current, incoming):
+    diagram = incoming.create(Diagram)
+    diagram.name = "Foo"
+
+    elem_change, attr_change = list(compare(current, incoming))
+
+    assert elem_change.type is ADD
+    assert elem_change.element_id == diagram.id
+    assert elem_change.element_name == "Diagram"
+    assert elem_change.element_type == Diagram
+
+    assert attr_change.type is ADD
+    assert attr_change.element_id == diagram.id
+    assert attr_change.property_name == "name"
+    assert attr_change.property_value == "Foo"
+
+
+def test_added_element_with_reference(current, incoming):
+    diagram = incoming.create(Diagram)
+    element = incoming.create(Element)
+    current.create_as(Element, element.id)
+    diagram.element = element
+
+    change, ref_change = list(compare(current, incoming))
+
+    assert change.type is ADD
     assert change.element_id == diagram.id
+    assert change.element_name == "Diagram"
+    assert change.element_type == Diagram
+
+    assert ref_change.type is ADD
+    assert ref_change.element_id == diagram.id
+    assert ref_change.property_name == "element"
+    assert ref_change.property_ref == element.id
 
 
 def test_removed_element(current, incoming):
     diagram = current.create(Diagram)
+    diagram.name = "Foo"
 
-    change_set = compare(current, incoming)
+    change = next(compare(current, incoming))
 
-    change = change_set.changes[0]
-
-    assert len(change_set.changes) == 1
     assert change.type is REMOVE
+    assert change.element_id == diagram.id
     assert change.element_name == "Diagram"
     assert change.element_type == Diagram
-    assert change.element_id == diagram.id
