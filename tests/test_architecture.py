eb026cce858d205adda3332aa03d879a38c1ccd9
Arjan Molenaar <gaphor@gmail.com>
2022-11-27 13:12:37 +0100

    Enforce modeling languages do not depend on GTK

--- tests/test_architecture.py
+++ tests/test_architecture.py
@@ -1,8 +1,9 @@
 from pytest_archon import archrule
 
 import gaphor
+from gaphor.entrypoint import load_entry_points
 
-GAHOR_CORE = [
+GAPHOR_CORE = [
     "gaphor.core*",
     "gaphor.abc",
     "gaphor.action",
@@ -12,14 +13,20 @@ GAHOR_CORE = [
     "gaphor.event",
 ]
 
+UI_LIBRARIES = [
+    "gi.repository.Gdk",
+    "gi.repository.Gtk",
+]
+
 
 def test_core_packages():
     (
         archrule("gaphor.core does not depend on the rest of the system")
         .match("gaphor.core*")
         .exclude("*.tests.*")
-        .may_import(*GAHOR_CORE)
+        .may_import(*GAPHOR_CORE)
         .should_not_import("gaphor*")
+        .should_not_import(*UI_LIBRARIES)
         .check(gaphor, skip_type_checking=True)
     )
 
@@ -31,11 +38,24 @@ def test_diagram_package():
         archrule("Diagrams are part of the core")
         .match("gaphor.diagram*")
         .exclude("*.tests.*")
-        .may_import(*GAHOR_CORE)
+        .may_import(*GAPHOR_CORE)
         .may_import("gaphor.diagram*")
         .may_import("gaphor.UML.recipes")
         .may_import("gaphor.UML.uml")
         .should_not_import("gaphor*")
+        .check(gaphor)
+    )
+
+    (
+        archrule("GTK dependencies")
+        .match("gaphor.diagram*")
+        .exclude("*.tests.*")
+        .exclude("gaphor.diagram.hoversupport")
+        .exclude("gaphor.diagram.general.uicomponents")
+        .exclude("gaphor.diagram.tools*")
+        .exclude("gaphor.diagram.*editors")
+        .exclude("gaphor.diagram.*propertypages")
+        .should_not_import(*UI_LIBRARIES)
         .check(gaphor)
     )
 
@@ -45,10 +65,19 @@ def test_services_package():
         archrule("Services only depend on core functionality")
         .match("gaphor.services*")
         .exclude("*.tests.*")
-        .may_import(*GAHOR_CORE)
+        .may_import(*GAPHOR_CORE)
         .may_import("gaphor.diagram*")
         .may_import("gaphor.services*")
         .should_not_import("gaphor*")
+        .check(gaphor)
+    )
+
+    (
+        archrule("Only Copy service depends on UI libraries")
+        .match("gaphor.services*")
+        .exclude("gaphor.services.copyservice")
+        .exclude("*.tests.*")
+        .should_not_import(*UI_LIBRARIES)
         .check(gaphor)
     )
 
@@ -58,11 +87,12 @@ def test_storage_package():
         archrule("Storage only depends on core functionality")
         .match("gaphor.storage*")
         .exclude("*.tests.*")
-        .may_import(*GAHOR_CORE)
+        .may_import(*GAPHOR_CORE)
         .may_import("gaphor.diagram*")
         .may_import("gaphor.storage*")
         .may_import("gaphor.application", "gaphor.services.componentregistry")
         .should_not_import("gaphor*")
+        .should_not_import(*UI_LIBRARIES)
         .check(gaphor)
     )
 
@@ -72,6 +102,20 @@ def test_modeling_languages_do_not_depend_on_ui_package():
         archrule("Modeling languages should not depend on the UI package")
         .match("gaphor.C4Model*", "gaphor.RAAML*", "gaphor.SysML*", "gaphor.UML*")
         .should_not_import("gaphor.ui*")
+        .check(gaphor)
+    )
+
+
+def test_moduling_languages_should_initialize_without_gtk():
+    modeling_languages = [  # type: ignore[var-annotated]
+        c.__module__ for c in load_entry_points("gaphor.modelinglanguages").values()
+    ]
+    assert modeling_languages
+
+    (
+        archrule("No GTK dependency for modeling languages")
+        .match(*modeling_languages)
+        .should_not_import(*UI_LIBRARIES)
         .check(gaphor)
     )
 
