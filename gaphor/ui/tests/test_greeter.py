b541b86922b39f445ae6f0e9cd3988ba2dcfb7ce
Arjan Molenaar <gaphor@gmail.com>
2022-11-30 13:16:12 +0100

    Fix unit tests

--- gaphor/ui/tests/test_greeter.py
+++ gaphor/ui/tests/test_greeter.py
@@ -1,3 +1,6 @@
+import pytest
+from gi.repository import Gtk
+
 from gaphor.diagram.tests.fixtures import find
 from gaphor.ui import APPLICATION_ID
 from gaphor.ui.greeter import Greeter
@@ -35,17 +38,18 @@ def test_greeter_with_recent_files(event_manager):
     greeter.open()
     widget = greeter.greeter
 
-    frame = find(widget, "recent-files-frame")
+    recent_files = find(widget, "recent-files")
 
-    assert frame.get_visible()
+    assert recent_files.get_visible()
 
 
+@pytest.mark.skipif(Gtk.get_major_version() == 3, reason="Works only for GTK4")
 def test_greeter_with_no_recent_files(event_manager):
     recent_manager = RecentManagerStub()
     greeter = Greeter(None, event_manager, recent_manager)
 
     greeter.open()
     widget = greeter.greeter
-    frame = find(widget, "recent-files-frame")
+    recent_files = find(widget, "recent-files")
 
-    assert not frame.get_visible()
+    assert not recent_files.get_visible()
