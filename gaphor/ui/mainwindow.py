d9bf873bdf741a0833d150dddd47414eb38fb48f
Arjan Molenaar <gaphor@gmail.com>
2022-11-30 11:26:24 +0100

    Swap menu entries: New before Open

--- gaphor/ui/mainwindow.py
+++ gaphor/ui/mainwindow.py
@@ -40,8 +40,8 @@ def create_hamburger_model(export_menu, tools_menu):
     model = Gio.Menu.new()
 
     part = Gio.Menu.new()
-    part.append(gettext("Open a Model…"), "app.file-open")
     part.append(gettext("New Model…"), "app.new-model")
+    part.append(gettext("Open a Model…"), "app.file-open")
     model.append_section(None, part)
 
     part = Gio.Menu.new()
