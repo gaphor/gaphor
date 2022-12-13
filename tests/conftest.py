75fc985552ce3e6de70d213a8959cca705d0a59c
Arjan Molenaar <gaphor@gmail.com>
2022-12-13 14:16:56 +0100

    Add element factory fixtures for integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 from hypothesis import settings
 
-from gaphor.conftest import test_models
+from gaphor.conftest import (
+    element_factory,
+    event_manager,
+    modeling_language,
+    test_models,
+)
 
 settings.register_profile(
     "test", derandomize=True, max_examples=10, stateful_step_count=100
