dbe8be216c097706151a9a6f086afcc7781278e3
Arjan Molenaar <gaphor@gmail.com>
2023-01-15 09:41:32 +0100

    Send extra Current diagram changed event on diagram name change

--- gaphor/ui/diagrams.py
+++ gaphor/ui/diagrams.py
@@ -386,9 +386,13 @@ class Diagrams(UIComponent, ActionProvider):
                     self._notebook.set_tab_label(
                         widget, tab_label(event.new_value, widget, self.event_manager)
                     )
+                    if page == self._notebook.get_current_page():
+                        self.event_manager.handle(CurrentDiagramChanged(event.element))
                     return
                 elif event.element is widget.get_child().diagram_page.diagram:
                     widget.set_title(event.new_value or "")
+                    if widget is self._notebook.get_selected_page():
+                        self.event_manager.handle(CurrentDiagramChanged(event.element))
                     return
 
 
