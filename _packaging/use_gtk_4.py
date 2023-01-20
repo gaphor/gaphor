d911bd2944863827a34ca23d3f827317c300d57f
Arjan Molenaar <gaphor@gmail.com>
2023-01-20 10:11:49 +0100

    fix debug output

--- _packaging/use_gtk_4.py
+++ _packaging/use_gtk_4.py
@@ -4,7 +4,7 @@ import sys
 import traceback
 
 os.environ["GAPHOR_USE_GTK"] = "4"
-print("XDG_DATA_DIRS", os.environ["XDG_DATA_DIRS"])
+print("XDG_DATA_DIRS", os.getenv("XDG_DATA_DIRS") or "<empty>")
 pprint.pprint(sys.modules)
 
 traceback.print_stack()
