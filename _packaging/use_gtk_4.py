40dacfd4b0d923bc47735bbec64746f68fc45a24
Arjan Molenaar <gaphor@gmail.com>
2023-01-20 10:02:58 +0100

    More debug info

--- _packaging/use_gtk_4.py
+++ _packaging/use_gtk_4.py
@@ -1,3 +1,10 @@
 import os
+import pprint
+import sys
+import traceback
 
 os.environ["GAPHOR_USE_GTK"] = "4"
+print("XDG_DATA_DIRS", os.environ["XDG_DATA_DIRS"])
+pprint.pprint(sys.modules)
+
+traceback.print_stack()
