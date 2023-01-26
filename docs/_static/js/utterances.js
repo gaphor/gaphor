712e59f0b1a0d56f9f666c5e81216e5ac875f909
Arjan Molenaar <gaphor@gmail.com>
2023-01-26 16:57:59 +0100

    Add little script to inject utterances

---
+++ docs/_static/js/utterances.js
@@ -0,0 +1,17 @@
+function addUtterances() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://utteranc.es/client.js";
+    script.async = "async";
+    script.setAttribute("repo", "gaphor/gaphor-utterances");
+    script.setAttribute("issue-term", "pathname");
+    script.setAttribute("theme", "preferred-color-scheme");
+    script.setAttribute("crossorigin", "anonymous");
+
+    var bottomOfPage = document.querySelector("footer > .bottom-of-page");
+    if (bottomOfPage) {
+        bottomOfPage.parentNode.insertBefore(script, bottomOfPage);
+    }
+}
+
+addUtterances();
