c2937695c44f9490030bfb0940f0bd8e35d7b7d4
Arjan Molenaar <gaphor@gmail.com>
2023-03-11 20:19:40 +0100

    Remove all .glade files

--- gaphor/tests/test_i18n.py
+++ gaphor/tests/test_i18n.py
@@ -2,6 +2,6 @@ from gaphor.i18n import translated_ui_string
 
 
 def test_ui_builder_builds_from_resources():
-    ui_xml = translated_ui_string("gaphor.diagram", "propertypages.glade")
+    ui_xml = translated_ui_string("gaphor.diagram", "propertypages.ui")
 
     assert "translatable=" not in ui_xml
