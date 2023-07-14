ce2c0a062d217fb11581d079bb922459eaa1b1ce
Arjan Molenaar <gaphor@gmail.com>
2023-07-14 08:36:04 +0200

    Packages also copy they owned elements

--- gaphor/UML/classes/copypaste.py
+++ gaphor/UML/classes/copypaste.py
@@ -1,7 +1,15 @@
 import itertools
 
 from gaphor.diagram.copypaste import copy
-from gaphor.UML import Association, Class, DataType, Enumeration, Interface, Operation
+from gaphor.UML import (
+    Association,
+    Class,
+    DataType,
+    Enumeration,
+    Interface,
+    Operation,
+    Package,
+)
 from gaphor.UML.copypaste import copy_named_element
 
 
@@ -40,3 +48,12 @@ def copy_association(element: Association):
     yield element.id, copy_named_element(element)
     for end in element.memberEnd:
         yield from copy(end)
+
+
+@copy.register
+def copy_package(element: Package):
+    yield element.id, copy_named_element(element)
+    for owned in itertools.chain(
+        element.ownedType, element.ownedDiagram, element.ownedRule
+    ):
+        yield from copy(owned)
