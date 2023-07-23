caa615a694ca953a6f7d5b61a9120f0464482891
Arjan Molenaar <gaphor@gmail.com>
2023-07-23 12:52:49 +0200

    Avoid copy cycles when fully pasting diagram items

--- gaphor/UML/tests/test_copypaste.py
+++ gaphor/UML/tests/test_copypaste.py
@@ -1,5 +1,8 @@
+from gaphor.core.modeling import Diagram
 from gaphor.diagram.copypaste import copy_full, paste_full
 from gaphor.diagram.tests.test_copypaste_link import two_classes_and_a_generalization
+from gaphor.UML.classes import PackageItem
+from gaphor import UML
 
 
 def test_copy_connected_item(diagram, element_factory):
@@ -13,3 +16,40 @@ def test_copy_connected_item(diagram, element_factory):
     assert new_cls_item.subject
     assert gen_item.subject.general is gen_cls_item.subject
     assert gen_item.subject.specific is spc_cls_item.subject
+
+
+def test_full_copy_package_from_owned_diagram(element_factory):
+    diagram: Diagram = element_factory.create(Diagram)
+    package_item = diagram.create(
+        PackageItem, subject=element_factory.create(UML.Package)
+    )
+    package_item.subject.ownedDiagram = diagram
+
+    copy_buffer = copy_full([package_item], element_factory.lookup)
+
+    new_diagram: Diagram = element_factory.create(Diagram)
+
+    (new_package_item,) = paste_full(copy_buffer, new_diagram)
+
+    assert new_package_item.subject
+    assert new_package_item.subject is not package_item.subject
+
+
+def test_full_copy_package_from_owned_diagram_in_super_package(element_factory):
+    diagram: Diagram = element_factory.create(Diagram)
+    package_item = diagram.create(
+        PackageItem, subject=element_factory.create(UML.Package)
+    )
+    package_item.subject.ownedDiagram = diagram
+
+    copy_buffer = copy_full([package_item], element_factory.lookup)
+
+    new_diagram: Diagram = element_factory.create(Diagram)
+    new_diagram.element = element_factory.create(UML.Package)
+
+    (new_package_item,) = paste_full(copy_buffer, new_diagram)
+
+    assert diagram.owner is package_item.subject
+    assert new_package_item.subject
+    assert new_package_item.subject is not package_item.subject
+    assert new_package_item.subject.owner is new_diagram.owner
