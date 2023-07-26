2872bfe6f1112bf8dba0f1ff10c5de92be1f8410
Arjan Molenaar <gaphor@gmail.com>
2023-07-26 16:33:20 +0200

    Clean up quotes in metadata action

--- .github/scripts/metadata.sh
+++ .github/scripts/metadata.sh
@@ -20,7 +20,7 @@ else
     RELEASE="false"
 
     # Update version, so it will also show in the Gaphor application
-    poetry version "${VERSION}""${REV}"
+    poetry version "${VERSION}${REV}"
 fi
 
 echo "version=${VERSION}${REV}" >> "$GITHUB_OUTPUT"
