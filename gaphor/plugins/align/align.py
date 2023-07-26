772b6b990e857e29cc2f6302e0cb216925a1c4b4
Zsolt Sandor <zsolt.sandor@gmail.com>
2023-07-26 06:43:13 +0200

    Align left and right is working...

--- gaphor/plugins/align/align.py
+++ gaphor/plugins/align/align.py
@@ -14,7 +14,6 @@ from gaphor.diagram.presentation import ElementPresentation
 
 from gaphor.transaction import Transaction
 
-
 class AlignService(Service, ActionProvider):
     def __init__(self, event_manager, diagrams, tools_menu=None, dump_gv=False):
         self.event_manager = event_manager
@@ -37,27 +36,35 @@ class AlignService(Service, ActionProvider):
         name="align-left", label=gettext("Align left")
     )
     def align_left(self):
+        self._align_elements(self._align_elements_left)
+
+    @action(
+        name="align-right", label=gettext("Align right")
+    )
+    def align_right(self):
+        self._align_elements(self._align_elements_right)
+
+    def _align_elements(self, f):
         if current_diagram := self.diagrams.get_current_diagram():
             elements = {item for item in self.event.selected_items if isinstance(item, ElementPresentation)}
             with Transaction(self.event_manager):
-                self._align_elements_left(elements)
+                f(elements)
                 current_diagram.update_now(current_diagram.get_all_items())
 
     def _align_elements_left(self, elements: set[ElementPresentation]):
-            min_x = min(set(map(lambda item : self.pos_x(item), elements)))
+            left_edge = min(set(map(lambda item : self._pos_x(item), elements)))
             for item in elements:
-                item.matrix.translate(min_x - self.pos_x(item), 0)
+                item.matrix.translate(left_edge - self._pos_x(item), 0)
             
     def _align_elements_right(self, elements: set[ElementPresentation]):
-            min_x = min(set(map(lambda item : self.pos_x(item), elements)))
-
+            right_edge = max(set(map(lambda item : self._pos_x(item) + item.width, elements)))
             for item in elements:
-                item.matrix.translate(min_x - self.pos_x(item), 0)
+                item.matrix.translate(right_edge - (self._pos_x(item) + item.width), 0)
 
-    def pos_x(self, item: ElementPresentation):
+    def _pos_x(self, item: ElementPresentation):
         _,_,_,_,x,_ = item.matrix.tuple()
         return x
 
-    def pos_y(self, item: ElementPresentation):
+    def _pos_y(self, item: ElementPresentation):
         _,_,_,_,_,y = item.matrix.tuple()
         return y
