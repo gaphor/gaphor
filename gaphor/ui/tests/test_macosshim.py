8f207b4bf4859debc02bd3e4428680c85d698e1c
Dan Yeaw <dan@yeaw.me>
2023-08-12 08:10:11 -0500

    Fix typo

--- gaphor/ui/tests/test_macosshim.py
+++ gaphor/ui/tests/test_macosshim.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 
-@pytest.mark.skipif(sys.platform != "darwin", reason="Only load shim om macOS")
+@pytest.mark.skipif(sys.platform != "darwin", reason="Only load shim on macOS")
 def test_macosshim_loaded():
     import gaphor.ui  # noqa: F401
 
