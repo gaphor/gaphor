d44f60040ef4cd999e7b56723156e6759354b413
Dan Yeaw <dan@yeaw.me>
2023-08-14 16:51:21 -0500

    Add to and from string functions for primitives

--- gaphor/UMLTypes/uml_types.py
+++ gaphor/UMLTypes/uml_types.py
@@ -14,11 +14,28 @@ eClassifiers = {}  # type: ignore
 getEClassifier = partial(Ecore.getEClassifier, searchspace=eClassifiers)
 
 Integer = EDataType("Integer", instanceClassName="int")
+Integer.from_string = lambda x: int(x)
 
 Real = EDataType("Real", instanceClassName="double")
+Real.from_string = lambda x: float(x)
 
 String = EDataType("String", instanceClassName="java.lang.String")
 
 UnlimitedNatural = EDataType("UnlimitedNatural", instanceClassName="int")
 
+
+def unlimited_from_string(x):
+    if x == "*":
+        return -1
+    value = int(x)
+    if value >= 0:
+        return value
+    raise ValueError("UnlimitedNatural must be a >= 0 value")
+
+
+UnlimitedNatural.from_string = unlimited_from_string
+UnlimitedNatural.to_string = lambda x: "*" if x < 0 else str(x)
+
 Boolean = EDataType("Boolean", instanceClassName="boolean")
+Boolean.to_string = lambda x: str(x).lower()
+Boolean.from_string = lambda x: x in ["True", "true"]
