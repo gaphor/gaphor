143294b3e7bad9fef25937bdd9689854241189a5
Zsolt Sandor <zsolt.sandor@gmail.com>
2023-08-19 20:40:54 +0200

    Fixed additional space error

--- gaphor/plugins/align/align.py
+++ gaphor/plugins/align/align.py
@@ -1,18 +1,14 @@
 from __future__ import annotations
 
-from gaphas.item import NW
-
 from gaphor.abc import ActionProvider, Service
-from gaphor.action import action
-
 from gaphor.i18n import gettext
 
 from gaphor.ui.event import DiagramSelectionChanged
 from gaphor.core import action, event_handler
-from gaphor.core.modeling import Diagram
 from gaphor.diagram.presentation import ElementPresentation
 
 from gaphor.transaction import Transaction
+
 
 class AlignService(Service, ActionProvider):
     def __init__(self, event_manager, diagrams, tools_menu=None, dump_gv=False):
@@ -32,61 +28,42 @@ class AlignService(Service, ActionProvider):
     @event_handler(DiagramSelectionChanged)
     def _selection_changed(self, event=None, focused_item=None):
         self.event = event
-        pass
 
     # -------------------------------------------------------------------------
 
-    @action(
-        name="align-left", label=gettext("Align left")
-    )
+    @action(name="align-left", label=gettext("Align left"))
     def align_left(self):
         self._modify_elements(self._align_elements_left)
 
-    @action(
-        name="align-right", label=gettext("Align right")
-    )
+    @action(name="align-right", label=gettext("Align right"))
     def align_right(self):
         self._modify_elements(self._align_elements_right)
 
-    @action(
-        name="align-vertical-center", label=gettext("Align vertical center")
-    )
+    @action(name="align-vertical-center", label=gettext("Align vertical center"))
     def align_vertical_center(self):
         self._modify_elements(self._align_elements_vertical_center)
 
-    @action(
-        name="align-top", label=gettext("Align top")
-    )
+    @action(name="align-top", label=gettext("Align top"))
     def align_top(self):
         self._modify_elements(self._align_elements_top)
 
-    @action(
-        name="align-bottom", label=gettext("Align bottom")
-    )
+    @action(name="align-bottom", label=gettext("Align bottom"))
     def align_bottom(self):
         self._modify_elements(self._align_elements_bottom)
 
-    @action(
-        name="align-horizontal-center", label=gettext("Align horizontal center")
-    )
+    @action(name="align-horizontal-center", label=gettext("Align horizontal center"))
     def align_horizontal_center(self):
         self._modify_elements(self._align_elements_horizontal_center)
 
-    @action(
-        name="resize-max-width", label=gettext("Max width")
-    )
+    @action(name="resize-max-width", label=gettext("Max width"))
     def resize_max_width(self):
         self._modify_elements(self._resize_elements_max_width)
 
-    @action(
-        name="resize-max-height", label=gettext("Max height")
-    )
+    @action(name="resize-max-height", label=gettext("Max height"))
     def resize_max_height(self):
         self._modify_elements(self._resize_elements_max_height)
 
-    @action(
-        name="resize-max-size", label=gettext("Max size")
-    )
+    @action(name="resize-max-size", label=gettext("Max size"))
     def resize_max_size(self):
         self._modify_elements(self._resize_elements_max_size)
 
@@ -95,20 +72,22 @@ class AlignService(Service, ActionProvider):
     def _modify_elements(self, f):
         if self.event:
             if current_diagram := self.diagrams.get_current_diagram():
-                elements = {item for item in self.event.selected_items if isinstance(item, ElementPresentation)}
+                elements = {
+                    item
+                    for item in self.event.selected_items
+                    if isinstance(item, ElementPresentation)
+                }
                 with Transaction(self.event_manager):
                     f(elements)
                     current_diagram.update_now(current_diagram.get_all_items())
 
     def _align_elements_left(self, elements: set[ElementPresentation]):
-
         left_edge = self._left_edge(elements)
 
         for item in elements:
             item.matrix.translate(left_edge - self._pos_x(item), 0)
-            
-    def _align_elements_right(self, elements: set[ElementPresentation]):
 
+    def _align_elements_right(self, elements: set[ElementPresentation]):
         right_edge = self._right_edge(elements)
 
         for item in elements:
@@ -143,21 +122,18 @@ class AlignService(Service, ActionProvider):
             item.matrix.translate(0, center_edge - self._pos_y(item) - item.height / 2)
 
     def _resize_elements_max_width(self, elements: set[ElementPresentation]):
-
         max_width = self._max_width(elements)
 
         for item in elements:
             item.width = max_width
 
     def _resize_elements_max_height(self, elements: set[ElementPresentation]):
-
         max_height = self._max_height(elements)
 
         for item in elements:
             item.height = max_height
 
     def _resize_elements_max_size(self, elements: set[ElementPresentation]):
-
         max_width = self._max_width(elements)
         max_height = self._max_height(elements)
 
@@ -166,27 +142,27 @@ class AlignService(Service, ActionProvider):
             item.height = max_height
 
     def _left_edge(self, elements: set[ElementPresentation]):
-        return min(set(map(lambda item : self._pos_x(item), elements)))
+        return min({self._pos_x(item) for item in elements})
 
     def _right_edge(self, elements: set[ElementPresentation]):
-        return max(set(map(lambda item : self._pos_x(item) + item.width, elements)))
+        return max({self._pos_x(item) + item.width for item in elements})
 
     def _top_edge(self, elements: set[ElementPresentation]):
-        return min(set(map(lambda item : self._pos_y(item), elements)))
+        return min({self._pos_y(item) for item in elements})
 
     def _bottom_edge(self, elements: set[ElementPresentation]):
-        return max(set(map(lambda item : self._pos_y(item) + item.height, elements)))
+        return max({self._pos_y(item) + item.height for item in elements})
 
     def _max_width(self, elements: set[ElementPresentation]):
-        return max(set(map(lambda item : item.width, elements)))
+        return max({item.width for item in elements})
 
     def _max_height(self, elements: set[ElementPresentation]):
-        return max(set(map(lambda item : item.height, elements)))
+        return max({item.height for item in elements})
 
     def _pos_x(self, item: ElementPresentation):
-        _,_,_,_,x,_ = item.matrix.tuple()
+        _, _, _, _, x, _ = item.matrix.tuple()
         return x
 
     def _pos_y(self, item: ElementPresentation):
-        _,_,_,_,_,y = item.matrix.tuple()
+        _, _, _, _, _, y = item.matrix.tuple()
         return y
