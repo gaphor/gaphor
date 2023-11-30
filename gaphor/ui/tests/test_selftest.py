2b85cadc982a2a45c7cbb5cd2acbb9a533fc1f1a
Arjan Molenaar <gaphor@gmail.com>
2023-11-30 18:18:06 +0100

    Simplify GSettings schema self test

--- gaphor/ui/tests/test_selftest.py
+++ gaphor/ui/tests/test_selftest.py
@@ -1,7 +1,10 @@
+import logging
+
 from gaphor.ui import run
 
 
-def test_self_test():
+def test_self_test(caplog):
+    caplog.set_level(logging.DEBUG)
     exit_code = run(["--self-test"])
 
     assert exit_code == 0
