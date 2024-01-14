d10ea1dacd3dd61100d3f131e33910820bf97860
Arjan Molenaar <gaphor@gmail.com>
2024-01-14 13:16:00 +0100

    Remove inline styling from Comment and CommentLine

--- gaphor/diagram/general/comment.py
+++ gaphor/diagram/general/comment.py
@@ -1,11 +1,9 @@
 """CommentItem diagram item."""
 
-from functools import partial
 
 from gaphor.core.modeling import Comment
-from gaphor.core.styling import TextAlign, VerticalAlign
 from gaphor.diagram.presentation import ElementPresentation
-from gaphor.diagram.shapes import Box, Text, stroke
+from gaphor.diagram.shapes import Box, CssNode, Text, stroke
 from gaphor.diagram.support import represents
 
 
@@ -13,31 +11,27 @@ from gaphor.diagram.support import represents
 class CommentItem(ElementPresentation):
     def __init__(self, diagram, id=None):
         super().__init__(diagram, id)
-        offset = 4
-        ear = 12
-        self.min_width = ear + 2 * offset
         self.height = 50
         self.width = 100
 
-        self._body = Text(
-            text=lambda: self.subject.body or "",
-            style={
-                "text-align": TextAlign.LEFT,
-                "vertical-align": VerticalAlign.TOP,
-            },
-        )
-
         self.shape = Box(
-            self._body,
-            style={"padding": (offset, ear + offset, offset, offset)},
-            draw=partial(draw_border, ear=ear),
+            CssNode(
+                "body",
+                self.subject,
+                Text(
+                    text=lambda: self.subject.body or "",
+                ),
+            ),
+            draw=draw_border,
         )
         self.watch("subject[Comment].body")
 
 
-def draw_border(box, context, bounding_box, ear):
+def draw_border(box, context, bounding_box):
     cr = context.cairo
     x, y, w, h = bounding_box
+    _, padding_right, _, padding_left = context.style.get("padding", (4, 16, 4, 4))
+    ear = max(padding_right - padding_left, padding_left)
     line_to = cr.line_to
     cr.move_to(w - ear, y)
     line_to(w - ear, y + ear)
