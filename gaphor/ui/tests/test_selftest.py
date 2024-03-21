b66cb89cd2f6c59e6948d2e4a75f931b8cc84419
Arjan Molenaar <gaphor@gmail.com>
2024-03-21 23:34:37 +0100

    Make self-test a command, instead of an option
    Now you can self-test by calling `gaphor self-test`.

--- gaphor/ui/tests/test_selftest.py
+++ gaphor/ui/tests/test_selftest.py
@@ -5,6 +5,6 @@ from gaphor.ui import run
 
 def test_self_test(caplog):
     caplog.set_level(logging.DEBUG)
-    exit_code = run(["--self-test"])
+    exit_code = run(argv=[], launch_service="self_test")
 
     assert exit_code == 0
