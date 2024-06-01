ecebd10f34aa0d619989a1aea61fc744784f6ce0
Arjan Molenaar <gaphor@gmail.com>
2024-06-01 11:59:51 +0200

    Fix model browser entries enter edit state
    The "editing" property on the TreeItem needs to be reset when editing is
    done.

--- gaphor/ui/treemodel.py
+++ gaphor/ui/treemodel.py
@@ -56,6 +56,9 @@ class TreeItem(GObject.Object):
     def start_editing(self):
         self.editing = True
 
+    def done_editing(self):
+        self.editing = False
+
 
 class RelationshipItem(TreeItem):
     def __init__(self, child_model):
