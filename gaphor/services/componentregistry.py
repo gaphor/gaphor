eb87a70025f2a1a929e8c95c659c9d435d093ecc
Arjan Molenaar <gaphor@gmail.com>
2024-06-18 09:38:18 +0200

    Add partial method to component registry
    The partial method can be used to inject services on arbitrary objects.

--- gaphor/services/componentregistry.py
+++ gaphor/services/componentregistry.py
@@ -1,5 +1,7 @@
 """A registry for components (e.g. services) and event handling."""
 
+import functools
+import inspect
 from typing import Iterator, List, Tuple, Type, TypeVar
 
 from gaphor.abc import Service
@@ -48,3 +50,18 @@ class ComponentRegistry(Service):
 
     def all(self, base: Type[T]) -> Iterator[Tuple[str, T]]:
         return ((n, c) for n, c in self._comp if isinstance(c, base))
+
+    def partial(self, func):
+        """Return a new function with partial application of services."""
+        kwargs = {}
+
+        for param_name, _param in inspect.signature(func).parameters.items():
+            found = [(n, c) for n, c in self._comp if n == param_name]
+            if len(found) > 1:
+                raise ComponentLookupError(
+                    f"More than one component matches {param_name}: {found}"
+                )
+            if found:
+                kwargs[param_name] = found[0][1]
+
+        return functools.partial(func, **kwargs)
