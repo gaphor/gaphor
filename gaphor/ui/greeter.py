f8fe9828964aadc34a9b79a2d9995c2730a1c393
Arjan Molenaar <gaphor@gmail.com>
2024-08-02 13:46:04 +0200

    Remove use of deprecated Gtk.StyleContext
    Instead, set style classes on widgets directly.

--- gaphor/ui/greeter.py
+++ gaphor/ui/greeter.py
@@ -120,7 +120,7 @@ class Greeter(Service, ActionProvider):
         self.greeter = builder.get_object("greeter")
         self.greeter.set_application(self.gtk_app)
         if ".dev" in distribution().version:
-            self.greeter.get_style_context().add_class("devel")
+            self.greeter.add_css_class("devel")
 
         listbox = builder.get_object("recent-files")
         templates = builder.get_object("templates")
