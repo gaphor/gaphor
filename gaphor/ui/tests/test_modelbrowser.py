14d278fce387d473b3cdf90e90332dcef9ff15b8
nomisge <nomisge@live.de>
2024-10-21 19:33:13 +0200

    ran pre-commit tasks

--- gaphor/ui/tests/test_modelbrowser.py
+++ gaphor/ui/tests/test_modelbrowser.py
@@ -1,8 +1,8 @@
 import pytest
 
 from gaphor import UML
-from gaphor.i18n import gettext
 from gaphor.core.modeling import Diagram, ModelReady
+from gaphor.i18n import gettext
 from gaphor.ui.modelbrowser import (
     ElementDragData,
     ModelBrowser,
@@ -267,7 +267,7 @@ def test_generalization_text(model_browser, element_factory):
     assert tree_item
     assert branch.relationships[0].element is generalization
     assert branch.relationships[0].readonly_text == gettext("general: General")
-    
+
 
 def test_drop_multiple_elements(model_browser, element_factory, event_manager):
     class_a = element_factory.create(UML.Class)
