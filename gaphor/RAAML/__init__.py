33eedb0295d27a1d113baad625fbfb158f21a2d3
Arjan Molenaar <gaphor@gmail.com>
2024-12-11 17:09:46 +0100

    All diagrams should get the same default icon

--- gaphor/RAAML/__init__.py
+++ gaphor/RAAML/__init__.py
@@ -1,6 +1,5 @@
 # ruff: noqa: F401
 
-import gaphor.RAAML.iconname
 import gaphor.RAAML.modelinglanguage
 
 __modeling_language__ = "RAAML"
--- gaphor/SysML/__init__.py
+++ gaphor/SysML/__init__.py
@@ -2,7 +2,6 @@
 
 import gaphor.SysML.diagramlabel
 import gaphor.SysML.drop
-import gaphor.SysML.iconname
 import gaphor.SysML.modelinglanguage
 import gaphor.SysML.sysmlfmt
 
