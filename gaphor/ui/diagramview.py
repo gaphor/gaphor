614f4cd149f3eca13aa8a55e4fb5408bb1bb172f
Arjan Molenaar <gaphor@gmail.com>
2024-12-28 10:55:59 +0100

    Centralize shortcut code

--- gaphor/ui/diagramview.py
+++ gaphor/ui/diagramview.py
@@ -1,11 +1,11 @@
 import logging
-import sys
 
 from gaphas.model import Model
 from gaphas.view import GtkView
-from gi.repository import GObject, Gtk
+from gi.repository import GObject
 
 from gaphor.diagram.selection import Selection
+from gaphor.ui.actiongroup import named_shortcut
 
 log = logging.getLogger(__name__)
 
@@ -58,21 +58,12 @@ if hasattr(DiagramView, "install_action"):
     )
     DiagramView.install_action("diagram.delete", None, _trigger_signal("delete"))
 
-    def _new_named_shortcut(shortcut, action_name):
-        return Gtk.Shortcut.new(
-            trigger=Gtk.ShortcutTrigger.parse_string(shortcut),
-            action=Gtk.NamedAction.new(action_name),
-        )
-
-    _mod = "<Meta>" if sys.platform == "darwin" else "<Control>"
-
-    DiagramView.add_shortcut(_new_named_shortcut(f"{_mod}x", "clipboard.cut"))
-    DiagramView.add_shortcut(_new_named_shortcut(f"{_mod}c", "clipboard.copy"))
-    DiagramView.add_shortcut(_new_named_shortcut(f"{_mod}v", "clipboard.paste"))
+    DiagramView.add_shortcut(named_shortcut("<Primary>x", "clipboard.cut"))
+    DiagramView.add_shortcut(named_shortcut("<Primary>c", "clipboard.copy"))
+    DiagramView.add_shortcut(named_shortcut("<Primary>v", "clipboard.paste"))
     DiagramView.add_shortcut(
-        _new_named_shortcut(f"{_mod}<Shift>v", "clipboard.paste-full")
+        named_shortcut("<Primary><Shift>v", "clipboard.paste-full")
     )
     DiagramView.add_shortcut(
-        _new_named_shortcut("Delete|BackSpace|<Meta>BackSpace", "diagram.delete")
+        named_shortcut("Delete|BackSpace|<Meta>BackSpace", "diagram.delete")
     )
-    del _mod
