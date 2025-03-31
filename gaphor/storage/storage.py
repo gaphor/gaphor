a778c943414130de9c311c7251e1885abc6f4d55
Mike Kidner <mike.kidner@resonatesystems.com.au>
2025-03-31 10:40:37 +1030

    post pre-commit run

--- gaphor/storage/storage.py
+++ gaphor/storage/storage.py
@@ -588,7 +588,7 @@ def upgrade_simple_properties_to_value_specifications(
                     if type is not None and isinstance(value, str):
                         upperValue = element_factory.create(type)
                         try:
-                        # Overwrite any string value that is not '*'
+                            # Overwrite any string value that is not '*'
                             upperValue.value = int(value)
                         except ValueError:
                             upperValue.value = "*"
