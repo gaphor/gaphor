83a99bfb312bbe94ba387ed32ee5f24ab17d6ec0
Max Ullrich <max@ullrichfamily.com>
2025-07-02 12:19:05 -0400

    Updated Spelling Mistake

--- gaphor/SysML/blocks/parametricstoolbox.py
+++ gaphor/SysML/blocks/parametricstoolbox.py
@@ -1,15 +1,13 @@
 """The definition for the parametric section of the toolbox."""
 
-from gaphor import UML
 from gaphor.core import gettext
 from gaphor.diagram.diagramtoolbox import ToolDef, ToolSection, new_item_factory
 from gaphor.SysML import diagramitems as sysml_items
 from gaphor.UML import diagramitems as uml_items
 from gaphor.UML.toolboxconfig import named_element_config
 
-
 parametric_blocks = ToolSection(
-    gettext("Parametric"),
+    gettext("Parametric Blocks"),
     (
         ToolDef(
             "toolbox-connector",
