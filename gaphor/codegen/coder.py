2326e86b43835c12a38df1529ea69f2d5113a1d1
marcos ppca <maldsjr@hotmail.com>
2025-11-10 10:03:52 -0300

    formatting in coder.py

--- gaphor/codegen/coder.py
+++ gaphor/codegen/coder.py
@@ -67,9 +67,7 @@ header = textwrap.dedent(
         relation_one,
     )
 
-    """.format(
-        "ruff"
-    )  # work around tooling triggers
+    """.format("ruff")  # work around tooling triggers
 )
 
 
@@ -115,8 +113,8 @@ def main(
     with (
         open(outfile, "w", encoding="utf-8")
         if outfile
-        else contextlib.nullcontext(sys.stdout)
-    ) as out:
+        else contextlib.nullcontext(sys.stdout) as out
+    ):
         for line in coder(model, super_models, overrides):
             print(line, file=out)
 
@@ -374,7 +372,6 @@ def default_value(a) -> str:
                     )
                 else:
                     defaultValue = a.defaultValue.title()
-
             case "str":
                 if isinstance(
                     a.defaultValue,
@@ -388,7 +385,6 @@ def default_value(a) -> str:
                     )
                 else:
                     defaultValue = f'"{a.defaultValue}"'
-
             case "bool":
                 if isinstance(a.defaultValue, UML.LiteralBoolean | UML.LiteralString):
                     defaultValue = UML.recipes.get_literal_value_as_string(
@@ -396,17 +392,14 @@ def default_value(a) -> str:
                     )
                 else:
                     defaultValue = a.defaultValue
-
                 if defaultValue == "true":
                     defaultValue = "True"
                 elif defaultValue == "false":
                     defaultValue = "False"
-
             case _:
                 raise ValueError(
                     f"Unknown default value type: {a.owner.name}.{a.name}: {a.typeValue} = {a.defaultValue}"
                 )
-
         return f", default={defaultValue}"
     return ""
 
@@ -484,9 +477,7 @@ def is_enumeration(c: UML.Type) -> bool:
 def is_simple_type(c: UML.Type) -> bool:
     return any(
         s.name == "SimpleAttribute" for s in UML.recipes.get_applied_stereotypes(c)
-    ) or any(
-        is_simple_type(g.general) for g in c.generalization
-    )  # type: ignore[attr-defined]
+    ) or any(is_simple_type(g.general) for g in c.generalization)  # type: ignore[attr-defined]
 
 
 def is_tilde_type(c: UML.Type) -> bool:
@@ -579,9 +570,9 @@ def in_super_model(
         and ".".join(e.owningPackage.qualifiedName) not in super_models
     ):
         element_type = modeling_language.lookup_element(cls.name, ns=ns)
-        assert (
-            element_type
-        ), f"Type {ns}.{name} found in model, but not in generated model"
+        assert element_type, (
+            f"Type {ns}.{name} found in model, but not in generated model"
+        )
         return element_type, cls
 
     raise AssertionError(f"Type {ns}.{name} found in model, but not in generated model")
