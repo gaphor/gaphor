ec29ec98c7e39f37f01d912a09dfa47fa316d6a9
Arjan Molenaar <gaphor@gmail.com>
2026-05-02 15:00:44 +0200

    SysML2: remove direct UML dependencies from language and toolbox

--- gaphor/SysML2/toolbox.py
+++ gaphor/SysML2/toolbox.py
@@ -1,4 +1,81 @@
-from gaphor.diagram.diagramtoolbox import ToolboxDefinition
-from gaphor.UML.general.generaltoolbox import general_tools
+import getpass
+import time
+
+from gaphas.item import SE
+
+from gaphor.core import gettext
+from gaphor.diagram.diagramtoolbox import (
+    ToolboxDefinition,
+    ToolDef,
+    ToolSection,
+    new_item_factory,
+)
+from gaphor.diagram.general import diagramitems as general
+
+
+def metadata_config(metadata_item: general.MetadataItem) -> None:
+    metadata_item.createdBy = getpass.getuser()
+    metadata_item.description = metadata_item.diagram.name
+    metadata_item.revision = "1.0"
+    metadata_item.createdOn = time.strftime("%Y-%m-%d")
+
+
+general_tools = ToolSection(
+    gettext("General"),
+    (
+        ToolDef(
+            "toolbox-pointer",
+            gettext("Pointer"),
+            "gaphor-pointer-symbolic",
+            "Escape",
+            item_factory=None,
+        ),
+        ToolDef(
+            "toolbox-magnet",
+            gettext("Magnet"),
+            "gaphor-magnet-symbolic",
+            "F1",
+            item_factory=None,
+        ),
+        ToolDef(
+            "toolbox-line",
+            gettext("Line"),
+            "gaphor-line-symbolic",
+            "l",
+            new_item_factory(general.Line),
+        ),
+        ToolDef(
+            "toolbox-box",
+            gettext("Box"),
+            "gaphor-box-symbolic",
+            "b",
+            new_item_factory(general.Box),
+            SE,
+        ),
+        ToolDef(
+            "toolbox-diamond",
+            gettext("Diamond"),
+            "gaphor-diamond-symbolic",
+            None,
+            new_item_factory(general.Diamond),
+            SE,
+        ),
+        ToolDef(
+            "toolbox-ellipse",
+            gettext("Ellipse"),
+            "gaphor-ellipse-symbolic",
+            "e",
+            new_item_factory(general.Ellipse),
+            SE,
+        ),
+        ToolDef(
+            "toolbox-metadata",
+            gettext("Diagram metadata"),
+            "gaphor-metadata-symbolic",
+            None,
+            new_item_factory(general.MetadataItem, config_func=metadata_config),
+        ),
+    ),
+)
 
 sysml2_toolbox_actions: ToolboxDefinition = (general_tools,)
